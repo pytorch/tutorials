ddef941d20efcf6162cd9e0d551df6aba46fe4ab
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2017-03-24 10:09:44 +0530

    Add sphinx notebook tutorials

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-PyTorch Tutorials
------------------
+# PyTorch Tutorials
 
 
-1. [Deep Learning with PyTorch: a 60-minute blitz](Deep%20Learning%20with%20PyTorch.ipynb)
-   - A perfect introduction to PyTorch's torch, autograd, nn and optim APIs
-   - If you are a former Torch user, you can check out this instead: [Introduction to PyTorch for former Torchies](Introduction%20to%20PyTorch%20for%20former%20Torchies.ipynb)
-2. Custom C extensions
-   - [Write your own C code that interfaces into PyTorch via FFI](Creating%20Extensions%20using%20FFI.md)
-3. [Writing your own neural network module that uses numpy and scipy](Creating%20extensions%20using%20numpy%20and%20scipy.ipynb)
-4. [Reinforcement (Q-)Learning with PyTorch](Reinforcement%20(Q-)Learning%20with%20PyTorch.ipynb)
+All the tutorials are now presented as sphinx style documentation at pytorch.org/tutorials. 
+
+
+
+## Contributing
+
+We use sphinx-gallery's [notebook styled examples](https://sphinx-gallery.readthedocs.io/en/latest/tutorials/plot_notebook.html#sphx-glr-tutorials-plot-notebook-py) to create the tutorials. Syntax is very simple. In essence, you write a slightly well formatted python file and it shows up as documentation page.
+
+Here's how to create a new tutorial:
+1. Create a notebook styled python file. If you want it executed while inserted into documentation, save the file as `your_tutorial.py`.
+2. Put it in one of the beginner_source, intermediate_source, advanced_source based on the level.
+2. Include it in the right TOC tree at index.rst
+3. Create a thumbnail in the index file using a command like `.. galleryitem:: beginner/your_tutorial.py`. (This is a custom directive. Argument should be a full path to the python file without a slash at the beginning.) 
---
+++ custom_directives.py
@@ -0,0 +1,153 @@
+from docutils.parsers.rst import Directive, directives
+from docutils.statemachine import StringList 
+from docutils import nodes
+import re
+import os
+import sphinx_gallery
+
+class IncludeDirective(Directive):
+
+    # defines the parameter the directive expects
+    # directives.unchanged means you get the raw value from RST
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    has_content = False
+    add_index = False
+
+    docstring_pattern = r'"""(?P<docstring>(?:.|[\r\n])*?)"""\n'
+    docstring_regex = re.compile(docstring_pattern)
+
+    def run(self):
+        document = self.state.document
+        env = document.settings.env
+        rel_filename, filename = env.relfn2path(self.arguments[0])
+
+        try:
+            text = open(filename).read()
+            text_no_docstring = self.docstring_regex.sub('', text)
+
+            code_block = nodes.literal_block(text=text_no_docstring)
+            return [code_block]
+        except FileNotFoundError as e:
+            print(e)
+            return []
+
+
+class GalleryItemDirective(Directive):
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec = {'figure': directives.unchanged,
+                   'intro': directives.unchanged}
+    has_content = False
+    add_index = False
+
+    def run(self):
+        args = self.arguments
+        fname = args[-1]
+        basename = os.path.basename(fname)
+        dirname = os.path.dirname(fname)
+
+        try:
+            if 'intro' in self.options:
+                intro = self.options['intro'][:195] + '...'
+            else:
+                intro = sphinx_gallery.gen_rst.extract_intro(fname)
+
+            thumbnail_rst = sphinx_gallery.backreferences._thumbnail_div(dirname, basename, intro)
+
+            if 'figure' in self.options:
+                env = self.state.document.settings.env
+                rel_figname, figname = env.relfn2path(self.options['figure'])
+                save_figname = os.path.join('_static/thumbs/', os.path.basename(figname))
+
+                try:
+                    os.makedirs('_static/thumbs')
+                except FileExistsError:
+                    pass
+
+                sphinx_gallery.gen_rst.scale_image(figname, save_figname, 400, 280)
+                thumbnail_rst = re.sub(r'..\sfigure::\s.*\.png',
+                                       '.. figure:: /{}'.format(save_figname),
+                                       thumbnail_rst)
+
+            thumbnail = StringList(thumbnail_rst.split('\n'))
+            thumb = nodes.paragraph()
+            self.state.nested_parse(thumbnail, self.content_offset, thumb)
+
+            return [thumb]
+        except FileNotFoundError as e:
+            print(e)
+            return []
+
+
+
+GALLERY_TEMPLATE = """
+.. raw:: html
+
+    <div class="sphx-glr-thumbcontainer" tooltip="{tooltip}">
+
+.. only:: html
+
+    .. figure:: {thumbnail}
+
+        {description}
+
+.. raw:: html
+
+    </div>
+"""
+
+class CustomGalleryItemDirective(Directive):
+    required_arguments = 0
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec = {'tooltip': directives.unchanged,
+                   'figure': directives.unchanged,
+                   'description': directives.unchanged}
+
+    has_content = False
+    add_index = False
+
+    def run(self):
+        try:
+            if 'tooltip' in self.options:
+                tooltip = self.options['tooltip'][:195] + '...'
+            else:
+                raise ValueError('tooltip not found')
+
+            if 'figure' in self.options:
+                env = self.state.document.settings.env
+                rel_figname, figname = env.relfn2path(self.options['figure'])
+                thumbnail = os.path.join('_static/thumbs/', os.path.basename(figname))
+
+                try:
+                    os.makedirs('_static/thumbs')
+                except FileExistsError:
+                    pass
+
+                sphinx_gallery.gen_rst.scale_image(figname, thumbnail, 400, 280)
+            else:
+                thumbnail = '_static/img/thumbnails/default.png'
+
+            if 'description' in self.options:
+                description = self.options['description']
+            else:
+                raise ValueError('description not doc found')
+
+        except FileNotFoundError as e:
+            print(e)
+            return []
+        except ValueError as e:
+            print(e)
+            raise
+            return []
+
+        thumbnail_rst = GALLERY_TEMPLATE.format(tooltip=tooltip,
+                                                thumbnail=thumbnail,
+                                                description=description)
+        thumbnail = StringList(thumbnail_rst.split('\n'))
+        thumb = nodes.paragraph()
+        self.state.nested_parse(thumbnail, self.content_offset, thumb)
+        return [thumb]
