e9627aa25c067e13b07a9bbb769097f8edc9b256
bajibabu <bajibabu7@gmail.com>
2017-04-22 13:09:10 +0300

    convert string to unicode for 2.7 compatibility

--- intermediate_source/char_rnn_generation_tutorial.py
+++ intermediate_source/char_rnn_generation_tutorial.py
@@ -85,6 +85,10 @@ def findFiles(path): return glob.glob(path)
 
 # Turn a Unicode string to plain ASCII, thanks to http://stackoverflow.com/a/518232/2809427
 def unicodeToAscii(s):
+    try:
+        s = unicode(s, 'utf-8')
+    except NameError:
+        pass
     return ''.join(
         c for c in unicodedata.normalize('NFD', s)
         if unicodedata.category(c) != 'Mn'
