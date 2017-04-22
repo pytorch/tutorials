9790b8f2d612f8c59932e326f0db1a39d23fa4f6
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2017-04-23 01:27:45 +0530

    unicode strings for 2.7 compatibility

--- intermediate_source/seq2seq_translation_tutorial.py
+++ intermediate_source/seq2seq_translation_tutorial.py
@@ -83,7 +83,8 @@ And for more, read the papers that introduced these topics:
 
 **Requirements**
 """
-
+from __future__ import unicode_literals, print_function, division
+from io import open
 import unicodedata
 import string
 import re
@@ -208,7 +209,8 @@ def readLangs(lang1, lang2, reverse=False):
     print("Reading lines...")
 
     # Read the file and split into lines
-    lines = open('data/%s-%s.txt' % (lang1, lang2)).read().strip().split('\n')
+    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+        read().strip().split('\n')
 
     # Split every line into pairs and normalize
     pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
