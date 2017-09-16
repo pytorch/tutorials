63357452535439c457466067bf37526ffd74c9ca
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2017-09-16 18:00:56 +0530

    rebuild

--- _downloads/super_resolution_with_caffe2.py
+++ _downloads/super_resolution_with_caffe2.py
@@ -86,7 +86,10 @@ model_url = 'https://s3.amazonaws.com/pytorch/test_data/export/superres_epoch100
 batch_size = 1    # just a random number
 
 # Initialize model with the pretrained weights
-torch_model.load_state_dict(model_zoo.load_url(model_url))
+map_location = lambda storage, loc: storage
+if torch.cuda.is_available():
+    map_location = None
+torch_model.load_state_dict(model_zoo.load_url(model_url, map_location=map_location))
 
 # set the train mode to false since we will only run the forward pass.
 torch_model.train(False)
