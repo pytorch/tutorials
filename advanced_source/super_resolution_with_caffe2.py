95bf3983baa537bb97e4a069e665fd68b05c616f
Lu Fang <lufang@fb.com>
2017-10-12 17:10:58 -0700

    Update the Super Resolution example, so it is compatible with latest ONNX

--- advanced_source/super_resolution_with_caffe2.py
+++ advanced_source/super_resolution_with_caffe2.py
@@ -131,21 +131,21 @@ torch_out = torch.onnx._export(torch_model,             # model being run
 import onnx
 import onnx_caffe2.backend
 
-# Load the ONNX GraphProto object. Graph is a standard Python protobuf object
-graph = onnx.load("super_resolution.onnx")
+# Load the ONNX ModelProto object. model is a standard Python protobuf object
+model = onnx.load("super_resolution.onnx")
 
-# prepare the caffe2 backend for executing the model this converts the ONNX graph into a
+# prepare the caffe2 backend for executing the model this converts the ONNX model into a
 # Caffe2 NetDef that can execute it. Other ONNX backends, like one for CNTK will be
 # availiable soon.
-prepared_backend = onnx_caffe2.backend.prepare(graph)
+prepared_backend = onnx_caffe2.backend.prepare(model)
 
 # run the model in Caffe2
 
 # Construct a map from input names to Tensor data.
-# The graph itself contains inputs for all weight parameters, followed by the input image.
+# The graph of the model itself contains inputs for all weight parameters, after the input image.
 # Since the weights are already embedded, we just need to pass the input image.
-# last input the grap
-W = {graph.input[-1]: x.data.numpy()}
+# Set the first input.
+W = {model.graph.input[0].name: x.data.numpy()}
 
 # Run the Caffe2 net:
 c2_out = prepared_backend.run(W)[0]
@@ -153,6 +153,7 @@ c2_out = prepared_backend.run(W)[0]
 # Verify the numerical correctness upto 3 decimal places
 np.testing.assert_almost_equal(torch_out.data.cpu().numpy(), c2_out, decimal=3)
 
+print("Exported model has been executed on Caffe2 backend, and the result looks good!")
 
 ######################################################################
 # We should see that the output of PyTorch and Caffe2 runs match
@@ -202,15 +203,15 @@ np.testing.assert_almost_equal(torch_out.data.cpu().numpy(), c2_out, decimal=3)
 # super-resolution model for the rest of this tutorial.
 #
 
-# extract the workspace and the graph proto from the internal representation
+# extract the workspace and the model proto from the internal representation
 c2_workspace = prepared_backend.workspace
-c2_graph = prepared_backend.predict_net
+c2_model = prepared_backend.predict_net
 
 # Now import the caffe2 mobile exporter
 from caffe2.python.predictor import mobile_exporter
 
 # call the Export to get the predict_net, init_net. These nets are needed for running things on mobile
-init_net, predict_net = mobile_exporter.Export(c2_workspace, c2_graph, c2_graph.external_input)
+init_net, predict_net = mobile_exporter.Export(c2_workspace, c2_model, c2_model.external_input)
 
 # Let's also save the init_net and predict_net to a file that we will later use for running them on mobile
 with open('init_net.pb', "wb") as fopen:
