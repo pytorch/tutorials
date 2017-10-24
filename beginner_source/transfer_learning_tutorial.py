8df712e9b20d58b894100f5b325e3a675404cb56
tztztztztz <120165967@qq.com>
2017-10-24 13:52:23 +0800

    fix image show error (#165)
    * since it normalized the channel value to specific std and mean, when show the image and  reverse its value to [0, 1], sometimes it got error of **Floating point image RGB values must be in the 0..1 range.**
    
    * use np.clip instead

--- beginner_source/transfer_learning_tutorial.py
+++ beginner_source/transfer_learning_tutorial.py
@@ -111,6 +111,7 @@ def imshow(inp, title=None):
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
     inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
     plt.imshow(inp)
     if title is not None:
         plt.title(title)
