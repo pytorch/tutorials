Fix loss calculation in transfer learning tutorial (#187)
* Fix loss calculation in transfer learning tutorial

* Multiply batch loss by batch size to get sum loss

beginner_source/transfer_learning_tutorial.py
@@ -189,7 +189,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                     optimizer.step()
 
                 # statistics
-                running_loss += loss.data[0]
+                running_loss += loss.data[0] * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
 
             epoch_loss = running_loss / dataset_sizes[phase]