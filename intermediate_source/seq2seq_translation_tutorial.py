6af5c79d7ad3f5d683cf0e2e0c716fe552b6f111
Jinyang Zhou <jinyang.zhou@guokr.com>
2018-04-02 14:27:44 +0800

    Update seq2seq_translation_tutorial.py
    seems like an index error. p[0] stands for the eng sentence right?

--- intermediate_source/seq2seq_translation_tutorial.py
+++ intermediate_source/seq2seq_translation_tutorial.py
@@ -251,7 +251,7 @@ eng_prefixes = (
 def filterPair(p):
     return len(p[0].split(' ')) < MAX_LENGTH and \
         len(p[1].split(' ')) < MAX_LENGTH and \
-        p[1].startswith(eng_prefixes)
+        p[0].startswith(eng_prefixes)
 
 
 def filterPairs(pairs):
