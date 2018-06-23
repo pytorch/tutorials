d58676065b6c4becd7ab30d364bf8d9fa15fade2
Kristofer Krus <krikru@users.noreply.github.com>
2018-06-24 00:09:29 +0200

    Fix broken pipe error in cifar10_tutorial.py

--- beginner_source/blitz/cifar10_tutorial.py
+++ beginner_source/blitz/cifar10_tutorial.py
@@ -67,15 +67,22 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
+########################################################################
+# When creating the ``DataLoader`` objects, we may need to set
+# ``num_workers`` to ``0`` on Windows to prevent that a `BrokenPipeError
+# exception <https://pytorch.org/docs/stable/notes/windows.html#multiprocessing-error-broken-pipe>`_
+# is thrown. On Linux and Mac, it can be set to something else, e.g.
+# ``2``.
+
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+                                          shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+                                         shuffle=False, num_workers=0)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
