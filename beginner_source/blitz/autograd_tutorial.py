bae452310fa692b4b08aad8ef633599faa59e0a0
Tim Schoenheider <timrs2998@gmail.com>
2018-08-02 19:45:02 -0500

    Correct typo in sentence in autograd_tutorial.py

--- beginner_source/blitz/autograd_tutorial.py
+++ beginner_source/blitz/autograd_tutorial.py
@@ -128,8 +128,7 @@ print(x.grad)
 
 ###############################################################
 # You can also stop autograd from tracking history on Tensors
-# with ``.requires_grad``=True by wrapping the code block in
-# ``with torch.no_grad():``
+# by wrapping the code block in ``with torch.no_grad():``.
 print(x.requires_grad)
 print((x ** 2).requires_grad)
 
