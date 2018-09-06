b9b8fc6bc52b2916ca255f49276d69c56020a90d
Manuel A. Vázquez <33908626+manuvazquez@users.noreply.github.com>
2018-09-06 22:20:54 +0200

    A couple of typos at the end (misplaced ``)

--- beginner_source/blitz/autograd_tutorial.py
+++ beginner_source/blitz/autograd_tutorial.py
@@ -128,8 +128,8 @@ print(x.grad)
 
 ###############################################################
 # You can also stop autograd from tracking history on Tensors
-# with ``.requires_grad``=True by wrapping the code block in
-# ``with torch.no_grad():``
+# with ``.requires_grad=True`` by wrapping the code block in
+# ``with torch.no_grad()``:
 print(x.requires_grad)
 print((x ** 2).requires_grad)
 
