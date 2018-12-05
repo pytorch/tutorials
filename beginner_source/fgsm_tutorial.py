03cd4915be50f10014722c35b8b04736c145d199
Will Feng <willfeng@fb.com>
2018-12-04 19:04:59 -0500

    Set use_cuda=True for fgsm_tutorial

--- beginner_source/fgsm_tutorial.py
+++ beginner_source/fgsm_tutorial.py
@@ -133,7 +133,7 @@ import matplotlib.pyplot as plt
 
 epsilons = [0, .05, .1, .15, .2, .25, .3]
 pretrained_model = "data/lenet_mnist_model.pth"
-use_cuda=False
+use_cuda=True
 
 
 ######################################################################
