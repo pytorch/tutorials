e4789e246ed8389193959ae2626a28407d4ca787
pytorchbot <soumith+bot@pytorch.org>
2018-12-13 06:52:05 +0000

    Automated tutorials push

--- _downloads/c09ceca853695eb26f36ed3d5a66c39b/chatbot_tutorial.py
+++ _downloads/c09ceca853695eb26f36ed3d5a66c39b/chatbot_tutorial.py
@@ -890,7 +890,7 @@ class LuongAttnDecoderRNN(nn.Module):
 
 def maskNLLLoss(inp, target, mask):
     nTotal = mask.sum()
-    crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)))
+    crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)).squeeze(1))
     loss = crossEntropy.masked_select(mask).mean()
     loss = loss.to(device)
     return loss, nTotal.item()
