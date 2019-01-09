4cc60a8e07afadcd11af361338abe15514582320
pytorchbot <soumith+bot@pytorch.org>
2019-01-09 03:39:09 +0000

    Automated tutorials push

--- _downloads/a96a2daac1918ec72f68233dfe3f2c47/seq2seq_translation_tutorial.py
+++ _downloads/a96a2daac1918ec72f68233dfe3f2c47/seq2seq_translation_tutorial.py
@@ -240,7 +240,7 @@ MAX_LENGTH = 10
 eng_prefixes = (
     "i am ", "i m ",
     "he is", "he s ",
-    "she is", "she s",
+    "she is", "she s ",
     "you are", "you re ",
     "we are", "we re ",
     "they are", "they re "
