a16b328713a878828604548a51f374c9f63fb77b
Ramiro <ramirocamino@gmail.com>
2019-05-15 15:11:16 +0200

    Drop all words after generating the EOS token.

--- beginner_source/chatbot_tutorial.py
+++ beginner_source/chatbot_tutorial.py
@@ -1226,7 +1226,7 @@ def evaluateInput(encoder, decoder, searcher, voc):
             # Evaluate sentence
             output_words = evaluate(encoder, decoder, searcher, voc, input_sentence)
             # Format and print response sentence
-            output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
+            output_words[:] = itertools.takewhile(lambda word: word != 'EOS', output_words)
             print('Bot:', ' '.join(output_words))
 
         except KeyError:
