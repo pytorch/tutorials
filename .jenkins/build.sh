ecd7683eb5f7f3091b91df7504c81d78fb103f8f
Will Feng <willfeng@fb.com>
2019-05-23 18:26:15 -0400

    Revert "fix"
    This reverts commit c9ba90571ea096840aa8d11435fb756385adcb8a.

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -27,8 +27,7 @@ pip install sphinx-gallery tqdm matplotlib ipython pillow==4.1.1
 # Install torchvision from conda
 pip uninstall -y torchvision || true
 pip uninstall -y torchvision || true
-# GPU, requires CUDA version 9.0
-conda install https://anaconda.org/pytorch/torchvision/0.3.0/download/linux-64/torchvision-0.3.0-py36_cu9.0.176_1.tar.bz2 --no-deps
+conda install -c pytorch torchvision --no-deps
 
 # Install torchaudio from source
 git clone https://github.com/pytorch/audio --quiet
@@ -45,6 +44,7 @@ if [[ $(pip show torch) ]]; then
 fi
 
 # Install a nightly build of pytorch
+
 # GPU, requires CUDA version 9.0
 pip install cython torch_nightly -f https://download.pytorch.org/whl/nightly/cu90/torch_nightly.html
 
