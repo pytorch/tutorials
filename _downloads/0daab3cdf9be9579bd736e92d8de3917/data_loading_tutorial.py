bb1ab84a395fe191bcf99a2fc4defa233f95d0ed
pytorchbot <soumith+bot@pytorch.org>
2019-05-27 05:48:50 +0000

    Automated tutorials push

--- _downloads/0daab3cdf9be9579bd736e92d8de3917/data_loading_tutorial.py
+++ _downloads/0daab3cdf9be9579bd736e92d8de3917/data_loading_tutorial.py
@@ -379,13 +379,14 @@ def show_landmarks_batch(sample_batched):
             sample_batched['image'], sample_batched['landmarks']
     batch_size = len(images_batch)
     im_size = images_batch.size(2)
+    grid_border_size = 2
 
     grid = utils.make_grid(images_batch)
     plt.imshow(grid.numpy().transpose((1, 2, 0)))
 
     for i in range(batch_size):
-        plt.scatter(landmarks_batch[i, :, 0].numpy() + i * im_size,
-                    landmarks_batch[i, :, 1].numpy(),
+        plt.scatter(landmarks_batch[i, :, 0].numpy() + i * im_size + (i + 1) * grid_border_size,
+                    landmarks_batch[i, :, 1].numpy() + grid_border_size,
                     s=10, marker='.', c='r')
 
         plt.title('Batch from dataloader')
