4aaa7eba3815860fb5df0120d3172c84b9057e81
karan07 <codered1704@gmail.com>
2019-06-03 19:47:41 +0530

    as_matrix deprecated in pandas

--- beginner_source/data_loading_tutorial.py
+++ beginner_source/data_loading_tutorial.py
@@ -139,7 +139,8 @@ class FaceLandmarksDataset(Dataset):
         img_name = os.path.join(self.root_dir,
                                 self.landmarks_frame.iloc[idx, 0])
         image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:].as_matrix()
+        landmarks = self.landmarks_frame.iloc[idx, 1:]
+        landmarks = np.array(landmarks)
         landmarks = landmarks.astype('float').reshape(-1, 2)
         sample = {'image': image, 'landmarks': landmarks}
 
