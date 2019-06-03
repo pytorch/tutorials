b29085c26455a8aa4c41511e469ee40f81419be7
karan07 <codered1704@gmail.com>
2019-06-03 20:14:36 +0530

    Update data_loading_tutorial.py

--- beginner_source/data_loading_tutorial.py
+++ beginner_source/data_loading_tutorial.py
@@ -140,7 +140,7 @@ class FaceLandmarksDataset(Dataset):
                                 self.landmarks_frame.iloc[idx, 0])
         image = io.imread(img_name)
         landmarks = self.landmarks_frame.iloc[idx, 1:]
-        landmarks = np.array(landmarks)
+        landmarks = np.array([landmarks])
         landmarks = landmarks.astype('float').reshape(-1, 2)
         sample = {'image': image, 'landmarks': landmarks}
 
