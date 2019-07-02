83ffcc7740f58838c8acb3b9e9bc9bf597c3f244
Brian Johnson <brianjo@fb.com>
2019-07-02 08:36:27 -0700

    Update cifar10_tutorial.py

--- beginner_source/blitz/cifar10_tutorial.py
+++ beginner_source/blitz/cifar10_tutorial.py
@@ -108,7 +108,7 @@ print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 ########################################################################
 # 2. Define a Convolutional Neural Network
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Copy the neural network from the Neural Networks section before and modify it to
 # take 3-channel images (instead of 1-channel images as it was defined).
 
