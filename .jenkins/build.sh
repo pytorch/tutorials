8af04dab1ff825ee1baf19d0e90d806d4baeb1a0
Brian Johnson <brianjo@fb.com>
2019-07-21 00:02:09 -0700

    Update build.sh
    repining sphinx trying pip install

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -16,7 +16,7 @@ rm -rf src
 pip install -r $DIR/../requirements.txt
 
 export PATH=/opt/conda/bin:$PATH
-conda install -y sphinx pandas
+pip install -y sphinx==1.8.2 pandas
 
 # install awscli
 pip uninstall awscli
