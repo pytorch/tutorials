ba975e18d7c8996fa162922ccb7300cb3564717f
Ubuntu <ubuntu@ip-172-31-22-54.ec2.internal>
2019-08-05 03:05:10 +0000

    Remove flask tutorial notebook folder

--- intermediate_source/flask_tutorial/test_flask.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-resp = requests.post("http://localhost:5000/predict", 
-                     files={"file": open('/home/ubuntu/03_pytorch-tutorials/_static/img/cat.jpg','rb')})
-
-print(resp.json())
