3adce626327079821cdc54067b8027ab7db4053f
Rich Morin <rdm@cfcl.com>
2019-10-14 01:01:01 -0700

    minor copy edits

--- beginner_source/blitz/tensor_tutorial.py
+++ beginner_source/blitz/tensor_tutorial.py
@@ -45,7 +45,7 @@ x = torch.rand(5, 3)
 print(x)
 
 ###############################################################
-# Construct a matrix filled zeros and of dtype long:
+# Construct a matrix filled with zeros and of dtype long:
 
 x = torch.zeros(5, 3, dtype=torch.long)
 print(x)
@@ -105,7 +105,7 @@ print(y)
 
 ###############################################################
 # .. note::
-#     Any operation that mutates a tensor in-place is post-fixed with an ``_``.
+#     Any operation that mutates a tensor in-place is suffixed with an ``_``.
 #     For example: ``x.copy_(y)``, ``x.t_()``, will change ``x``.
 #
 # You can use standard NumPy-like indexing with all bells and whistles!
@@ -120,8 +120,8 @@ z = x.view(-1, 8)  # the size -1 is inferred from other dimensions
 print(x.size(), y.size(), z.size())
 
 ###############################################################
-# If you have a one element tensor, use ``.item()`` to get the value as a
-# Python number
+# If you have a one-element tensor, use ``.item()`` to get the value as a
+# Python number.
 x = torch.randn(1)
 print(x)
 print(x.item())
@@ -141,7 +141,7 @@ print(x.item())
 # Converting a Torch Tensor to a NumPy array and vice versa is a breeze.
 #
 # The Torch Tensor and NumPy array will share their underlying memory
-# locations (if the Torch Tensor is on CPU), and changing one will change
+# locations (if the Torch Tensor is on the CPU) and changing one will change
 # the other.
 #
 # Converting a Torch Tensor to a NumPy Array
@@ -166,7 +166,7 @@ print(b)
 ###############################################################
 # Converting NumPy Array to Torch Tensor
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# See how changing the np array changed the Torch Tensor automatically
+# See how changing the np array changed the Torch Tensor automatically.
 
 import numpy as np
 a = np.ones(5)
@@ -184,8 +184,8 @@ print(b)
 #
 # Tensors can be moved onto any device using the ``.to`` method.
 
-# let us run this cell only if CUDA is available
-# We will use ``torch.device`` objects to move tensors in and out of GPU
+# Let us run this cell only if CUDA is available.
+# We will use ``torch.device`` objects to move tensors in and out of the GPU.
 if torch.cuda.is_available():
     device = torch.device("cuda")          # a CUDA device object
     y = torch.ones_like(x, device=device)  # directly create a tensor on GPU
