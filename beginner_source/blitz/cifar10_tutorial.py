af94e8e84d18bb9f70fa528ec3437d3c1dfc55ab
Tony Duan <tonyduan@users.noreply.github.com>
2019-11-11 21:47:14 -0800

    Fix channel normalization in CIFAR-10 tutorial.
    I believe these are the actual mean and standard deviation statistics across the CIFAR dataset.
    
    Not sure why they were set to 0.5 for all channels previously, this can cause confusion.

--- beginner_source/blitz/cifar10_tutorial.py
+++ beginner_source/blitz/cifar10_tutorial.py
@@ -68,7 +68,7 @@ import torchvision.transforms as transforms
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
