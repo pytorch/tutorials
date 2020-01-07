c7d1c8f6010a60c805871fef35bd61db32d1404a
Josh Gordon <random-forests@users.noreply.github.com>
2020-01-07 13:53:26 -0500

    Update tensor_tutorial.py

--- beginner_source/blitz/tensor_tutorial.py
+++ beginner_source/blitz/tensor_tutorial.py
@@ -7,7 +7,7 @@ It’s a Python-based scientific computing package targeted at two sets of
 audiences:
 
 -  A replacement for NumPy to use the power of GPUs
--  a deep learning research platform that provides maximum flexibility
+-  A deep learning research platform that provides maximum flexibility
    and speed
 
 Getting Started
