bd1d9eb938a89e4c3c38b044e908ede06f319792
Ilya Kamenshchikov <ikamenshchikov@gmail.com>
2020-05-03 16:08:03 +0200

    Use python3 super syntax
    minor improvement - only python3 is supported by Torch 1.5, so we can use newer, shorter syntax.

--- beginner_source/blitz/cifar10_tutorial.py
+++ beginner_source/blitz/cifar10_tutorial.py
@@ -121,7 +121,7 @@ import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
