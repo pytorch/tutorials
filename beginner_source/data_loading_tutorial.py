1fa94a6d50a52a7e66f82deb72bbfe100a89e7f9
LeafyLi <zenglifa@msu.edu>
2020-05-14 19:57:11 -0400

    Fix spelling of "in to" -> "into"

--- beginner_source/data_loading_tutorial.py
+++ beginner_source/data_loading_tutorial.py
@@ -4,7 +4,7 @@ Writing Custom Datasets, DataLoaders and Transforms
 ===================================================
 **Author**: `Sasank Chilamkurthy <https://chsasank.github.io>`_
 
-A lot of effort in solving any machine learning problem goes in to
+A lot of effort in solving any machine learning problem goes into
 preparing the data. PyTorch provides many tools to make data loading
 easy and hopefully, to make your code more readable. In this tutorial,
 we will see how to load and preprocess/augment data from a non trivial
