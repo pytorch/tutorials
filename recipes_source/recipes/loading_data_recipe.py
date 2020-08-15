8ef3cacd55b7caca03bd9cc4a93a7678b8899741
Namgyu Ho <itsnamgyu@gmail.com>
2020-08-15 14:30:54 +0900

    Update loading_data_recipe.py
    Fix discontinued comment block

--- recipes_source/recipes/loading_data_recipe.py
+++ recipes_source/recipes/loading_data_recipe.py
@@ -91,7 +91,7 @@ import torchaudio
 # training and testing dataset will exist. The other parameters are
 # optional, with their default values shown. Here is some additional
 # useful info on the other parameters:
-
+#
 # * ``download``: If true, downloads the dataset from the internet and puts it in root directory. If dataset is already downloaded, it is not downloaded again.
 # * ``transform``: Using transforms on your data allows you to take it from its source state and transform it into data that’s joined together, de-normalized, and ready for training. Each library in PyTorch supports a growing list of transformations.
 # * ``target_transform``: A function/transform that takes in the target and transforms it.
