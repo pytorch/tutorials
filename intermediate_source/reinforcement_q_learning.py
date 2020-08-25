cc4e6f59750346a6323523e315f2c0539d3d7593
I-Ju Lin <9304486+lin826@users.noreply.github.com>
2020-08-25 16:16:04 +0800

    Update reinforcement_q_learning.py
    Simplify as `argmax` (ref: https://pytorch.org/docs/master/generated/torch.argmax.html#torch.argmax)

--- intermediate_source/reinforcement_q_learning.py
+++ intermediate_source/reinforcement_q_learning.py
@@ -347,10 +347,7 @@ def select_action(state):
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
-            # t.max(1) will return largest column value of each row.
-            # second column on max result is index of where max element was
-            # found, so we pick action with the larger expected reward.
-            return policy_net(state).max(1)[1].view(1, 1)
+            return policy_net(state).argmax(1).view(1, 1)
     else:
         return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
 
