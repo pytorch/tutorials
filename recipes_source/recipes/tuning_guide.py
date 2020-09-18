d45706d0e764e4daf921c9ae00c7c11b050231b5
Szymon Migacz <1934379+szmigacz@users.noreply.github.com>
2020-09-18 14:22:13 -0700

    Implemented changes suggested during initial review

--- recipes_source/recipes/tuning_guide.py
+++ recipes_source/recipes/tuning_guide.py
@@ -24,31 +24,12 @@ General optimizations
 #
 # Setting ``num_workers > 0`` enables asynchronous data loading and overlap
 # between the training and data loading. ``num_workers`` should be tuned
-# depending on the workload, CPU, accelerator, and location of training data.
+# depending on the workload, CPU, GPU, and location of training data.
 #
 # ``DataLoader`` accepts ``pin_memory`` argument, which defaults to ``False``.
-# When using an accelerator it's better to set ``pin_memory=True``, this
-# instructs ``DataLoader`` to use pinned memory and enables faster and
-# asynchronous memory copy from the host to the accelerator.
-
-###############################################################################
-# Disable debugging APIs
-# ~~~~~~~~~~~~~~~~~~~~~~
-# Many PyTorch APIs are intended for debugging and should be disabled for
-# regular training runs:
-#
-# * anomaly detection:
-#   `torch.autograd.detect_anomaly <https://pytorch.org/docs/stable/autograd.html#torch.autograd.detect_anomaly>`_
-#   or
-#   `torch.autograd.set_detect_anomaly(True) <https://pytorch.org/docs/stable/autograd.html#torch.autograd.set_detect_anomaly>`_
-# * profiler related:
-#   `torch.autograd.profiler.emit_nvtx <https://pytorch.org/docs/stable/autograd.html#torch.autograd.profiler.emit_nvtx>`_,
-#   `torch.autograd.profiler.profile <https://pytorch.org/docs/stable/autograd.html#torch.autograd.profiler.profile>`_
-# * autograd gradcheck:
-#   `torch.autograd.gradcheck <https://pytorch.org/docs/stable/autograd.html#torch.autograd.gradcheck>`_
-#   or
-#   `torch.autograd.gradgradcheck <https://pytorch.org/docs/stable/autograd.html#torch.autograd.gradgradcheck>`_
-#
+# When using a GPU it's better to set ``pin_memory=True``, this instructs
+# ``DataLoader`` to use pinned memory and enables faster and asynchronous memory
+# copy from the host to the GPU.
 
 ###############################################################################
 # Disable gradient calculation for validation or inference
@@ -82,12 +63,17 @@ General optimizations
 # This is also applicable to 1d and 3d convolutions as long as ``BatchNorm`` (or
 # other normalization layer) normalizes on the same dimension as convolution's
 # bias.
+#
+# Models available from `torchvision <https://github.com/pytorch/vision>`_
+# already implement this optimization.
 
 ###############################################################################
-# Use parameter.grad = None instead of model.zero_grad()
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Use parameter.grad = None instead of model.zero_grad() or optimizer.zero_grad()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Instead of calling:
 model.zero_grad()
+# or
+optimizer.zero_grad()
 
 ###############################################################################
 # to zero out gradients, use the following method instead:
@@ -96,25 +82,31 @@ for param in model.parameters():
     param.grad = None
 
 ###############################################################################
-# The second code snippet doesn't call memset on each individual parameter, also
-# the subsequent backward pass uses assignment instead of addition assignment to
-# store gradients, this reduces number of memory operations.
+# The second code snippet does not zero the memory of each individual parameter,
+# also the subsequent backward pass uses assignment instead of addition
+# assignment to store gradients, this reduces number of memory operations.
+#
+# Setting gradient to ``None`` has a slightly different numerical behavior than
+# setting it to zero, for more details refer to the
+# `documentation <https://pytorch.org/docs/master/optim.html#torch.optim.Optimizer.zero_grad>`_.
+#
+# Alternatively, starting from PyTorch 1.7, call ``model`` or
+# ``optimizer.zero_grad(set_to_none=True)``.
 
 ###############################################################################
 # Fuse pointwise operations
 # ~~~~~~~~~~~~~~~~~~~~~~~~~
 # Pointwise operations (elementwise addition, multiplication, math functions -
-# ``sin()``, ``cos()``, ``sigmoid()`` etc.) should be fused into a single kernel
+# ``sin()``, ``cos()``, ``sigmoid()`` etc.) can be fused into a single kernel
 # to amortize memory access time and kernel launch time.
 #
 # `PyTorch JIT <https://pytorch.org/docs/stable/jit.html>`_ can fuse kernels
 # automatically, although there could be additional fusion opportunities not yet
 # implemented in the compiler, and not all device types are supported equally.
 #
-# Unfused pointwise operations are memory-bound, for each unfused operation
-# PyTorch launches a separate kernel. Each kernel loads data from the memory,
-# performs computation (this step is usually inexpensive) and stores results
-# back into the memory.
+# Pointwise operations are memory-bound, for each operation PyTorch launches a
+# separate kernel. Each kernel loads data from the memory, performs computation
+# (this step is usually inexpensive) and stores results back into the memory.
 #
 # Fused operator launches only one kernel for multiple fused pointwise ops and
 # loads/stores data only once to the memory. This makes JIT very useful for
@@ -122,65 +114,41 @@ for param in model.parameters():
 #
 # In the simplest case fusion can be enabled by applying
 # `torch.jit.script <https://pytorch.org/docs/stable/generated/torch.jit.script.html#torch.jit.script>`_
-# decorator to the function definition.
+# decorator to the function definition, for example:
+
+@torch.jit.script
+def fused_gelu(x):
+    return x * 0.5 * (1.0 + torch.erf(x / 1.41421))
+
+###############################################################################
 # Refer to
 # `TorchScript documentation <https://pytorch.org/docs/stable/jit.html>`_
 # for more advanced use cases.
 
 ###############################################################################
-# Enable channels_last memory format for computer vision models (experimental)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Enable channels_last memory format for computer vision models
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # PyTorch 1.5 introduced support for `channels_last` memory format for
 # convolutional networks. This format is meant to be used in conjunction with
 # `AMP <https://pytorch.org/docs/stable/amp.html>`_ to further accelerate
 # convolutional neural networks with
 # `Tensor Cores <https://www.nvidia.com/en-us/data-center/tensor-cores/>`_.
 #
-# Support for `channels_last` is new and experimental, but it's expected to work
-# for standard computer vision models (e.g. ResNet-50, SSD). To convert models
-# to `channels_last` format follow
+# Support for `channels_last` is experimental, but it's expected to work for
+# standard computer vision models (e.g. ResNet-50, SSD). To convert models to
+# `channels_last` format follow
 # `Channels Last Memory Format Tutorial <https://pytorch.org/tutorials/intermediate/memory_format_tutorial.html>`_.
 # The tutorial includes a section on
 # `converting existing models <https://pytorch.org/tutorials/intermediate/memory_format_tutorial.html#converting-existing-models>`_.
-
-###############################################################################
-# Parallelize kernel execution
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# When short kernels are executed in serial, the execution efficiency becomes
-# low due to time needed to schedule and launch work on the accelerator.
-# `JIT <https://pytorch.org/docs/stable/jit.html>`_ can help alleviate this cost
-# but its benefit is restricted when inputs are not feedforward between kernels.
-#
-# When kernels do not have input dependency, aggregating the kernels with the
-# same input dimensions and executing work in parallel helps to reduce the
-# number of kernels to execute and increase the resource occupancy within each
-# kernel execution. Good examples to apply kernel vectorization are parameter
-# optimizers and modules with multiple heads (e.g. self-attention).
-
-# Build list of outputs
-out = []
-for block in layer_blocks:
-    out.append(block(x))
-
-# Construct stacked tensor
-out_ = torch.stack(out, dim=0)
-
-# Execute functions that are shared by all blocks in parallel
-out_ = element_wise_layers(out_)
-
-# Re-distribute outputs back to each head
-out = torch.split(out_, dim=0)
-
-# Continue processing layer sequences of each layer
 
 ###############################################################################
 # Checkpoint intermediate buffers
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Buffer checkpointing is a technique to mitigate the memory capacity burden of
 # model training. Instead of storing inputs of all layers to compute upstream
-# gradients in back-prop, it stores the inputs of a few layers and the others
-# are recomputed during backward pass. The reduced memory requirements enables
-# increasing the batch size that can improve utilization.
+# gradients in backward propagation, it stores the inputs of a few layers and
+# the others are recomputed during backward pass. The reduced memory
+# requirements enables increasing the batch size that can improve utilization.
 #
 # Checkpointing targets should be selected carefully. The best is not to store
 # large layer outputs that have small re-computation cost. The example target
@@ -192,29 +160,22 @@ out = torch.split(out_, dim=0)
 # API to automatically perform checkpointing and recomputation.
 
 ###############################################################################
-# Pre-allocate memory in case of variable input length
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Models for speech recognition or for NLP are often trained on input tensors
-# with variable sequence length. Variable length can be problematic for PyTorch
-# caching allocator and can lead to reduced performance or to unexpected
-# out-of-memory errors. If a batch with a short sequence length is followed by
-# an another batch with longer sequence length, then PyTorch is forced to
-# release intermediate buffers from previous iteration and to re-allocate new
-# buffers. This process is time consuming and causes fragmentation in the
-# caching allocator which may result in out-of-memory errors.
+# Disable debugging APIs
+# ~~~~~~~~~~~~~~~~~~~~~~
+# Many PyTorch APIs are intended for debugging and should be disabled for
+# regular training runs:
 #
-# A typical solution is to implement pre-allocation. It consists of the
-# following steps:
-#
-# #. generate a (usually random) batch of inputs with maximum sequence length
-#    (either corresponding to max length in the training dataset or to some
-#    predefined threshold)
-# #. execute a forward and a backward pass with the generated batch, do not
-#    execute an optimizer or a learning rate scheduler, this step pre-allocates
-#    buffers of maximum size, which can be reused in subsequent
-#    training iterations
-# #. zero out gradients
-# #. proceed to regular training
+# * anomaly detection:
+#   `torch.autograd.detect_anomaly <https://pytorch.org/docs/stable/autograd.html#torch.autograd.detect_anomaly>`_
+#   or
+#   `torch.autograd.set_detect_anomaly(True) <https://pytorch.org/docs/stable/autograd.html#torch.autograd.set_detect_anomaly>`_
+# * profiler related:
+#   `torch.autograd.profiler.emit_nvtx <https://pytorch.org/docs/stable/autograd.html#torch.autograd.profiler.emit_nvtx>`_,
+#   `torch.autograd.profiler.profile <https://pytorch.org/docs/stable/autograd.html#torch.autograd.profiler.profile>`_
+# * autograd gradcheck:
+#   `torch.autograd.gradcheck <https://pytorch.org/docs/stable/autograd.html#torch.autograd.gradcheck>`_
+#   or
+#   `torch.autograd.gradgradcheck <https://pytorch.org/docs/stable/autograd.html#torch.autograd.gradgradcheck>`_
 #
 
 ###############################################################################
@@ -225,7 +186,7 @@ out = torch.split(out_, dim=0)
 # Enable cuDNN auto-tuner
 # ~~~~~~~~~~~~~~~~~~~~~~~
 # `NVIDIA cuDNN <https://developer.nvidia.com/cudnn>`_ supports many algorithms
-# to compute convolution. Autotuner runs a short benchmark and selects the
+# to compute a convolution. Autotuner runs a short benchmark and selects the
 # kernel with the best performance on a given hardware for a given input size.
 #
 # For convolutional networks (other types currently not supported), enable cuDNN
@@ -251,10 +212,13 @@ torch.backends.cudnn.benchmark = True
 #
 # When possible, avoid operations which require synchronizations, for example:
 #
-# * ``print(CUDA_tensor)``
-# * ``CUDA_tensor.item()``
-# * memory copies: ``tensor.cuda()``,  ``CUDA_tensor.cpu()`` and equivalent
+# * ``print(cuda_tensor)``
+# * ``cuda_tensor.item()``
+# * memory copies: ``tensor.cuda()``,  ``cuda_tensor.cpu()`` and equivalent
 #   ``tensor.to(device)`` calls
+# * ``cuda_tensor.nonzero()``
+# * python control flow which depends on results of operations performed on cuda
+#   tensors e.g. ``if (cuda_tensor != 0).all()``
 #
 
 ###############################################################################
@@ -301,6 +265,32 @@ torch.backends.cudnn.benchmark = True
 #     `examples <https://pytorch.org/docs/stable/notes/amp_examples.html#amp-examples>`_,
 #     `tutorial <https://pytorch.org/tutorials/recipes/recipes/amp_recipe.html>`_
 #
+#
+
+###############################################################################
+# Pre-allocate memory in case of variable input length
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Models for speech recognition or for NLP are often trained on input tensors
+# with variable sequence length. Variable length can be problematic for PyTorch
+# caching allocator and can lead to reduced performance or to unexpected
+# out-of-memory errors. If a batch with a short sequence length is followed by
+# an another batch with longer sequence length, then PyTorch is forced to
+# release intermediate buffers from previous iteration and to re-allocate new
+# buffers. This process is time consuming and causes fragmentation in the
+# caching allocator which may result in out-of-memory errors.
+#
+# A typical solution is to implement pre-allocation. It consists of the
+# following steps:
+#
+# #. generate a (usually random) batch of inputs with maximum sequence length
+#    (either corresponding to max length in the training dataset or to some
+#    predefined threshold)
+# #. execute a forward and a backward pass with the generated batch, do not
+#    execute an optimizer or a learning rate scheduler, this step pre-allocates
+#    buffers of maximum size, which can be reused in subsequent
+#    training iterations
+# #. zero out gradients
+# #. proceed to regular training
 #
 
 ###############################################################################
