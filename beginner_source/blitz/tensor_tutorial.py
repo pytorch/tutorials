e62ee7c024162b9138e5cb1b3b019bd2d497bb69
suraj813 <suraj813@gmail.com>
2020-09-28 23:21:59 -0400

    Update tensor_tutorial.py

--- beginner_source/blitz/tensor_tutorial.py
+++ beginner_source/blitz/tensor_tutorial.py
@@ -1,195 +1,166 @@
-# -*- coding: utf-8 -*-
 """
-What is PyTorch?
-================
+Tensors - The building blocks of deep learning
+--------------------------------------------
 
-It’s a Python-based scientific computing package targeted at two sets of
-audiences:
+Tensors are a specialized data structure that are very similar to arrays
+and matrices. In PyTorch, we use tensors to encode the inputs and
+outputs of a model, as well as the model’s parameters.
 
--  A replacement for NumPy to use the power of GPUs
--  a deep learning research platform that provides maximum flexibility
-   and speed
+Tensors are similar to NumPy’s ndarrays, except that tensors can run on
+a GPU to accelerate computing. If you’re familiar with ndarrays, you’ll
+be right at home with the Tensor API. If not, follow along in this quick
+API walkthrough.
 
-Getting Started
----------------
-
-Tensors
-^^^^^^^
-
-Tensors are similar to NumPy’s ndarrays, with the addition being that
-Tensors can also be used on a GPU to accelerate computing.
 """
 
-from __future__ import print_function
 import torch
-
-###############################################################
-# .. note::
-#     An uninitialized matrix is declared,
-#     but does not contain definite known
-#     values before it is used. When an
-#     uninitialized matrix is created,
-#     whatever values were in the allocated
-#     memory at the time will appear as the initial values.
-
-###############################################################
-# Construct a 5x3 matrix, uninitialized:
-
-x = torch.empty(5, 3)
-print(x)
- 
-###############################################################
-# Construct a randomly initialized matrix:
-
-x = torch.rand(5, 3)
-print(x)
-
-###############################################################
-# Construct a matrix filled zeros and of dtype long:
-
-x = torch.zeros(5, 3, dtype=torch.long)
-print(x)
-
-###############################################################
-# Construct a tensor directly from data:
-
-x = torch.tensor([5.5, 3])
-print(x)
-
-###############################################################
-# or create a tensor based on an existing tensor. These methods
-# will reuse properties of the input tensor, e.g. dtype, unless
-# new values are provided by user
-
-x = x.new_ones(5, 3, dtype=torch.double)      # new_* methods take in sizes
-print(x)
-
-x = torch.randn_like(x, dtype=torch.float)    # override dtype!
-print(x)                                      # result has the same size
-
-###############################################################
-# Get its size:
-
-print(x.size())
-
-###############################################################
-# .. note::
-#     ``torch.Size`` is in fact a tuple, so it supports all tuple operations.
-#
-# Operations
-# ^^^^^^^^^^
-# There are multiple syntaxes for operations. In the following
-# example, we will take a look at the addition operation.
-#
-# Addition: syntax 1
-y = torch.rand(5, 3)
-print(x + y)
-
-###############################################################
-# Addition: syntax 2
-
-print(torch.add(x, y))
-
-###############################################################
-# Addition: providing an output tensor as argument
-result = torch.empty(5, 3)
-torch.add(x, y, out=result)
-print(result)
-
-###############################################################
-# Addition: in-place
-
-# adds x to y
-y.add_(x)
-print(y)
-
-###############################################################
-# .. note::
-#     Any operation that mutates a tensor in-place is post-fixed with an ``_``.
-#     For example: ``x.copy_(y)``, ``x.t_()``, will change ``x``.
-#
-# You can use standard NumPy-like indexing with all bells and whistles!
-
-print(x[:, 1])
-
-###############################################################
-# Resizing: If you want to resize/reshape tensor, you can use ``torch.view``:
-x = torch.randn(4, 4)
-y = x.view(16)
-z = x.view(-1, 8)  # the size -1 is inferred from other dimensions
-print(x.size(), y.size(), z.size())
-
-###############################################################
-# If you have a one element tensor, use ``.item()`` to get the value as a
-# Python number
-x = torch.randn(1)
-print(x)
-print(x.item())
-
-###############################################################
-# **Read later:**
-#
-#
-#   100+ Tensor operations, including transposing, indexing, slicing,
-#   mathematical operations, linear algebra, random numbers, etc.,
-#   are described
-#   `here <https://pytorch.org/docs/torch>`_.
-#
-# NumPy Bridge
-# ------------
-#
-# Converting a Torch Tensor to a NumPy array and vice versa is a breeze.
-#
-# The Torch Tensor and NumPy array will share their underlying memory
-# locations (if the Torch Tensor is on CPU), and changing one will change
-# the other.
-#
-# Converting a Torch Tensor to a NumPy Array
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-a = torch.ones(5)
-print(a)
-
-###############################################################
-#
-
-b = a.numpy()
-print(b)
-
-###############################################################
-# See how the numpy array changed in value.
-
-a.add_(1)
-print(a)
-print(b)
-
-###############################################################
-# Converting NumPy Array to Torch Tensor
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# See how changing the np array changed the Torch Tensor automatically
-
 import numpy as np
-a = np.ones(5)
-b = torch.from_numpy(a)
-np.add(a, 1, out=a)
-print(a)
-print(b)
+
+
+######################################################################
+# Tensor Initialization
+# ~~~~~~~~~~~~~~~~~~~~~
+#
+# Tensors can be initialized in various ways. Take a look at the following examples
+#
+# **Directly from data / NumPy arrays:**
+#
+
+data = [[1, 2],[3, 4]]
+np_array = np.array(data)
+
+tnsr_from_data = torch.tensor(data)
+tnsr_from_np = torch.from_numpy(np_array)
+
+######################################################################
+# **With random or constant values:**
+#
+
+shape = (2,3,)
+rand_tnsr = torch.rand(shape)
+ones_tnsr = torch.ones(shape)
+zeros_tnsr = torch.zeros(shape)
+print(f"Random Tensor:\n{rand_tnsr}\n\nOnes Tensor:\n{ones_tnsr}\n\nZeros Tensor:\n{zeros_tnsr}")
+
 
 ###############################################################
-# All the Tensors on the CPU except a CharTensor support converting to
-# NumPy and back.
-#
-# CUDA Tensors
-# ------------
-#
-# Tensors can be moved onto any device using the ``.to`` method.
+# **From another tensor:**
+# The new tensor retains the properties of the arg tensor, unless explicitly overridden
 
-# let us run this cell only if CUDA is available
-# We will use ``torch.device`` objects to move tensors in and out of GPU
-if torch.cuda.is_available():
-    device = torch.device("cuda")          # a CUDA device object
-    y = torch.ones_like(x, device=device)  # directly create a tensor on GPU
-    x = x.to(device)                       # or just use strings ``.to("cuda")``
-    z = x + y
-    print(z)
-    print(z.to("cpu", torch.double))       # ``.to`` can also change dtype together!
+new_ones_tnsr = torch.ones_like(tnsr_from_data) # 2 x 2 matrix of ones
+
+try:
+  new_rand_tnsr = torch.rand_like(tnsr_from_data) # 2 x 2 matrix of random numbers
+except RuntimeError as e:
+  print(f"RuntimeError thrown: {e}")
+  print()
+  print(f"Random values in PyTorch are floating points. Datatype passed to torch.rand_like(): {tnsr_from_data.dtype}")
+
+
+######################################################################
+# This works after we override the dtype property
+#
+
+new_rand_tnsr = torch.rand_like(tnsr_from_data, dtype=torch.float)
+print(new_rand_tnsr)
+
+
+
+######################################################################
+# --------------
+#
+
+
+######################################################################
+# Tensor Attributes
+# ~~~~~~~~~~~~~~~~~
+#
+# Tensors have attributes that describe their contents and functions. They
+# also have attributes that autograd uses to keep track of them in the
+# computational graph (more on this in the next section).
+#
+# **Docs Issues** - https://pytorch.org/docs/stable/tensor_attributes.html
+# is not comprehensive (missing data, grad, grad_fn, shape). Contains
+# ``memory_format`` which is not an attribute
+#
+
+tnsr = torch.rand(3,4)
+
+print(f"Data stored in tensor:\n{tnsr.data}\n")
+print(f"Shape of tensor: {tnsr.shape}")
+print(f"Datatype of tensor: {tnsr.dtype}")
+print(f"Device tensor lives on: {tnsr.device}")
+
+print("-------")
+
+# Autograd-related attributes; more on this later
+print(f"Is tensor a leaf on computational graph: {tnsr.is_leaf}")
+print(f"Does tensor require gradients: {tnsr.requires_grad}")
+print(f"Accumulated gradients of tensor: {tnsr.grad}")
+print(f"Function that computed this tensor's gradient: {tnsr.grad_fn}")
+
+
+
+######################################################################
+# --------------
+#
+
+
+######################################################################
+# Tensor Operations
+# ~~~~~~~~~~~~~~~~~
+#
+# Over 100 tensor operations, including transposing, indexing, slicing,
+# mathematical operations, linear algebra, random sampling, and more are
+# comprehensively described
+# `here <https://pytorch.org/docs/stable/torch.html>`__.
+#
+# Each of them can be run on the GPU (at typically higher speeds than on a
+# CPU). If you’re using Colab, allocate a GPU by going to Edit > Notebook
+# Settings.
+#
+
+# We move our tensor to the GPU if available
+if torch.cuda.is_available:
+  tnsr.to('cuda')
+
+
+######################################################################
+# Try out some of the operations from the list. You may come across multiple
+# syntaxes for the same operation. Don’t get confused by the aliases!
+#
+
+###############################################################
+# **Standard numpy-like indexing and slicing:**
+
+tnsr = torch.ones(4, 4)
+tnsr[:,1] = 0
+print(tnsr)
+
+######################################################################
+# **Both of these are joining ops, but they are subtly different.**
+
+t1 = torch.cat([tnsr, tnsr], dim=1)
+t2 = torch.stack([tnsr, tnsr], dim=1)
+print(t1)
+print()
+print(t2)
+
+######################################################################
+# **Multiply op - multiple syntaxes**
+
+# both ops compute element-wise product
+print(tnsr * tnsr == tnsr.mul(tnsr))
+
+# both ops compute matrix product
+print(tnsr @ tnsr.T == tnsr.matmul(tnsr.T))
+
+######################################################################
+# .. note::
+#     Operations that have a '_' suffix are in-place. For example:
+#     ``x.copy_(y)``, ``x.t_()``, will change ``x``. In-place operations
+#     don't work well with Autograd, and their use is discouraged.
+
+print(tnsr)
+tnsr.t_()
+print(tnsr)
