75efe8b2051b1dd394f64655166bc9f7744644bc
suraj813 <suraj813@gmail.com>
2020-10-20 14:35:40 -0400

    Fixes typo

--- beginner_source/blitz/autograd_tutorial.py
+++ beginner_source/blitz/autograd_tutorial.py
@@ -44,7 +44,7 @@ model = torchvision.models.resnet18(pretrained=True)
 data = torch.rand(1, 3, 64, 64)
 labels = torch.rand(1, 1000)
 prediction = model(data) # forward pass
-loss = prediction - labels
+loss = (prediction - labels).sum()
 loss.backward() # backward pass
 
 optim = torch.optim.SGD(model.parameters())
