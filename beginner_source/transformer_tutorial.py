e5f60c64367b2ff3c19e5b73a2c7eac4bf1269f1
albanD <desmaison.alban@gmail.com>
2020-10-27 14:11:45 -0300

    Remove views when not necessary (#1211)
    * Update graph_mode_static_quantization_tutorial.py
    
    * Update transformer_tutorial.py
    
    * Update dynamic_quantization_tutorial.py

--- beginner_source/transformer_tutorial.py
+++ beginner_source/transformer_tutorial.py
@@ -194,7 +194,7 @@ bptt = 35
 def get_batch(source, i):
     seq_len = min(bptt, len(source) - 1 - i)
     data = source[i:i+seq_len]
-    target = source[i+1:i+1+seq_len].view(-1)
+    target = source[i+1:i+1+seq_len].reshape(-1)
     return data, target
 
 
