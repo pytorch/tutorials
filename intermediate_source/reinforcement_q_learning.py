5d37b3553c2e52e770dc8cc50fde7aabcdb22836
Lucien-cs <74787000+Lucien-cs@users.noreply.github.com>
2020-11-21 14:59:00 +0800

    Update reinforcement_q_learning.py
    Correct the Reinforcement Learning (DQN) Tutorial

--- intermediate_source/reinforcement_q_learning.py
+++ intermediate_source/reinforcement_q_learning.py
@@ -25,16 +25,16 @@ an action, the environment *transitions* to a new state, and also
 returns a reward that indicates the consequences of the action. In this
 task, rewards are +1 for every incremental timestep and the environment
 terminates if the pole falls over too far or the cart moves more then 2.4
-units away from center. This means better performing scenarios will run
-for longer duration, accumulating larger return.
+units away from the center. This means better performing scenarios will run
+for a longer duration, accumulating larger return.
 
-The CartPole task is designed so that the inputs to the agent are 4 real
+The CartPole task is designed so that the inputs to the agent are four real
 values representing the environment state (position, velocity, etc.).
 However, neural networks can solve the task purely by looking at the
 scene, so we'll use a patch of the screen centered on the cart as an
 input. Because of this, our results aren't directly comparable to the
 ones from the official leaderboard - our task is much harder.
-Unfortunately this does slow down the training, because we have to
+Unfortunately, this does slow down the training because we have to
 render all the frames.
 
 Strictly speaking, we will present the state as the difference between
