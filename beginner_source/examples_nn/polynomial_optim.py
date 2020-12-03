ce58d5904c04c4be10561447e41a153f573a3f93
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-12-03 06:42:55 -0800

    Replace "Learning PyTorch with Examples" with fitting sine function with a third order polynomial (#1265)
    * Replace tutorial with fitting sine function with third order polynomial
    
    * more
    
    * Save
    
    * save
    
    * save
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * no tensor.data
    
    * fix
    
    * P3
    
    * save
    
    * save
    
    * save
    
    * save
    
    * fix
    
    * fix
    
    * fix

--- beginner_source/examples_nn/polynomial_optim.py
+++ beginner_source/examples_nn/polynomial_optim.py
@@ -3,8 +3,8 @@
 PyTorch: optim
 --------------
 
-A fully-connected ReLU network with one hidden layer, trained to predict y from x
-by minimizing squared Euclidean distance.
+A third order polynomial, trained to predict :math:`y=\sin(x)` from :math:`-\pi`
+to :math:`pi` by minimizing squared Euclidean distance.
 
 This implementation uses the nn package from PyTorch to build the network.
 
@@ -14,32 +14,33 @@ for us. The optim package defines many optimization algorithms that are commonly
 used for deep learning, including SGD+momentum, RMSProp, Adam, etc.
 """
 import torch
+import math
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 1000, 100, 10
 
-# Create random Tensors to hold inputs and outputs
-x = torch.randn(N, D_in)
-y = torch.randn(N, D_out)
+# Create Tensors to hold input and outputs.
+x = torch.linspace(-math.pi, math.pi, 2000)
+y = torch.sin(x)
+
+# Prepare the input tensor (x, x^2, x^3).
+p = torch.tensor([1, 2, 3])
+xx = x.unsqueeze(-1).pow(p)
 
 # Use the nn package to define our model and loss function.
 model = torch.nn.Sequential(
-    torch.nn.Linear(D_in, H),
-    torch.nn.ReLU(),
-    torch.nn.Linear(H, D_out),
+    torch.nn.Linear(3, 1),
+    torch.nn.Flatten(0, 1)
 )
 loss_fn = torch.nn.MSELoss(reduction='sum')
 
 # Use the optim package to define an Optimizer that will update the weights of
-# the model for us. Here we will use Adam; the optim package contains many other
-# optimization algorithms. The first argument to the Adam constructor tells the
+# the model for us. Here we will use RMSprop; the optim package contains many other
+# optimization algorithms. The first argument to the RMSprop constructor tells the
 # optimizer which Tensors it should update.
-learning_rate = 1e-4
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-for t in range(500):
+learning_rate = 1e-3
+optimizer = torch.optim.RMSprop(model.parameters(), lr=learning_rate)
+for t in range(2000):
     # Forward pass: compute predicted y by passing x to the model.
-    y_pred = model(x)
+    y_pred = model(xx)
 
     # Compute and print loss.
     loss = loss_fn(y_pred, y)
@@ -60,3 +61,7 @@ for t in range(500):
     # Calling the step function on an Optimizer makes an update to its
     # parameters
     optimizer.step()
+
+
+linear_layer = model[0]
+print(f'Result: y = {linear_layer.bias.item()} + {linear_layer.weight[:, 0].item()} x + {linear_layer.weight[:, 1].item()} x^2 + {linear_layer.weight[:, 2].item()} x^3')
