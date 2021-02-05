0acb81f5e1bce72ed2b27a8ae1692918c0cca914
suraj813 <5676233+suraj813@users.noreply.github.com>
2021-02-05 11:30:43 -0500

    Update tensor_tutorial.py

--- beginner_source/quickstart/tensor_tutorial.py
+++ beginner_source/quickstart/tensor_tutorial.py
@@ -142,12 +142,16 @@ print(t1)
 # This computes the matrix multiplication between two tensors. y1, y2, y3 will have the same value
 y1 = tensor @ tensor.T
 y2 = tensor.matmul(tensor.T)
+
+y3 = torch.rand_like(tensor)
 torch.matmul(tensor, tensor.T, out=y3)
 
 
 # This computes the element-wise product. z1, z2, z3 will have the same value
 z1 = tensor * tensor
 z2 = tensor.mul(tensor)
+
+z3 = torch.rand_like(tensor)
 torch.mul(tensor, tensor, out=z3)
 
 
