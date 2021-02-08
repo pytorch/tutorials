e9c95fc3a32034c78a5871fb1f6558064b531118
Brian Johnson <brianjo@fb.com>
2021-02-08 17:19:12 -0500

    Update build.sh

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -17,9 +17,9 @@ pip install -r $DIR/../requirements.txt
 
 #Install PyTorch Nightly for test. 
 # Nightly - pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
-# RC Link - Moved to requirements.txt
-# pip uninstall -y torch torchvision torchaudio torchtext
-# pip install -f https://download.pytorch.org/whl/test/cu112/torch_test.html torch torchvision torchaudio torchtext
+# RC Link
+pip uninstall -y torch torchvision torchaudio torchtext
+pip install -f https://download.pytorch.org/whl/test/cu112/torch_test.html torch torchvision torchaudio torchtext
 
 
 # export PATH=/opt/conda/bin:$PATH
