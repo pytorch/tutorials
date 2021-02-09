e6db9a47b59994ade109f9f777cebc6838b0dcf7
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2021-02-09 13:50:39 -0800

    switch to raw text dataset in torchtext 0.9.0 release

--- beginner_source/torchtext_translation.py
+++ beginner_source/torchtext_translation.py
@@ -40,49 +40,41 @@ By the end of this tutorial, you will be able to preprocess sentences into tenso
 
 import torchtext
 import torch
+# With torchtext 0.9.0 rc
+# from torchtext.datasets import Multi30k
+from torchtext.experimental.datasets.raw import Multi30k
 from torchtext.data.utils import get_tokenizer
 from collections import Counter
 from torchtext.vocab import Vocab
-from torchtext.utils import download_from_url, extract_archive
-import io
 
-url_base = 'https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/'
-train_urls = ('train.de.gz', 'train.en.gz')
-val_urls = ('val.de.gz', 'val.en.gz')
-test_urls = ('test_2016_flickr.de.gz', 'test_2016_flickr.en.gz')
+train_iter, = Multi30k(split=('train'))
+de_tokenizer = get_tokenizer('spacy', language='de_core_news_sm')
+en_tokenizer = get_tokenizer('spacy', language='en_core_web_sm')
 
-train_filepaths = [extract_archive(download_from_url(url_base + url))[0] for url in train_urls]
-val_filepaths = [extract_archive(download_from_url(url_base + url))[0] for url in val_urls]
-test_filepaths = [extract_archive(download_from_url(url_base + url))[0] for url in test_urls]
+def build_vocab(train_iter, de_tokenizer, en_tokenizer):
+  de_counter = Counter()
+  en_counter = Counter()
+  for (raw_de, raw_en) in train_iter:
+    de_counter.update(de_tokenizer(raw_de))
+    en_counter.update(en_tokenizer(raw_en))
+  return Vocab(de_counter, specials=['<unk>', '<pad>', '<bos>', '<eos>']), Vocab(en_counter, specials=['<unk>', '<pad>', '<bos>', '<eos>'])
 
-de_tokenizer = get_tokenizer('spacy', language='de')
-en_tokenizer = get_tokenizer('spacy', language='en')
+de_vocab, en_vocab = build_vocab(train_iter, de_tokenizer, en_tokenizer)
 
-def build_vocab(filepath, tokenizer):
-  counter = Counter()
-  with io.open(filepath, encoding="utf8") as f:
-    for string_ in f:
-      counter.update(tokenizer(string_))
-  return Vocab(counter, specials=['<unk>', '<pad>', '<bos>', '<eos>'])
-
-de_vocab = build_vocab(train_filepaths[0], de_tokenizer)
-en_vocab = build_vocab(train_filepaths[1], en_tokenizer)
-
-def data_process(filepaths):
-  raw_de_iter = iter(io.open(filepaths[0], encoding="utf8"))
-  raw_en_iter = iter(io.open(filepaths[1], encoding="utf8"))
+def data_process(data_iter):
   data = []
-  for (raw_de, raw_en) in zip(raw_de_iter, raw_en_iter):
+  for (raw_de, raw_en) in data_iter:
     de_tensor_ = torch.tensor([de_vocab[token] for token in de_tokenizer(raw_de)],
-                            dtype=torch.long)
+                              dtype=torch.long)
     en_tensor_ = torch.tensor([en_vocab[token] for token in en_tokenizer(raw_en)],
-                            dtype=torch.long)
+                              dtype=torch.long)
     data.append((de_tensor_, en_tensor_))
   return data
 
-train_data = data_process(train_filepaths)
-val_data = data_process(val_filepaths)
-test_data = data_process(test_filepaths)
+train_iter, val_iter, test_iter = Multi30k()
+train_data = data_process(train_iter)
+val_data = data_process(val_iter)
+test_data = data_process(test_iter)
 
 ######################################################################
 # ``DataLoader``
