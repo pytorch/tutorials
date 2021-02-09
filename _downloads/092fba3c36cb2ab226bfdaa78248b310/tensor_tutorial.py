d871ff790862207d71004efe3b066cb10c7fba01
pytorchbot <soumith+bot@pytorch.org>
2021-02-09 22:29:12 +0000

    Automated tutorials push

--- _downloads/092fba3c36cb2ab226bfdaa78248b310/tensor_tutorial.py
+++ _downloads/092fba3c36cb2ab226bfdaa78248b310/tensor_tutorial.py
@@ -13,8 +13,8 @@ API walkthrough.
 
 """
 
-import torch
-import numpy as np
+
+
 
 
 ######################################################################
@@ -27,15 +27,15 @@ import numpy as np
 #
 # Tensors can be created directly from data. The data type is automatically inferred.
 
-data = [[1, 2],[3, 4]]
-x_data = torch.tensor(data)
+
+
 
 ######################################################################
 # **From a NumPy array**
 #
 # Tensors can be created from NumPy arrays (and vice versa - see :ref:`bridge-to-np-label`).
-np_array = np.array(data)
-x_np = torch.from_numpy(np_array)
+
+
 
 
 ###############################################################
@@ -43,11 +43,11 @@ x_np = torch.from_numpy(np_array)
 #
 # The new tensor retains the properties (shape, datatype) of the argument tensor, unless explicitly overridden.
 
-x_ones = torch.ones_like(x_data) # retains the properties of x_data
-print(f"Ones Tensor: \n {x_ones} \n")
 
-x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
-print(f"Random Tensor: \n {x_rand} \n")
+
+
+
+
 
 
 ######################################################################
@@ -55,14 +55,14 @@ print(f"Random Tensor: \n {x_rand} \n")
 #
 # ``shape`` is a tuple of tensor dimensions. In the functions below, it determines the dimensionality of the output tensor.
 
-shape = (2,3,)
-rand_tensor = torch.rand(shape)
-ones_tensor = torch.ones(shape)
-zeros_tensor = torch.zeros(shape)
 
-print(f"Random Tensor: \n {rand_tensor} \n")
-print(f"Ones Tensor: \n {ones_tensor} \n")
-print(f"Zeros Tensor: \n {zeros_tensor}")
+
+
+
+
+
+
+
 
 
 
@@ -78,11 +78,11 @@ print(f"Zeros Tensor: \n {zeros_tensor}")
 #
 # Tensor attributes describe their shape, datatype, and the device on which they are stored.
 
-tensor = torch.rand(3,4)
 
-print(f"Shape of tensor: {tensor.shape}")
-print(f"Datatype of tensor: {tensor.dtype}")
-print(f"Device tensor is stored on: {tensor.device}")
+
+
+
+
 
 
 ######################################################################
@@ -105,8 +105,8 @@ print(f"Device tensor is stored on: {tensor.device}")
 #
 
 # We move our tensor to the GPU if available
-if torch.cuda.is_available():
-  tensor = tensor.to('cuda')
+
+
 
 
 ######################################################################
@@ -117,40 +117,40 @@ if torch.cuda.is_available():
 ###############################################################
 # **Standard numpy-like indexing and slicing:**
 
-tensor = torch.ones(4, 4)
-tensor[:,1] = 0
-print(tensor)
+
+
+
 
 ######################################################################
 # **Joining tensors** You can use ``torch.cat`` to concatenate a sequence of tensors along a given dimension.
 # See also `torch.stack <https://pytorch.org/docs/stable/generated/torch.stack.html>`__,
 # another tensor joining op that is subtly different from ``torch.cat``.
-t1 = torch.cat([tensor, tensor, tensor], dim=1)
-print(t1)
+
+
 
 ######################################################################
 # **Multiplying tensors**
 
 # This computes the element-wise product
-print(f"tensor.mul(tensor) \n {tensor.mul(tensor)} \n")
+
 # Alternative syntax:
-print(f"tensor * tensor \n {tensor * tensor}")
+
 
 ######################################################################
 #
 # This computes the matrix multiplication between two tensors
-print(f"tensor.matmul(tensor.T) \n {tensor.matmul(tensor.T)} \n")
+
 # Alternative syntax:
-print(f"tensor @ tensor.T \n {tensor @ tensor.T}")
+
 
 
 ######################################################################
 # **In-place operations**
 # Operations that have a ``_`` suffix are in-place. For example: ``x.copy_(y)``, ``x.t_()``, will change ``x``.
 
-print(tensor, "\n")
-tensor.add_(5)
-print(tensor)
+
+
+
 
 ######################################################################
 # .. note::
@@ -174,27 +174,29 @@ print(tensor)
 ######################################################################
 # Tensor to NumPy array
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-t = torch.ones(5)
-print(f"t: {t}")
-n = t.numpy()
-print(f"n: {n}")
+
+
+
+
 
 ######################################################################
 # A change in the tensor reflects in the NumPy array.
 
-t.add_(1)
-print(f"t: {t}")
-print(f"n: {n}")
+
+
+
 
 
 ######################################################################
 # NumPy array to Tensor
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-n = np.ones(5)
-t = torch.from_numpy(n)
+
+
 
 ######################################################################
 # Changes in the NumPy array reflects in the tensor.
-np.add(n, 1, out=n)
-print(f"t: {t}")
-print(f"n: {n}")
+
+
+
+
+# %%%%%%RUNNABLE_CODE_REMOVED%%%%%%
