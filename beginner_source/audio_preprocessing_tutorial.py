5abd05e5c4e7f2de324a44b0887913a817232363
moto <855818+mthrok@users.noreply.github.com>
2021-02-28 00:15:28 -0500

    Update

--- beginner_source/audio_preprocessing_tutorial.py
+++ beginner_source/audio_preprocessing_tutorial.py
@@ -36,6 +36,7 @@ print(torchaudio.__version__)
 #-------------------------------------------------------------------------------
 import io
 import os
+import math
 import tarfile
 import multiprocessing
 
@@ -50,37 +51,45 @@ import matplotlib.pyplot as plt
 from IPython.display import Audio, display
 
 [width, height] = matplotlib.rcParams['figure.figsize']
-matplotlib.rcParams['figure.figsize'] = [width * 2.7, height]
+if width < 10:
+  matplotlib.rcParams['figure.figsize'] = [width * 2.5, height]
 
+_SAMPLE_DIR = "_sample_data"
 SAMPLE_WAV_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/steam-train-whistle-daniel_simon.wav"
-SAMPLE_WAV_PATH = "_sample.wav"
+SAMPLE_WAV_PATH = os.path.join(_SAMPLE_DIR, "steam.wav")
 
 SAMPLE_WAV_SPEECH_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/VOiCES_devkit/source-16k/train/sp0307/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav"
-SAMPLE_WAV_SPEECH_PATH = "_sample_speech.wav"
+SAMPLE_WAV_SPEECH_PATH = os.path.join(_SAMPLE_DIR, "speech.wav")
+
+SAMPLE_RIR_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/VOiCES_devkit/distant-16k/room-response/rm1/impulse/Lab41-SRI-VOiCES-rm1-impulse-mc01-stu-clo.wav"
+SAMPLE_RIR_PATH = os.path.join(_SAMPLE_DIR, "rir.wav")
+
+SAMPLE_NOISE_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/VOiCES_devkit/distant-16k/distractors/rm1/babb/Lab41-SRI-VOiCES-rm1-babb-mc01-stu-clo.wav"
+SAMPLE_NOISE_PATH = os.path.join(_SAMPLE_DIR, "bg.wav")
 
 SAMPLE_MP3_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/steam-train-whistle-daniel_simon.mp3"
-SAMPLE_MP3_PATH = "_sample.mp3"
+SAMPLE_MP3_PATH = os.path.join(_SAMPLE_DIR, "steam.mp3")
 
 SAMPLE_GSM_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/steam-train-whistle-daniel_simon.gsm"
-SAMPLE_GSM_PATH = "_sample.gsm"
+SAMPLE_GSM_PATH = os.path.join(_SAMPLE_DIR, "steam.gsm")
 
 SAMPLE_TAR_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/VOiCES_devkit.tar.gz"
-SAMPLE_TAR_PATH = "_sample.tar.gz"
+SAMPLE_TAR_PATH = os.path.join(_SAMPLE_DIR, "sample.tar.gz")
 SAMPLE_TAR_ITEM = "VOiCES_devkit/source-16k/train/sp0307/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav"
 
 S3_BUCKET = "pytorch-tutorial-assets"
 S3_KEY = "VOiCES_devkit/source-16k/train/sp0307/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav"
 
-YESNO_DATASET_PATH = "./_dataset/yes_no"
-LJSPEECH_DATASET_PATH = "./_dataset/ljspeech"
-
+YESNO_DATASET_PATH = os.path.join(_SAMPLE_DIR, "yes_no")
 os.makedirs(YESNO_DATASET_PATH, exist_ok=True)
-os.makedirs(LJSPEECH_DATASET_PATH, exist_ok=True)
+os.makedirs(_SAMPLE_DIR, exist_ok=True)
 
 def _fetch_data():
   uri = [
     (SAMPLE_WAV_URL, SAMPLE_WAV_PATH),
     (SAMPLE_WAV_SPEECH_URL, SAMPLE_WAV_SPEECH_PATH),
+    (SAMPLE_RIR_URL, SAMPLE_RIR_PATH),
+    (SAMPLE_NOISE_URL, SAMPLE_NOISE_PATH),
     (SAMPLE_MP3_URL, SAMPLE_MP3_PATH),
     (SAMPLE_GSM_URL, SAMPLE_GSM_PATH),
     (SAMPLE_TAR_URL, SAMPLE_TAR_PATH),
@@ -89,48 +98,41 @@ def _fetch_data():
     with open(path, 'wb') as file_:
       file_.write(requests.get(url).content)
 
-
 _fetch_data()
-
 
 def _download_yesno():
   if os.path.exists(os.path.join(YESNO_DATASET_PATH, "waves_yesno.tar.gz")):
     return
   torchaudio.datasets.YESNO(root=YESNO_DATASET_PATH, download=True)
 
-
-def _download_ljspeech():
-  if os.path.exists(os.path.join(LJSPEECH_DATASET_PATH, "LJSpeech-1.1.tar.bz2")):
-    return
-  torchaudio.datasets.LJSPEECH(root=LJSPEECH_DATASET_PATH, download=True)
-
-
 YESNO_DOWNLOAD_PROCESS = multiprocessing.Process(target=_download_yesno)
 YESNO_DOWNLOAD_PROCESS.start()
 
-
-LJSPEECH_DOWNLOAD_PROCESS = multiprocessing.Process(target=_download_ljspeech)
-LJSPEECH_DOWNLOAD_PROCESS.start()
-
-
 def _get_sample(path, resample=None):
+  effects = [
+    ["remix", "1"]
+  ]
   if resample:
-    waveform, sample_rate = torchaudio.sox_effects.apply_effects_file(
-        path, effects=[["rate", f'{resample}']])
-  else:
-    waveform, sample_rate = torchaudio.load(path)
-  waveform = waveform[:1, ...]
-  return waveform, sample_rate
-
+    effects.append(["rate", f'{resample}'])
+  return torchaudio.sox_effects.apply_effects_file(path, effects=effects)
 
 def get_speech_sample(*, resample=None):
   return _get_sample(SAMPLE_WAV_SPEECH_PATH, resample=resample)
 
-
 def get_sample(*, resample=None):
   return _get_sample(SAMPLE_WAV_PATH, resample=resample)
 
+def get_rir_sample(*, resample=None, processed=False):
+  rir_raw, sample_rate = _get_sample(SAMPLE_RIR_PATH, resample=resample)
+  if not processed:
+    return rir_raw, sample_rate
+  rir = rir_raw[:, int(sample_rate*1.01):int(sample_rate*1.3)]
+  rir = rir / torch.norm(rir, p=2)
+  rir = torch.flip(rir, [1])
+  return rir, sample_rate
 
+def get_noise_sample(*, resample=None):
+  return _get_sample(SAMPLE_NOISE_PATH, resample=resample)
 
 def print_metadata(metadata, src=None):
   if src:
@@ -143,7 +145,6 @@ def print_metadata(metadata, src=None):
   print(" - bits_per_sample:", metadata.bits_per_sample)
   print(" - encoding:", metadata.encoding)
   print()
-
 
 def print_stats(waveform, sample_rate=None, src=None):
   if src:
@@ -162,8 +163,7 @@ def print_stats(waveform, sample_rate=None, src=None):
   print(waveform)
   print()
 
-
-def plot_waveform(waveform, sample_rate, title="waveform"):
+def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
   waveform = waveform.numpy()
 
   num_channels, num_frames = waveform.shape
@@ -175,14 +175,16 @@ def plot_waveform(waveform, sample_rate, title="waveform"):
   for c in range(num_channels):
     axes[c].plot(time_axis, waveform[c], linewidth=1)
     axes[c].grid(True)
-    axes[c].set_ylim((-1.0, 1.0))
-    axes[c].set_ylabel(f'Channel {c+1}')
+    if num_channels > 1:
+      axes[c].set_ylabel(f'Channel {c+1}')
+    if xlim:
+      axes[c].set_xlim(xlim)
+    if ylim:
+      axes[c].set_ylim(ylim)
   figure.suptitle(title)
   plt.show(block=False)
-  return figure, axes
 
-
-def plot_specgram(waveform, sample_rate, title=None, n_fft=256):
+def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None):
   waveform = waveform.numpy()
 
   num_channels, num_frames = waveform.shape
@@ -192,11 +194,13 @@ def plot_specgram(waveform, sample_rate, title=None, n_fft=256):
   if num_channels == 1:
     axes = [axes]
   for c in range(num_channels):
-    axes[c].specgram(waveform[c], Fs=sample_rate, NFFT=n_fft)
-    axes[c].set_ylabel(f'Channel {c+1}')
-  figure.suptitle(title or f"Spectrogram: #FFT bins: {n_fft}")
+    axes[c].specgram(waveform[c], Fs=sample_rate)
+    if num_channels > 1:
+      axes[c].set_ylabel(f'Channel {c+1}')
+    if xlim:
+      axes[c].set_xlim(xlim)
+  figure.suptitle(title)
   plt.show(block=False)
-
 
 def play_audio(waveform, sample_rate):
   waveform = waveform.numpy()
@@ -209,7 +213,6 @@ def play_audio(waveform, sample_rate):
   else:
     raise ValueError("Waveform with more than 2 channels are not supported.")
 
-
 def inspect_file(path):
   print("-" * 10)
   print("Source:", path)
@@ -217,13 +220,14 @@ def inspect_file(path):
   print(f" - File size: {os.path.getsize(path)} bytes")
   print_metadata(torchaudio.info(path))
 
-
-def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto'):
+def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto', xmax=None):
   fig, axs = plt.subplots(1, 1)
   axs.set_title(title or 'Spectrogram (db)')
   axs.set_ylabel(ylabel)
   axs.set_xlabel('frame')
   im = axs.imshow(librosa.power_to_db(spec), origin='lower', aspect=aspect)
+  if xmax:
+    axs.set_xlim((0, xmax))
   fig.colorbar(im, ax=axs)
   plt.show(block=False)
 
@@ -252,7 +256,6 @@ def get_spectrogram(
   )
   return spectrogram(waveform)
 
-
 def plot_pitch(waveform, sample_rate, pitch):
   figure, axis = plt.subplots(1, 1)
   axis.set_title("Pitch Feature")
@@ -262,10 +265,6 @@ def plot_pitch(waveform, sample_rate, pitch):
   time_axis = torch.linspace(0, end_time,  waveform.shape[1])
   axis.plot(time_axis, waveform[0], linewidth=1, color='gray', alpha=0.3)
 
-  # time_axis = torch.linspace(0, end_time, pitch.shape[1])
-  # ln1 = axis.plot(time_axis, pitch[0], linewidth=2, label='Pitch', color='green')
-  # axis.set_ylim((-1.3, 1.3))
-
   axis2 = axis.twinx()
   time_axis = torch.linspace(0, end_time, pitch.shape[1])
   ln2 = axis2.plot(
@@ -273,7 +272,6 @@ def plot_pitch(waveform, sample_rate, pitch):
 
   axis2.legend(loc=0)
   plt.show(block=False)
-
 
 def plot_kaldi_pitch(waveform, sample_rate, pitch, nfcc):
   figure, axis = plt.subplots(1, 1)
@@ -643,8 +641,8 @@ effects = [
 waveform2, sample_rate2 = torchaudio.sox_effects.apply_effects_tensor(
     waveform1, sample_rate1, effects)
 
-plot_waveform(waveform1, sample_rate1, title="Original")
-plot_waveform(waveform2, sample_rate2, title="Effects Applied")
+plot_waveform(waveform1, sample_rate1, title="Original", xlim=(-.1, 3.2))
+plot_waveform(waveform2, sample_rate2, title="Effects Applied", xlim=(-.1, 3.2))
 print_stats(waveform1, sample_rate=sample_rate1, src="Original")
 print_stats(waveform2, sample_rate=sample_rate2, src="Effects Applied")
 
@@ -656,10 +654,112 @@ print_stats(waveform2, sample_rate=sample_rate2, src="Effects Applied")
 # sound more dramatic?
 # 
 
-plot_specgram(waveform1, sample_rate1, title="Original")
+plot_specgram(waveform1, sample_rate1, title="Original", xlim=(0, 3.04))
 play_audio(waveform1, sample_rate1)
-plot_specgram(waveform2, sample_rate2, title="Effects Applied")
+plot_specgram(waveform2, sample_rate2, title="Effects Applied", xlim=(0, 3.04))
 play_audio(waveform2, sample_rate2)
+
+
+######################################################################
+# Simulating room reverbration
+# ----------------------------
+# 
+# `Convolution
+# reverb <https://en.wikipedia.org/wiki/Convolution_reverb>`__ is a
+# technique used to make a clean audio data sound like in a different
+# environment.
+# 
+# Using Room Impulse Response (RIR), we can make a clean speech sound like
+# uttered in a conference room.
+# 
+# For this process, we need RIR data. The following data are from VOiCES
+# dataset, but you can record one by your self. Just turn on microphone
+# and clap you hands.
+# 
+
+sample_rate = 8000
+
+rir_raw, _ = get_rir_sample(resample=sample_rate)
+
+plot_waveform(rir_raw, sample_rate, title="Room Impulse Response (raw)", ylim=None)
+plot_specgram(rir_raw, sample_rate, title="Room Impulse Response (raw)")
+play_audio(rir_raw, sample_rate)
+
+
+######################################################################
+# First, we need to clean up the RIR. We extract the main impulse,
+# normalize the signal power, then flip the time axis.
+# 
+
+rir = rir_raw[:, int(sample_rate*1.01):int(sample_rate*1.3)]
+rir = rir / torch.norm(rir, p=2)
+rir = torch.flip(rir, [1])
+
+print_stats(rir)
+plot_waveform(rir, sample_rate, title="Room Impulse Response", ylim=None)
+
+
+
+######################################################################
+# Then we convolve the speech signal with the RIR filter.
+# 
+
+speech, _ = get_speech_sample(resample=sample_rate)
+
+speech_ = torch.nn.functional.pad(speech, (rir.shape[1]-1, 0))
+augmented = torch.nn.functional.conv1d(speech_[None, ...], rir[None, ...])[0]
+
+plot_waveform(speech, sample_rate, title="Original", ylim=None)
+plot_waveform(augmented, sample_rate, title="RIR Applied", ylim=None)
+
+plot_specgram(speech, sample_rate, title="Original")
+play_audio(speech, sample_rate)
+
+plot_specgram(augmented, sample_rate, title="RIR Applied")
+play_audio(augmented, sample_rate)
+
+
+######################################################################
+# Adding background noise
+# -----------------------
+# 
+# To add background noise to audio data, you can simply add audio Tensor
+# and noise Tensor. A commonly way to adjust the intensity of noise is to
+# change Signal-to-Noise Ratio (SNR).
+# [`wikipedia <https://en.wikipedia.org/wiki/Signal-to-noise_ratio>`__]
+# 
+# .. math::
+# 
+# 
+#    \mathrm{SNR} = \frac{P_\mathrm{signal}}{P_\mathrm{noise}}
+# 
+# .. math::
+# 
+# 
+#    {\mathrm  {SNR_{{dB}}}}=10\log _{{10}}\left({\mathrm  {SNR}}\right)
+# 
+
+sample_rate = 8000
+speech, _ = get_speech_sample(resample=sample_rate)
+noise, _ = get_noise_sample(resample=sample_rate)
+noise = noise[:, :speech.shape[1]]
+
+plot_waveform(noise, sample_rate, title="Background noise")
+plot_specgram(noise, sample_rate, title="Background noise")
+play_audio(noise, sample_rate)
+
+speech_power = speech.norm(p=2)
+noise_power = noise.norm(p=2)
+
+for snr_db in [20, 10, 3]:
+  snr = math.exp(snr_db / 10)
+  scale = snr * noise_power / speech_power
+  noisy_speech = (scale * speech + noise) / 2
+
+  plot_waveform(noisy_speech, sample_rate, title=f"SNR: {snr_db} [dB]")
+  plot_specgram(noisy_speech, sample_rate, title=f"SNR: {snr_db} [dB]")
+  play_audio(noisy_speech, sample_rate)
+
 
 
 ######################################################################
@@ -678,9 +778,9 @@ play_audio(waveform, sample_rate)
 
 configs = [
     ({"format": "wav", "encoding": 'ULAW', "bits_per_sample": 8}, "8 bit mu-law"),
-    ({"format": "mp3", "compression": -9}, "MP3 VBR lowest quality"),
-    ({"format": "vorbis", "compression": -1}, "Vorbis lowest quality"),
-    ({"format": "gsm"}, "GSM"),
+    ({"format": "gsm"}, "GSM-FR"),
+    ({"format": "mp3", "compression": -9}, "MP3"),
+    ({"format": "vorbis", "compression": -1}, "Vorbis"),
 ]
 for param, title in configs:
   augmented = F.apply_codec(waveform, sample_rate, **param)
@@ -690,46 +790,61 @@ for param, title in configs:
 
 
 ######################################################################
-# Simulating telephony data
-# -------------------------
+# Simulating a phone recoding
+# ---------------------------
 # 
-# Combining the previous techniques, we can create noisy data.
+# Combining the previous techniques, we can simulate audio that sounds
+# like a person talking over a phone in a echoey room with people talking
+# in the background.
 # 
-# Let’s simulate telephone communication.
-# 
 
-waveform, sample_rate = get_speech_sample()
+sample_rate = 16000
+speech, _ = get_speech_sample(resample=sample_rate)
 
-plot_specgram(waveform, sample_rate, title="Original")
-play_audio(waveform, sample_rate)
+plot_specgram(speech, sample_rate, title="Original")
+play_audio(speech, sample_rate)
 
-# Add background noise (white noise)
-bg = 0.1 * torch.randn(waveform.shape)
-ratio = 0.1
-waveform = ratio * bg + (1.0 - ratio) * waveform
+# Apply RIR
+rir, _ = get_rir_sample(resample=sample_rate, processed=True)
+speech_ = torch.nn.functional.pad(speech, (rir.shape[1]-1, 0))
+speech = torch.nn.functional.conv1d(speech_[None, ...], rir[None, ...])[0]
 
-plot_specgram(waveform, sample_rate, title="Background noise added")
-play_audio(waveform, sample_rate)
+plot_specgram(speech, sample_rate, title="RIR Applied")
+play_audio(speech, sample_rate)
+
+# Add background noise
+# Because the noise is recorded in the actual environment, we consider that 
+# the noise contains the acoustic feature of the environment. Therefore, we add
+# the noise after RIR application.
+noise, _ = get_noise_sample(resample=sample_rate)
+noise = noise[:, :speech.shape[1]]
+
+snr_db = 8
+scale = math.exp(snr_db / 10) * noise.norm(p=2) / speech.norm(p=2)
+speech = (scale * speech + noise) / 2
+
+plot_specgram(speech, sample_rate, title="BG noise added")
+play_audio(speech, sample_rate)
 
 # Apply filtering and change sample rate
-waveform, sample_rate = torchaudio.sox_effects.apply_effects_tensor(
-  waveform,
+speech, sample_rate = torchaudio.sox_effects.apply_effects_tensor(
+  speech,
   sample_rate,
   effects=[
+      ["lowpass", "4000"],
       ["compand", "0.02,0.05", "-60,-60,-30,-10,-20,-8,-5,-8,-2,-8", "-8", "-7", "0.05"],
       ["rate", "8000"],
-      ["remix", "1"],
   ],
 )
 
-plot_specgram(waveform, sample_rate, title="Filtered")
-play_audio(waveform, sample_rate)
+plot_specgram(speech, sample_rate, title="Filtered")
+play_audio(speech, sample_rate)
 
 # Apply telephony codec
-waveform = F.apply_codec(waveform, sample_rate, format="gsm")
+speech = F.apply_codec(speech, sample_rate, format="gsm")
 
-plot_specgram(waveform, sample_rate, title="Codec Applied")
-play_audio(waveform, sample_rate)
+plot_specgram(speech, sample_rate, title="GSM Codec Applied")
+play_audio(speech, sample_rate)
 
 
 
@@ -1054,13 +1169,13 @@ strech = T.TimeStretch()
 
 rate = 1.2
 spec_ = strech(spec, rate)
-plot_spectrogram(F.complex_norm(spec_[0]), title=f"Stretched x{rate}", aspect='equal')
+plot_spectrogram(F.complex_norm(spec_[0]), title=f"Stretched x{rate}", aspect='equal', xmax=304)
 
-plot_spectrogram(F.complex_norm(spec[0]), title="Original", aspect='equal')
+plot_spectrogram(F.complex_norm(spec[0]), title="Original", aspect='equal', xmax=304)
 
 rate = 0.9
 spec_ = strech(spec, rate)
-plot_spectrogram(F.complex_norm(spec_[0]), title=f"Stretched x{rate}", aspect='equal')
+plot_spectrogram(F.complex_norm(spec_[0]), title=f"Stretched x{rate}", aspect='equal', xmax=304)
 
 
 ######################################################################
