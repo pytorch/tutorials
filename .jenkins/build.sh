c90d58176ae97bb610a5c5eac72b8fa54b296104
Brian Johnson <brianjo@fb.com>
2021-03-08 12:38:27 -0500

    Update build.sh
    Update build to nightly.

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -23,10 +23,8 @@ pip install -r $DIR/../requirements.txt
 # pip install sphinx==1.8.2 pandas
 
 #Install PyTorch Nightly for test. 
-# Nightly - pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
-# RC Link
-# pip uninstall -y torch torchvision torchaudio torchtext
-# pip install -f https://download.pytorch.org/whl/test/cu102/torch_test.html torch torchvision torchaudio torchtext
+pip uninstall -y torch torchvision torchaudio torchtext
+pip install --pre torch torchvision torchaudio torchtext -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
 
 # For Tensorboard. Until 1.14 moves to the release channel.
 pip install tb-nightly
