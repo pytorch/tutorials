374fb5032b011363c15dce675580afd07378bedd
pytorchbot <soumith+bot@pytorch.org>
2021-03-15 16:56:45 +0000

    Automated tutorials push

--- _downloads/c9aee5c8955d797c051f02c07927b0c0/fgsm_tutorial.py
+++ _downloads/c9aee5c8955d797c051f02c07927b0c0/fgsm_tutorial.py
@@ -90,21 +90,21 @@ first and most popular attack methods, the Fast Gradient Sign Attack
 # into the implementation.
 # 
 
-from __future__ import print_function
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-import numpy as np
-import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
 
 # NOTE: This is a hack to get around "User-agent" limitations when downloading MNIST datasets
 #       see, https://github.com/pytorch/vision/issues/3497 for more information
-from six.moves import urllib
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-urllib.request.install_opener(opener)
+
+
+
+
 
 
 ######################################################################
@@ -138,9 +138,9 @@ urllib.request.install_opener(opener)
 #    not take much time.
 # 
 
-epsilons = [0, .05, .1, .15, .2, .25, .3]
-pretrained_model = "data/lenet_mnist_model.pth"
-use_cuda=True
+
+
+
 
 
 ######################################################################
@@ -157,43 +157,43 @@ use_cuda=True
 # 
 
 # LeNet Model definition
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # MNIST Test dataset and dataloader declaration
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, download=True, transform=transforms.Compose([
-            transforms.ToTensor(),
-            ])), 
-        batch_size=1, shuffle=True)
+
+
+
+
+
 
 # Define what device we are using
-print("CUDA Available: ",torch.cuda.is_available())
-device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
+
+
 
 # Initialize the network
-model = Net().to(device)
+
 
 # Load the pretrained model
-model.load_state_dict(torch.load(pretrained_model, map_location='cpu'))
+
 
 # Set the model in evaluation mode. In this case this is for the Dropout layers
-model.eval()
+
 
 
 ######################################################################
@@ -215,15 +215,15 @@ model.eval()
 # 
 
 # FGSM attack code
-def fgsm_attack(image, epsilon, data_grad):
-    # Collect the element-wise sign of the data gradient
-    sign_data_grad = data_grad.sign()
-    # Create the perturbed image by adjusting each pixel of the input image
-    perturbed_image = image + epsilon*sign_data_grad
-    # Adding clipping to maintain [0,1] range
-    perturbed_image = torch.clamp(perturbed_image, 0, 1)
-    # Return the perturbed image
-    return perturbed_image
+
+
+
+
+
+
+
+
+
 
 
 ######################################################################
@@ -244,67 +244,67 @@ def fgsm_attack(image, epsilon, data_grad):
 # successful adversarial examples to be visualized later.
 # 
 
-def test( model, device, test_loader, epsilon ):
 
-    # Accuracy counter
-    correct = 0
-    adv_examples = []
 
-    # Loop over all examples in test set
-    for data, target in test_loader:
 
-        # Send the data and label to the device
-        data, target = data.to(device), target.to(device)
 
-        # Set requires_grad attribute of tensor. Important for Attack
-        data.requires_grad = True
 
-        # Forward pass the data through the model
-        output = model(data)
-        init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
 
-        # If the initial prediction is wrong, dont bother attacking, just move on
-        if init_pred.item() != target.item():
-            continue
 
-        # Calculate the loss
-        loss = F.nll_loss(output, target)
 
-        # Zero all existing gradients
-        model.zero_grad()
 
-        # Calculate gradients of model in backward pass
-        loss.backward()
 
-        # Collect datagrad
-        data_grad = data.grad.data
 
-        # Call FGSM Attack
-        perturbed_data = fgsm_attack(data, epsilon, data_grad)
 
-        # Re-classify the perturbed image
-        output = model(perturbed_data)
 
-        # Check for success
-        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-        if final_pred.item() == target.item():
-            correct += 1
-            # Special case for saving 0 epsilon examples
-            if (epsilon == 0) and (len(adv_examples) < 5):
-                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
-        else:
-            # Save some adv examples for visualization later
-            if len(adv_examples) < 5:
-                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
 
-    # Calculate final accuracy for this epsilon
-    final_acc = correct/float(len(test_loader))
-    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
 
-    # Return the accuracy and an adversarial example
-    return final_acc, adv_examples
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ######################################################################
@@ -320,14 +320,14 @@ def test( model, device, test_loader, epsilon ):
 # with no attack.
 # 
 
-accuracies = []
-examples = []
+
+
 
 # Run test for each epsilon
-for eps in epsilons:
-    acc, ex = test(model, device, test_loader, eps)
-    accuracies.append(acc)
-    examples.append(ex)
+
+
+
+
 
 
 ######################################################################
@@ -349,14 +349,14 @@ for eps in epsilons:
 # :math:`\epsilon=0.25` and :math:`\epsilon=0.3`.
 # 
 
-plt.figure(figsize=(5,5))
-plt.plot(epsilons, accuracies, "*-")
-plt.yticks(np.arange(0, 1.1, step=0.1))
-plt.xticks(np.arange(0, .35, step=0.05))
-plt.title("Accuracy vs Epsilon")
-plt.xlabel("Epsilon")
-plt.ylabel("Accuracy")
-plt.show()
+
+
+
+
+
+
+
+
 
 
 ######################################################################
@@ -378,21 +378,21 @@ plt.show()
 # 
 
 # Plot several examples of adversarial samples at each epsilon
-cnt = 0
-plt.figure(figsize=(8,10))
-for i in range(len(epsilons)):
-    for j in range(len(examples[i])):
-        cnt += 1
-        plt.subplot(len(epsilons),len(examples[0]),cnt)
-        plt.xticks([], [])
-        plt.yticks([], [])
-        if j == 0:
-            plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=14)
-        orig,adv,ex = examples[i][j]
-        plt.title("{} -> {}".format(orig, adv))
-        plt.imshow(ex, cmap="gray")
-plt.tight_layout()
-plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ######################################################################
@@ -420,3 +420,5 @@ plt.show()
 # it differs from FGSM. Then, try to defend the model from your own
 # attacks.
 # 
+
+# %%%%%%RUNNABLE_CODE_REMOVED%%%%%%
