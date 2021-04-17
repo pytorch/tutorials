fce4e45f05b6a7c6984086332aa6da755580bb44
pytorchbot <soumith+bot@pytorch.org>
2021-04-17 03:16:32 +0000

    Automated tutorials push

--- _downloads/ba100c1433c3c42a16709bb6a2ed0f85/cifar10_tutorial.py
+++ _downloads/ba100c1433c3c42a16709bb6a2ed0f85/cifar10_tutorial.py
@@ -43,15 +43,15 @@ Training an image classifier
 
 We will do the following steps in order:
 
-1. Load and normalizing the CIFAR10 training and test datasets using
+1. Load and normalize the CIFAR10 training and test datasets using
    ``torchvision``
 2. Define a Convolutional Neural Network
 3. Define a loss function
 4. Train the network on the training data
 5. Test the network on the test data
 
-1. Loading and normalizing CIFAR10
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+1. Load and normalize CIFAR10
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 Using ``torchvision``, it’s extremely easy to load CIFAR10.
 """
