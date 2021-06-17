37d62232aebc163c04855720b9fab7a77ae66de3
Arda Aytekin <aytekinar@mailbox.org>
2021-06-17 13:13:11 +0200

    Correction on the use of "epoch"
    Made the switch from "epoch" to "mini-batches" to clarify the training process.

--- beginner_source/basics/quickstart_tutorial.py
+++ beginner_source/basics/quickstart_tutorial.py
@@ -173,13 +173,14 @@ def test(dataloader, model):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 ##############################################################################
-# The training process is conducted over several iterations (*epochs*). During each epoch, the model learns 
-# parameters to make better predictions. We print the model's accuracy and loss at each epoch; we'd like to see the
-# accuracy increase and the loss decrease with every epoch.
+# The training process is conducted over several iterations. During each iteration, the model learns
+# parameters, from mini-batches of training samples (in this example, training samples of size `batch_size=64`),
+# to make better predictions. We print the model's accuracy and loss at each iteration; we'd like to see the
+# accuracy increase and the loss decrease with every iteration.
 
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
+iterations = 5
+for t in range(iterations):
+    print(f"Iteration {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
     test(test_dataloader, model)
 print("Done!")
