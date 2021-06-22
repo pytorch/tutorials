066b4d9a5f97aad99b0b6c29bc81576bf1236b42
FaranIdo <IdoOsbron@gmail.com>
2021-06-22 15:49:47 +0300

    Fix testloss calculation in Quickstart and Optimization tutorials (#1536)
    * Update optimization_tutorial.py
    
    * Update quickstart_tutorial.py
    
    * Fix typo in optimization tutorial
    
    Co-authored-by: suraj813 <5676233+suraj813@users.noreply.github.com>

--- beginner_source/basics/quickstart_tutorial.py
+++ beginner_source/basics/quickstart_tutorial.py
@@ -160,6 +160,7 @@ def train(dataloader, model, loss_fn, optimizer):
 
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
+    num_batches = len(dataloader)
     model.eval()
     test_loss, correct = 0, 0
     with torch.no_grad():
@@ -168,7 +169,7 @@ def test(dataloader, model, loss_fn):
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= size
+    test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
