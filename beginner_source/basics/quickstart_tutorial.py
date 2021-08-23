cc41f3fae435d9574268a3558317bf712204ec58
Wesxdz <sirwesleybarlow@gmail.com>
2021-08-23 10:33:40 -0500

    Place evaluation data on device The last part of the quickstart tutorial would not run by default and had the error "Expected all tensors to be on the same device, but found at least two devices, cuda:0 and cpu" The issue could be particular to my local environment, but if this issue is applicable to others using CUDA , this is the solution

--- beginner_source/basics/quickstart_tutorial.py
+++ beginner_source/basics/quickstart_tutorial.py
@@ -233,7 +233,7 @@ classes = [
 model.eval()
 x, y = test_data[0][0], test_data[0][1]
 with torch.no_grad():
-    pred = model(x)
+    pred = model(x.to(device))
     predicted, actual = classes[pred[0].argmax(0)], classes[y]
     print(f'Predicted: "{predicted}", Actual: "{actual}"')
 
