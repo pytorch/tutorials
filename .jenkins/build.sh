dc2579facdf0784251227dfd1404b17986089c60
Mike Guo <myguo@microsoft.com>
2021-10-22 14:08:39 +0800

    Add non-context manager tutorial for PyTorch Tensorboard plugin (#1625)
    * 1.10.0-RC-Test (#1695)
    
    * Update build.sh
    
    * Update audio tutorial (#1713)
    
    * Update audio tutorial
    
    * fix
    
    * Update pipeline_tutorial.py (#1715)
    
    Reduce value to save memory.
    
    * Update tutorial of memory view (#1672)
    
    * update assets of memory view tutorial
    
    * update memory view tutorial
    
    * Update tensorboard_profiler_tutorial.py
    
    Touch to kick build.
    
    Co-authored-by: Brian Johnson <brianjo@fb.com>
    
    Co-authored-by: moto <855818+mthrok@users.noreply.github.com>
    Co-authored-by: cloudhan <cloudhan@outlook.com>
    
    * Add text-to-speech tutorial (#1710)
    
    * Update build.sh
    
    * Update audio tutorial (#1713)
    
    * Update audio tutorial
    
    * fix
    
    * Add text-to-speech tutorial
    
    * Update contact
    
    * Apply suggestions from code review
    
    Co-authored-by: Caroline Chen <carolinechen@fb.com>
    
    * Remove _tutorial so as not build
    
    * Fix audio display
    
    Co-authored-by: Brian Johnson <brianjo@fb.com>
    Co-authored-by: Yao-Yuan Yang <yangarbiter@gmail.com>
    Co-authored-by: Caroline Chen <carolinechen@fb.com>
    
    * Update build.sh (#1718)
    
    * Update build.sh
    
    * Update build.sh
    
    * Update nnapi tutorial (#1716)
    
    Summary:
    
    Update nnapi tutorial for beta launch
    
    * Add host build
    * Update instructions for install
    
    Test Plan:
    
    make html-noplot
    
    Reviewers:
    
    dreiss
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Co-authored-by: Brian Johnson <brianjo@fb.com>
    
    * Add speech recognition tutorial (#1714)
    
    * Update build.sh
    
    * Update audio tutorial (#1713)
    
    * Update audio tutorial
    
    * fix
    
    * Add speech recognition tutorial
    
    * update title
    
    * Fix some
    
    * Apply suggestions from code review
    
    Co-authored-by: Caroline Chen <carolinechen@fb.com>
    
    * Update intermediate_source/speech_recognition_pipeline_tutorial.py
    
    * Update intermediate_source/speech_recognition_pipeline_tutorial.py
    
    * Update intermediate_source/speech_recognition_pipeline_tutorial.py
    
    * Add link to the tutorial page
    
    * Fix
    
    Co-authored-by: Brian Johnson <brianjo@fb.com>
    Co-authored-by: Caroline Chen <carolinechen@fb.com>
    
    * Add non-context manager tutorial for PyTorch Tensorboard plugin
    
    * add split line in tutorials
    
    * fix bugs
    
    Co-authored-by: Brian Johnson <brianjo@fb.com>
    Co-authored-by: moto <855818+mthrok@users.noreply.github.com>
    Co-authored-by: cloudhan <cloudhan@outlook.com>
    Co-authored-by: Yao-Yuan Yang <yangarbiter@gmail.com>
    Co-authored-by: Caroline Chen <carolinechen@fb.com>
    Co-authored-by: Akshit Khurana <axit@fb.com>

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -55,6 +55,8 @@ if [[ "${JOB_BASE_NAME}" == *worker_* ]]; then
   # Temp remove for mnist download issue. (Re-enabled for 1.8.1)
   # python $DIR/remove_runnable_code.py beginner_source/fgsm_tutorial.py beginner_source/fgsm_tutorial.py || true
   # python $DIR/remove_runnable_code.py intermediate_source/spatial_transformer_tutorial.py intermediate_source/spatial_transformer_tutorial.py || true
+  # Temp remove for 1.10 release. 
+  python $DIR/remove_runnable_code.py advanced_source/neural_style_tutorial.py advanced_source/neural_style_tutorial.py || true
 
   # TODO: Fix bugs in these tutorials to make them runnable again
   # python $DIR/remove_runnable_code.py beginner_source/audio_classifier_tutorial.py beginner_source/audio_classifier_tutorial.py || true
