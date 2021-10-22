2d6e7eebdf3618c4896ca65d263a2032d38614e9
Brian Johnson <brianjo@fb.com>
2021-10-22 11:25:16 -0400

    Update saveloadrun_tutorial.py

--- beginner_source/basics/saveloadrun_tutorial.py
+++ beginner_source/basics/saveloadrun_tutorial.py
@@ -59,5 +59,5 @@ model = torch.load('model.pth')
 
 #######################
 # Related Tutorials
-# ==================
-# `Saving and Loading a General Checkpoint in PyTorch<https://pytorch.org/tutorials/recipes/recipes/saving_and_loading_a_general_checkpoint.html>`_.
+# -----------------
+# `Saving and Loading a General Checkpoint in PyTorch <https://pytorch.org/tutorials/recipes/recipes/saving_and_loading_a_general_checkpoint.html>`_.
