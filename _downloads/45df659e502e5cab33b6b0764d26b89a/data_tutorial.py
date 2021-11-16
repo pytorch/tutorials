2bbb61babb5412a16d1911b203d53d25d6454027
pytorchbot <soumith+bot@pytorch.org>
2021-11-16 17:27:42 +0000

    Automated tutorials push

--- _downloads/45df659e502e5cab33b6b0764d26b89a/data_tutorial.py
+++ _downloads/45df659e502e5cab33b6b0764d26b89a/data_tutorial.py
@@ -160,7 +160,7 @@ class CustomImageDataset(Dataset):
 
 
 def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-    self.img_labels = pd.read_csv(annotations_file)
+    self.img_labels = pd.read_csv(annotations_file, names=['file_name', 'label'])
     self.img_dir = img_dir
     self.transform = transform
     self.target_transform = target_transform
