12626b13465ba1c5b989c5b653f95158307cd4fe
Brian Johnson <brianjo@fb.com>
2021-12-21 12:23:50 -0500

    Update build.sh
    Updates to nightly build for 1.11 test branch.

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -26,6 +26,9 @@ pip install -r $DIR/../requirements.txt
 
 #Install PyTorch Nightly for test. 
 # Nightly - pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
+pip uninstall -y torch torchvision torchaudio torchtext
+pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
+
 # RC Link
 # pip uninstall -y torch torchvision torchaudio torchtext
 # pip install --pre --upgrade -f https://download.pytorch.org/whl/test/cu102/torch_test.html torch torchvision torchaudio torchtext
