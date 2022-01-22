7ca6e93a6f060ca0ce73a45103907d47e453a64f
Wesley A. Cheng <15952538+wesleyacheng@users.noreply.github.com>
2022-01-21 17:07:55 -0800

    Missing argument name
    For consistency

--- beginner_source/basics/transforms_tutorial.py
+++ beginner_source/basics/transforms_tutorial.py
@@ -35,7 +35,7 @@ ds = datasets.FashionMNIST(
     train=True,
     download=True,
     transform=ToTensor(),
-    target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
+    target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(dim=0, torch.tensor(y), value=1))
 )
 
 #################################################
