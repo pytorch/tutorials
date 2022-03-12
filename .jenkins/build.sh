6722a4da4633616c34fe28fee3483b05ac4c39c0
Nikita Shulga <nshulga@fb.com>
2022-03-11 18:05:20 -0800

    Update to awsv2
    As old one conflicts with docutils

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -44,7 +44,8 @@ rm -rf src
 pip install -e git+https://github.com/pytorch/pytorch_sphinx_theme.git#egg=pytorch_sphinx_theme
 pip install sphinx-gallery==0.3.1 tqdm matplotlib ipython pillow==8.1.0
 
-aws configure set default.s3.multipart_threshold 5120MB
+awsv2 -i
+awsv2 configure set default.s3.multipart_threshold 5120MB
 
 # Decide whether to parallelize tutorial builds, based on $JOB_BASE_NAME
 export NUM_WORKERS=20
@@ -165,7 +166,7 @@ if [[ "${JOB_BASE_NAME}" == *worker_* ]]; then
 
   # Step 6: Copy generated files to S3, tag with commit ID
   7z a worker_${WORKER_ID}.7z docs
-  aws s3 cp worker_${WORKER_ID}.7z s3://${BUCKET_NAME}/${COMMIT_ID}/worker_${WORKER_ID}.7z --acl public-read
+  awsv2 s3 cp worker_${WORKER_ID}.7z s3://${BUCKET_NAME}/${COMMIT_ID}/worker_${WORKER_ID}.7z --acl public-read
 elif [[ "${JOB_BASE_NAME}" == *manager ]]; then
   # Step 1: Generate no-plot HTML pages for all tutorials
   make html-noplot
@@ -174,7 +175,7 @@ elif [[ "${JOB_BASE_NAME}" == *manager ]]; then
   # Step 2: Wait for all workers to finish
   set +e
   for ((worker_id=0;worker_id<NUM_WORKERS;worker_id++)); do
-    until aws s3api head-object --bucket ${BUCKET_NAME} --key ${COMMIT_ID}/worker_$worker_id.7z
+    until awsv2 s3api head-object --bucket ${BUCKET_NAME} --key ${COMMIT_ID}/worker_$worker_id.7z
     do
       echo "Waiting for worker $worker_id to finish..."
       sleep 5
@@ -185,7 +186,7 @@ elif [[ "${JOB_BASE_NAME}" == *manager ]]; then
   # Step 3: Download generated with-plot HTML files and static files from S3, merge into one folder
   mkdir -p docs_with_plot/docs
   for ((worker_id=0;worker_id<NUM_WORKERS;worker_id++)); do
-    aws s3 cp s3://${BUCKET_NAME}/${COMMIT_ID}/worker_$worker_id.7z worker_$worker_id.7z
+    awsv2 s3 cp s3://${BUCKET_NAME}/${COMMIT_ID}/worker_$worker_id.7z worker_$worker_id.7z
     7z x worker_$worker_id.7z -oworker_$worker_id
     yes | cp -R worker_$worker_id/docs/* docs_with_plot/docs
   done
@@ -198,7 +199,7 @@ elif [[ "${JOB_BASE_NAME}" == *manager ]]; then
 
   # Step 6: Copy generated HTML files and static files to S3
   7z a manager.7z docs
-  aws s3 cp manager.7z s3://${BUCKET_NAME}/${COMMIT_ID}/manager.7z --acl public-read
+  awsv2 s3 cp manager.7z s3://${BUCKET_NAME}/${COMMIT_ID}/manager.7z --acl public-read
 
   # Step 7: push new HTML files and static files to gh-pages
   if [[ "$COMMIT_SOURCE" == master ]]; then
