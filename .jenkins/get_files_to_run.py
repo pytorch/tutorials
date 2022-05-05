d3a0a43a8d8d23d9f2a7c770610c06493fc1cae6
Catherine Lee <csl@fb.com>
2022-05-04 17:11:21 -0700

    shard based on duration

--- .jenkins/get_files_to_run.py
+++ .jenkins/get_files_to_run.py
@@ -19,17 +19,35 @@ def get_all_files(encoding="utf-8") -> List[str]:
     return run(cmd, capture_output=True).stdout.decode(encoding).splitlines()
 
 
+def calculate_shards(all_files, num_shards=20):
+    metadata = json.load(open(".jenkins/metadata.json"))
+    sorted_files = sorted(all_files, key=lambda x: metadata.get(x, {}).get("duration", 1), reverse=True)
+
+    sharded_files = [(0.0, []) for _ in range(num_shards)]
+    for filename in sorted_files:
+        min_shard_index = sorted(range(num_shards), key=lambda i: sharded_files[i][0])[0]
+        curr_shard_time, curr_shard_jobs = sharded_files[min_shard_index]
+        curr_shard_jobs.append(filename)
+        sharded_files[min_shard_index] = (
+            curr_shard_time + metadata.get(filename, {}).get("duration", 1),
+            curr_shard_jobs,
+        )
+    return list(map(lambda x: x[1], sharded_files))
+
+
+def remove_other_files(all_files, files_to_run):
+    for file in all_files:
+        if file not in files_to_run:
+            remove_runnable_code(file, file)
+
+
 def main():
     num_shards = int(os.environ.get("NUM_WORKERS", 20))
     shard_num = int(os.environ.get("WORKER_ID", 0))
 
     all_files = get_all_files()
-    files_to_run = []
-    for i, name in enumerate(all_files):
-        if i % num_shards == shard_num:
-            files_to_run.append(name)
-        else:
-            remove_runnable_code(name, name)
+    files_to_run = calculate_shards(all_files, num_shards=num_shards)[shard_num]
+    remove_other_files(all_files, files_to_run)
     stripped_file_names = list(map(lambda x: Path(x).stem, files_to_run))
     print(" ".join(stripped_file_names))
 
