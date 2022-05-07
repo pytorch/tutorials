b241961fd1167e4646fc50c2d53f2ca3cee265e1
pytorchbot <soumith+bot@pytorch.org>
2022-05-07 01:12:21 +0000

    Automated tutorials push

--- _downloads/d20cbe06d71c96a4bb8f222f7c64cf25/tensorboard_profiler_tutorial.py
+++ _downloads/d20cbe06d71c96a4bb8f222f7c64cf25/tensorboard_profiler_tutorial.py
@@ -43,47 +43,47 @@ To install ``torch`` and ``torchvision`` use the following command:
 # First, import all necessary libraries:
 #
 
-
-
-
-
-
-
-
-
+import torch
+import torch.nn
+import torch.optim
+import torch.profiler
+import torch.utils.data
+import torchvision.datasets
+import torchvision.models
+import torchvision.transforms as T
 
 ######################################################################
 # Then prepare the input data. For this tutorial, we use the CIFAR10 dataset.
 # Transform it to the desired format and use DataLoader to load each batch.
 
-
-
-
-
-
-
+transform = T.Compose(
+    [T.Resize(224),
+     T.ToTensor(),
+     T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
 
 ######################################################################
 # Next, create Resnet model, loss function, and optimizer objects.
 # To run on GPU, move model and loss to GPU device.
 
-
-
-
-
-
+device = torch.device("cuda:0")
+model = torchvision.models.resnet18(pretrained=True).cuda(device)
+criterion = torch.nn.CrossEntropyLoss().cuda(device)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+model.train()
 
 
 ######################################################################
 # Define the training step for each batch of input data.
 
-
-
-
-
-
-
-
+def train(data):
+    inputs, labels = data[0].to(device=device), data[1].to(device=device)
+    outputs = model(inputs)
+    loss = criterion(outputs, labels)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
 
 ######################################################################
@@ -119,33 +119,33 @@ To install ``torch`` and ``torchvision`` use the following command:
 #   If the TensorBoard is launched in VSCode (`reference <https://code.visualstudio.com/docs/datascience/pytorch-support#_tensorboard-integration>`_),
 #   clicking a stack frame will navigate to the specific code line.
 
-
-
-
-
-
-
-
-
-
-
-
-
+with torch.profiler.profile(
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/resnet18'),
+        record_shapes=True,
+        profile_memory=True,
+        with_stack=True
+) as prof:
+    for step, batch_data in enumerate(train_loader):
+        if step >= (1 + 1 + 3) * 2:
+            break
+        train(batch_data)
+        prof.step()  # Need to call this at the end of each step to notify profiler of steps' boundary.
 
 ######################################################################
 # Alternatively, the following non-context manager start/stop is supported as well.
-
-
-
-
-
-
-
-
-
-
-
-
+prof = torch.profiler.profile(
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/resnet18'),
+        record_shapes=True,
+        with_stack=True)
+prof.start()
+for step, batch_data in enumerate(train_loader):
+    if step >= (1 + 1 + 3) * 2:
+        break
+    train(batch_data)
+    prof.step()
+prof.stop()
 
 ######################################################################
 # 3. Run the profiler
@@ -397,5 +397,3 @@ To install ``torch`` and ``torchvision`` use the following command:
 #
 # -  `Pytorch TensorBoard Profiler github <https://github.com/pytorch/kineto/tree/master/tb_plugin>`_
 # -  `torch.profiler API <https://pytorch.org/docs/master/profiler.html>`_
-
-# %%%%%%RUNNABLE_CODE_REMOVED%%%%%%
