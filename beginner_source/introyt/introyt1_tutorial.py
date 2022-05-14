02cd8c8c2fd9d7ae0d37480d7d9a8b925c86d3ae
DokySp <uhug@naver.com>
2022-05-14 18:03:56 +0900

    Update introyt1_tutorial.py

--- beginner_source/introyt/introyt1_tutorial.py
+++ beginner_source/introyt/introyt1_tutorial.py
@@ -442,17 +442,21 @@ def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
 
+def load_dataset():
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
 
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
+if __name__ == "__main__":
+    freeze_support()
+    load_dataset()
 
 ##########################################################################
 # This is the model we’ll train. If it looks familiar, that’s because it’s
