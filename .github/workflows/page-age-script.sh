9a9328df2120e10d915d6fa44ee81479d20b7f47
Svetlana Karslioglu <svekars@fb.com>
2022-08-03 17:57:51 -0700

    Update

--- .github/workflows/page-age-script.sh
+++ .github/workflows/page-age-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git ls-tree -r main --name-only | grep -E '.*\.(py|rst)' | while read filename; do
+git ls-tree -r master --name-only | grep -E '.*\.(py|rst)' | while read filename; do
   echo "$(git log -1 --date=short --format="%ad" -- $filename) $filename";
 done | sort -r | tr  " " ","  > page-age.csv
