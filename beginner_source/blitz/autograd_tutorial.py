05ffcb261755e3b663e022ce36e2c41ff03eac21
Aditya Punetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-08-29 12:27:23 +0530

    Update autograd_tutorial.py
    The parameter 'pretrained' is deprecated. Updated it to latest standard.

--- beginner_source/blitz/autograd_tutorial.py
+++ beginner_source/blitz/autograd_tutorial.py
@@ -43,8 +43,9 @@ shape (1,1000).
     This tutorial work only on CPU and will not work on GPU (even if tensor are moved to CUDA).
 
 """
-import torch, torchvision
-model = torchvision.models.resnet18(pretrained=True)
+import torch
+from torchvision.models import resnet18, ResNet18_Weights
+model = resnet18(weights=ResNet18_Weights.DEFAULT)
 data = torch.rand(1, 3, 64, 64)
 labels = torch.rand(1, 1000)
 
