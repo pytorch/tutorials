3188b6644af25dbd65c646741c1db29faa0eb1dd
Svetlana Karslioglu <svekars@fb.com>
2022-09-26 12:48:32 -0700

    Update

--- beginner_source/flava_finetuning_tutorial.py
+++ beginner_source/flava_finetuning_tutorial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-TorchMultimodal Tutorial : Finetuning FLAVA
-=======================
+TorchMultimodal Tutorial: Finetuning FLAVA
+============================================
 """
 
 ######################################################################
@@ -14,43 +14,33 @@ TorchMultimodal Tutorial : Finetuning FLAVA
 # 
 # In this tutorial, we will demonstrate how to use a **pretrained SoTA
 # model called** `FLAVA <https://arxiv.org/pdf/2112.04482.pdf>`__ **from
-# TorchMultimodal library to finetune on a multimodal task i.e. visual
+# TorchMultimodal library to finetune on a multimodal task i.e. visual
 # question answering** (VQA).
 # 
 
 
 ######################################################################
 # Installations
-# 
+#
+#
 # We will use TextVQA dataset from HuggingFace for this
-# tutorial. So we install datasets in addition to TorchMultimodal
-# 
+# tutorial. So we install datasets in addition to TorchMultimodal.
 
-# TODO: replace with install from pip when binary is ready
-!git clone https://github.com/facebookresearch/multimodal.git
-!pip install -r multimodal/requirements.txt
 import os
-import sys 
+import sys
 sys.path.append(os.path.join(os.getcwd(),"multimodal"))
 sys.path.append(os.getcwd())
-!pip install datasets
-!pip install transformers
-
 
 ######################################################################
 # For this tutorial, we treat VQA as a classification task. So we need to
 # download the vocab file with answer classes and create the answer to
 # label mapping.
-# 
+#
 # We also load the `textvqa
 # dataset <https://arxiv.org/pdf/1904.08920.pdf>`__ from HuggingFace
-# 
+#
 
-!wget http://dl.fbaipublicfiles.com/pythia/data/vocab.tar.gz
-!tar xf vocab.tar.gz
-
-
-with open("vocabs/answers_textvqa_more_than_1.txt") as f:
+with open("data/vocabs/answers_textvqa_more_than_1.txt") as f:
   vocab = f.readlines()
 
 answer_to_idx = {}
@@ -61,7 +51,7 @@ for idx, entry in enumerate(vocab):
 ######################################################################
 # We see there are 3997 answer classes including a class representing
 # unknown answers
-# 
+#
 
 print(len(vocab))
 print(vocab[:5])
@@ -81,9 +71,9 @@ display(dataset["train"][idx]["image"].resize((500,500)))
 # Tensors consumable by our model - For images, we use the transforms from
 # torchvision to convert to Tensor and resize to uniform sizes - For text,
 # we tokenize (and pad) them using the BertTokenizer from HuggingFace -
-# For answers (i.e. labels), we take the most frequently occuring answer
+# For answers (i.e. labels), we take the most frequently occuring answer
 # as the label to train with
-# 
+#
 
 import torch
 from torchvision import transforms
@@ -99,15 +89,15 @@ def transform(tokenizer, input):
 
   tokenized=tokenizer(input["question"],return_tensors='pt',padding="max_length",max_length=512)
   batch.update(tokenized)
-  
-  
+
+
   ans_to_count = defaultdict(int)
   for ans in input["answers"][0]:
     ans_to_count[ans] += 1
   max_value = max(ans_to_count, key=ans_to_count.get)
   ans_idx = answer_to_idx.get(max_value,0)
   batch["answers"] = torch.as_tensor([ans_idx])
-  
+
   return batch
 
 tokenizer=BertTokenizer.from_pretrained("bert-base-uncased",padding="max_length",max_length=512)
@@ -119,14 +109,14 @@ dataset.set_transform(transform)
 # Finally, we import the flava_model_for_classification from
 # torchmultimodal. It loads the pretrained flava checkpoint by default and
 # includes a classification head.
-# 
+#
 # The model forward function passes the image through the visual encoder
 # and the question through the text encoder. The image and question
 # embeddings are then passed through the multimodal encoder. The final
 # embedding corresponding to the CLS token is passed through a MLP head
 # which finally gives the probability distribution over each possible
 # answers.
-# 
+#
 
 from torchmultimodal.models.flava.model import flava_model_for_classification
 model = flava_model_for_classification(num_classes=len(vocab))
@@ -135,7 +125,7 @@ model = flava_model_for_classification(num_classes=len(vocab))
 ######################################################################
 # We put together the dataset and model in a toy training loop to
 # demonstrate how to train the model for 3 iterations.
-# 
+#
 
 from torch import nn
 BATCH_SIZE = 2
@@ -144,8 +134,8 @@ from torch.utils.data import DataLoader
 
 train_dataloader = DataLoader(dataset["train"], batch_size= BATCH_SIZE)
 optimizer = torch.optim.AdamW(model.parameters())
-  
-     
+
+
 epochs = 1
 for _ in range(epochs):
   for idx, batch in enumerate(train_dataloader):
@@ -161,7 +151,7 @@ for _ in range(epochs):
 
 ######################################################################
 # Conclusion
-# 
+#
 # This tutorial introduced the basics around how to finetune on a
 # multimodal task using FLAVA from TorchMultimodal. Please also check out
 # other examples from the library like
@@ -169,5 +159,4 @@ for _ in range(epochs):
 # which is a multimodal model for object detection and
 # `Omnivore <https://github.com/facebookresearch/multimodal/blob/main/torchmultimodal/models/omnivore.py>`__
 # which is multitask model spanning image, video and 3d classification.
-# 
-
+#
