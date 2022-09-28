26f774f088dff278fc60b8bb00b6b41f923c069d
SiftingSands <zcz2020@gmail.com>
2022-09-28 19:09:50 -0400

    Naive removal of block beginning with comments to see if it fixes the webpage

--- intermediate_source/reinforcement_q_learning.py
+++ intermediate_source/reinforcement_q_learning.py
@@ -232,13 +232,6 @@ class DQN(nn.Module):
 #    episode.
 #
 
-# BATCH_SIZE is the number of transitions sampled from the replay buffer
-# GAMMA is the discount factor as mentioned in the previous section
-# EPS_START is the starting value of epsilon
-# EPS_END is the final value of epsilon
-# EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
-# TAU is the update rate of the target network
-# LR is the learning rate of the AdamW optimizer
 BATCH_SIZE = 128
 GAMMA = 0.99
 EPS_START = 0.9
