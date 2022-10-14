a1309cd304455c8318f8412a530143dc956a20e2
Svetlana Karslioglu <svekars@fb.com>
2022-10-14 11:37:46 -0700

    Update build.sh

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -29,7 +29,7 @@ pip install -r $DIR/../requirements.txt
 
 # Test enabled for PyTorch 1.13 RC Below
 pip uninstall -y torch torchvision torchaudio torchtext
-pip3 install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/test/cu116/torch_test.html
+pip3 install --pre torch torchvision torchaudio torchtext -f https://download.pytorch.org/whl/test/cu116/torch_test.html
 
 # Install two language tokenizers for Translation with TorchText tutorial
 python -m spacy download en_core_web_sm
