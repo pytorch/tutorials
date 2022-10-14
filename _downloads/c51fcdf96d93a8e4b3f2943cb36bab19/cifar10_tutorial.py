f8b24c13323f55fff6a12a8bc28848721c7257e2
pytorchbot <soumith+bot@pytorch.org>
2022-10-14 00:05:24 +0000

    Automated tutorials push

--- _downloads/c51fcdf96d93a8e4b3f2943cb36bab19/cifar10_tutorial.py
+++ _downloads/c51fcdf96d93a8e4b3f2943cb36bab19/cifar10_tutorial.py
@@ -105,7 +105,7 @@ def imshow(img):
 
 # get some random training images
 dataiter = iter(trainloader)
-images, labels = dataiter.next()
+images, labels = next(dataiter)
 
 # show images
 imshow(torchvision.utils.make_grid(images))
@@ -210,7 +210,7 @@ torch.save(net.state_dict(), PATH)
 # Okay, first step. Let us display an image from the test set to get familiar.
 
 dataiter = iter(testloader)
-images, labels = dataiter.next()
+images, labels = next(dataiter)
 
 # print images
 imshow(torchvision.utils.make_grid(images))
