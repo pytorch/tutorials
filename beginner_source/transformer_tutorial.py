18e5bfe471796ff3f9c847161004b67c65328e1b
Nayef Ahmed <nayef211@meta.com>
2023-01-25 09:26:57 -0800

    Fix CI errors

--- beginner_source/transformer_tutorial.py
+++ beginner_source/transformer_tutorial.py
@@ -349,33 +349,32 @@ def evaluate(model: nn.Module, eval_data: Tensor) -> float:
 best_val_loss = float('inf')
 epochs = 3
 
-tempdir = TemporaryDirectory()
-best_model_params_path = os.path.join(tempdir.path, "best_model_params.pt")
-for epoch in range(1, epochs + 1):
-    epoch_start_time = time.time()
-    train(model)
-    val_loss = evaluate(model, val_data)
-    val_ppl = math.exp(val_loss)
-    elapsed = time.time() - epoch_start_time
-    print('-' * 89)
-    print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
-        f'valid loss {val_loss:5.2f} | valid ppl {val_ppl:8.2f}')
-    print('-' * 89)
+with TemporaryDirectory() as tempdir:
+    best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
 
-    if val_loss < best_val_loss:
-        best_val_loss = val_loss
-        torch.save(model.state_dict(), best_model_params_path)
+    for epoch in range(1, epochs + 1):
+        epoch_start_time = time.time()
+        train(model)
+        val_loss = evaluate(model, val_data)
+        val_ppl = math.exp(val_loss)
+        elapsed = time.time() - epoch_start_time
+        print('-' * 89)
+        print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+            f'valid loss {val_loss:5.2f} | valid ppl {val_ppl:8.2f}')
+        print('-' * 89)
 
-    scheduler.step()
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(model.state_dict(), best_model_params_path)
+
+        scheduler.step()
+    model.load_state_dict(torch.load(best_model_params_path)) # load best model states
 
 
 ######################################################################
 # Evaluate the best model on the test dataset
 # -------------------------------------------
 #
-
-model = torch.load(best_model_params_path) # load best model states
-tempdir.cleanup()
 
 test_loss = evaluate(model, test_data)
 test_ppl = math.exp(test_loss)
