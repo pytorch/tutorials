95fe7e6903815f8fe70bca729dffa63f53ce6fbc
Munirdin Jadikar <49895184+munirdin87@users.noreply.github.com>
2023-01-30 22:35:42 +0100

    Update transfer_learning_tutorial.py
    Comma removed

--- beginner_source/transfer_learning_tutorial.py
+++ beginner_source/transfer_learning_tutorial.py
@@ -86,7 +86,7 @@ data_transforms = {
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
+    ])
 }
 
 data_dir = 'data/hymenoptera_data'
