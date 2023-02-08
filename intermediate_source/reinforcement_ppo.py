1cc55f4cc5a3e74e79de162b00f4f1a98ed2bc6d
vmoens <vincentmoens@gmail.com>
2023-02-08 14:38:11 +0000

    amend

--- intermediate_source/reinforcement_ppo.py
+++ intermediate_source/reinforcement_ppo.py
@@ -74,19 +74,26 @@ models (policy and value function), loss modules, data collectors and replay buf
 # updates that would produce significant shifts from the previous configuration
 # are being discouraged.
 #
-# This tutorial is structured as follows: first, we will define the set of hyperparameters
-# we will be using for training.
-# Next, we will focus on creating our environment, or simulator, using TorchRL's
-# wrappers and transforms. We will design the policy network and the value model,
-# which is indispensable to the loss function. These modules will be used to configure
-# our loss module, and finally we will be creating the replay buffer and data loader.
+# This tutorial is structured as follows:
 #
-# Once all these elements are on stage, we will be able to run our training loop.
+# 1. First, we will define a set of hyperparameters we will be using for training.
 #
-# Throughout this tutorial, we'll be using the :py:mod:`tensordict` library. :class:`TensorDict`
-# is the lingua franca of TorchRL: it helps us abstract what a module reads and writes
-# and care less about the specific data description and more about the algorithm itself.
-
+# 2. Next, we will focus on creating our environment, or simulator, using TorchRL's
+#    wrappers and transforms.
+#
+# 3. Next, We will design the policy network and the value model,
+#    which is indispensable to the loss function. These modules will be used
+#    to configure our loss module.
+#
+# 4. Next, we will create the replay buffer and data loader.
+#
+# 5. Finally, we will run our training loop and analyze the results.
+#
+# Throughout this tutorial, we'll be using the :py:mod:`tensordict` library.
+# :class:`TensorDict` is the lingua franca of TorchRL: it helps us abstract
+# what a module reads and writes and care less about the specific data
+# description and more about the algorithm itself.
+#
 
 from collections import defaultdict
 
@@ -106,7 +113,6 @@ from torchrl.envs.utils import set_exploration_mode, check_env_specs
 from torchrl.modules import TanhNormal, ProbabilisticActor, ValueOperator
 from torchrl.objectives import ClipPPOLoss
 from torchrl.objectives.value import GAE
-from torchrl.record import VideoRecorder
 from tqdm import tqdm
 
 ######################################################################
@@ -124,20 +130,50 @@ from tqdm import tqdm
 
 num_procs = 4
 num_cells = 256
-frame_skip = 1
-frames_per_batch = 1000 // frame_skip
-# For a complete training, bring the number of frames up to 1M
-total_frames = 50_000 // frame_skip
 device = "cpu" if not torch.has_cuda else "cuda:0"
 gamma = 0.99
 lmbda = 0.95
-batch_size = 64  # total batch-size to compute the loss
-num_epochs = 10  # optimisation steps per batch of data collected
-clip_epsilon = 0.2  # clip value for PPO loss
 lr = 3e-4
 max_grad_norm = 1.0
 entropy_eps = 1e-4
 seed = 0
+
+######################################################################
+# Data collection parameters
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# When collecting data, we will be able to choose how big each batch will be
+# by defining a ``frames_per_batch`` parameter. We will also define how many
+# frames (ie number of interactions with the simulator) we allow ourselves to
+# use. In general, the goal of an RL algorithm is to learn to solve the task
+# as fast as we can in terms of environment interactions: the lowere the ``total_frames``
+# the better.
+# We also define a ``frame_skip``: in some contexts, repeating the same action
+# multiple times over the course of a trajectory may be beneficial as it makes
+# the behaviour more consistent and less erratic. However, a "skipping"
+# too many frames will hamper training by reducing the reactivity of the actor
+# to observation changes.
+#
+frame_skip = 1
+frames_per_batch = 1000 // frame_skip
+# For a complete training, bring the number of frames up to 1M
+total_frames = 50_000 // frame_skip
+
+######################################################################
+# PPO parameters
+# ~~~~~~~~~~~~~~
+#
+# At each data collection (or batch collection) we will run the optimization
+# over a certain number of "epochs", each time consuming the entire data we just
+# acquired in a nested training loop. Here, the ``sub_batch_size`` is different from the
+# ``frames_per_batch`` here above: recall that we are working with a "batch of data"
+# coming from our collector, which size is defined by ``frames_per_batch``, and that
+# we will further split in smaller sub-batches during the inner training loop.
+# The size of these sub-batches is controlled by ``sub_batch_size``.
+#
+sub_batch_size = 64  # total batch-size to compute the loss
+num_epochs = 10  # optimisation steps per batch of data collected
+clip_epsilon = 0.2  # clip value for PPO loss: see the equation in the intro for more context.
 
 ######################################################################
 # Define an environment
@@ -155,7 +191,7 @@ seed = 0
 base_env = GymEnv(
     "InvertedDoublePendulum-v4", device=device,
     frame_skip=frame_skip
-    )
+)
 torch.manual_seed(seed)
 base_env.set_seed(seed)
 
@@ -200,6 +236,16 @@ base_env.set_seed(seed)
 # the environment is terminated. We will use this measure as a supplementary measure
 # of performance.
 #
+# As we will see later, many of the TorchRL's classes rely on :class:`TensorDict`
+# to communicate. You could think of it as a python dictionary with some extra
+# tensor features. In practice, this means that many modules we will be working
+# with need to be told what key to read (``in_keys``) and what key to write
+# (``out_keys``) in the tensordict they will receive. Usually, if ``out_keys``
+# is omitted, it is assumed that the ``in_keys`` entries will be updated
+# in-place. For our transforms, the only entry we are interested in is referred
+# to as ``"observation"`` and our transform layers will be told to modify this
+# entry and this entry only:
+#
 
 env = TransformedEnv(
     base_env,
@@ -218,19 +264,32 @@ env = TransformedEnv(
 #
 env.transform[0].init_stats(num_iter=1000, reduce_dim=0, cat_dim=0)
 
-# A little sanity check for the shape of our summary stats:
+######################################################################
+# The :class:`ObservationNorm` transform has now been populated with a
+# location and a scale that will be used to normalize the data.
+#
+# Let us do a little sanity check for the shape of our summary stats:
+#
 print("normalization constant shape:", env.transform[0].loc.shape)
 
 ######################################################################
 # An environment is not only defined by its simulator and transforms, but also
-# by a series of metadata that describe what can be expected during its execution.
-# For efficiency purposes, TorchRL is quite stringent when it comes to environment specs,
-# but you can easily check that your environment specs are adequate.
-# Let's see a concrete example using our transformed environment by looking at its specs.
-# There are three specs to look at: ``observation_spec`` which defines what is to
-# be expected when executing an action in the environment, ``reward_spec`` which indicates
-# the reward domain and finally the ``input_spec`` (which contains the ``action_spec``)
-# and which represents everything an environment requires to execute a single step.
+# by a series of metadata that describe what can be expected during its
+# execution.
+# For efficiency purposes, TorchRL is quite stringent when it comes to
+# environment specs, but you can easily check that your environment specs are
+# adequate.
+# In our example, the :class:`GymWrapper` and :class:`GymEnv` that inherits
+# from it already take care of setting the proper specs for your env so
+# you should not have to care about this.
+#
+# Nevertheless, let's see a concrete example using our transformed
+# environment by looking at its specs.
+# There are three specs to look at: ``observation_spec`` which defines what
+# is to be expected when executing an action in the environment,
+# ``reward_spec`` which indicates the reward domain and finally the
+# ``input_spec`` (which contains the ``action_spec``) and which represents
+# everything an environment requires to execute a single step.
 #
 print("observation_spec:", env.observation_spec)
 print("reward_spec:", env.reward_spec)
@@ -260,10 +319,10 @@ check_env_specs(env)
 #
 rollout = env.rollout(3)
 print("rollout of three steps:", rollout)
-print("Shape of the rollout TensorDict:", rollout.batch_size)
+print("Shape of the rollout TensorDict:", rollout.sub_batch_size)
 
 ######################################################################
-# Our rollout data has a batch-size of ``torch.Size([3])`, which matches the number of steps
+# Our rollout data has a shape of ``torch.Size([3])`, which matches the number of steps
 # we ran it for. The ``"next"`` entry points to the data coming after the current step.
 # In most cases, the ``"next""`` data at time `t` matches the data at ``t+1``, but this
 # may not be the case if we are using some specific transformations (e.g. mutli-step).
@@ -272,17 +331,25 @@ print("Shape of the rollout TensorDict:", rollout.batch_size)
 # ------
 #
 # PPO utilizes a stochastic policy to handle exploration. This means that our
-# neural network will have to output the parameters of a distribution, rather than
-# a single value corresponding to the action taken.
+# neural network will have to output the parameters of a distribution, rather
+# than a single value corresponding to the action taken.
 #
 # As the data is continuous, we use a Tanh-Normal distribution to respect the
-# action space boundaries. TorchRL provides such distribution, and the only thing we
-# need to care about is to build a neural network that outputs the right number of
-# parameters for the policy to work with.
+# action space boundaries. TorchRL provides such distribution, and the only
+# thing we need to care about is to build a neural network that outputs the
+# right number of parameters for the policy to work with (a location, or mean,
+# and a scale):
+#
+# .. math::
+#
+#     f_{\theta}(\text{observation}) = \mu_{\theta}(\text{observation}), \sigma^{+}_{\theta}(\text{observation})
+#
+# The only extra-difficulty that is brought up here is to split our output in two
+# equal parts and map the second to a scrictly positive space.
 #
 # We design the policy in three steps:
 #
-# 1. Define a neural network D_obs -> 2 D_action (loc and scale both have dimension D_action);
+# 1. Define a neural network ``D_obs`` -> ``2 * D_action``. Indeed, our ``loc`` (mu) and ``scale`` (sigma) both have dimension ``D_action``;
 #
 # 2. Append a :class:`NormalParamExtractor` to extract a location and a scale (ie splits the input in two equal parts
 #   and applies a positive transformation to the scale parameter);
@@ -310,14 +377,22 @@ actor_net = nn.Sequential(
 policy_module = TensorDictModule(
     actor_net, in_keys=["observation"],
     out_keys=["loc", "scale"]
-    )
+)
 
 ######################################################################
-# We now need to build a distribution out of the location and scale of our normal
-# distribution. To do so, we instruct the :class:`ProbabilisticActor` class to
-# build a :class:`TanhNormal` out of the location and scale parameters. We also
-# provide the minimum and maximum values of this distribution, which we gather from the
-# environment specs.
+# We now need to build a distribution out of the location and scale of our
+# normal distribution. To do so, we instruct the :class:`ProbabilisticActor`
+# class to build a :class:`TanhNormal` out of the location and scale
+# parameters. We also provide the minimum and maximum values of this
+# distribution, which we gather from the environment specs.
+#
+# The name of the ``in_keys`` (and hence the name of the ``out_keys`` from
+# the :class:`TensorDictModule` above) cannot be set to any value one may
+# like, as the :class:`TanhNormal` distribution constructor will expect the
+# ``loc`` and ``scale`` keyword arguments. That being said,
+# :class:`ProbabilisticActor` also accepts ``Dict[str, str]`` typed ``in_keys``
+# where the key-value pair indicates what ``in_key`` string should be used for
+# every keyword argument that is to be used.
 #
 policy_module = ProbabilisticActor(
     module=policy_module,
@@ -370,15 +445,31 @@ print("Running value:", value_module(env.reset()))
 # Data collector
 # --------------
 #
-# TorchRL provides a set of :class:`DataCollector` classes. Briefly, these classes
-# allow you to control how many frames to collect at each iteration,
-# when to reset the environment, on which device the policy should be executed etc.
-# The simplest data collector is the :class:`SyncDataCollector`: it is an iterator
-# that you can use to get batches of data of a given length, and that will stop
-# once a total number of frames have been collected.
-# As for the policy and environment before, the data collector will return :class:`TensorDict`
-# instances which allow you to write dataloading pipelines that are 100% oblivious
-# to the actual specificities of the rollout content.
+# TorchRL provides a set of :class:`DataCollector` classes. Briefly, these
+# classes execute three operations: reset an environment, compute an action
+# given the latest observation, execute a step in the environment, and repeat
+# the last two steps until the environment reaches a stop signal (or ``"done"``
+# state).
+#
+# They allow you to control how many frames to collect at each iteration
+# (through the ``frames_per_batch`` parameter),
+# when to reset the environment (through the ``max_frames_per_traj`` argument),
+# on which ``device`` the policy should be executed, etc. They are also
+# designed to work efficiently with batched and multiprocessed environments.
+#
+# The simplest data collector is the :class:`SyncDataCollector`: it is an
+# iterator that you can use to get batches of data of a given length, and
+# that will stop once a total number of frames (``total_frames``) have been
+# collected.
+# Other data collectors (``MultiSyncDataCollector`` and
+# ``MultiaSyncDataCollector``) will execute the same operations in synchronous
+# and asynchronous manner over a set of multiprocessed workers.
+#
+# As for the policy and environment before, the data collector will return
+# :class:`TensorDict` instances with a total number of elements that will
+# match ``frames_per_batch``. Using :class:`TensorDict` to pass data to the
+# training loop allows you to write dataloading pipelines
+# that are 100% oblivious to the actual specificities of the rollout content.
 #
 collector = SyncDataCollector(
     env,
@@ -386,6 +477,7 @@ collector = SyncDataCollector(
     frames_per_batch=frames_per_batch,
     total_frames=total_frames,
     split_trajs=False,
+    device=device,
 )
 collector.set_seed(seed)
 
@@ -397,11 +489,16 @@ collector.set_seed(seed)
 # In on-policy contexts, a replay buffer is refilled every time a batch of
 # data is collected, and its data is repeatedly consumed for a certain number
 # of epochs.
-# TorchRL's replay buffers are built using a common container :class:`ReplayBuffer` which
-# takes as argument the components of the buffer: a storage, a writer, a sampler
-# and possibly some transforms. Only the storage (which indicates the replay buffer
-# capacity) is mandatory. We also specify a sampler without repetition to
-# avoid sampling multiple times the same item in one epoch.
+#
+# TorchRL's replay buffers are built using a common container
+# :class:`ReplayBuffer` which takes as argument the components of the buffer:
+# a storage, a writer, a sampler and possibly some transforms. Only the
+# storage (which indicates the replay buffer capacity) is mandatory. We
+# also specify a sampler without repetition to avoid sampling multiple times
+# the same item in one epoch.
+# Using a replay buffer for PPO is not mandatory and we could simply
+# sample the sub-batches from the collected batch, but using these classes
+# make it easy for us to build the inner training loop in a reproducible way.
 #
 
 replay_buffer = ReplayBuffer(
@@ -456,7 +553,7 @@ scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
     optim,
     total_frames // frames_per_batch,
     0.0
-    )
+)
 
 ######################################################################
 # Training loop
@@ -470,34 +567,49 @@ scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
 logs = defaultdict(list)
 pbar = tqdm(total=total_frames * frame_skip)
 eval_str = ""
-for i, data in enumerate(collector):
+
+# We iterate over the collector until it reaches the total number of frames it was
+# designed to collect:
+for i, tensordict_data in enumerate(collector):
+    # we now have a batch of data to work with. Let's learn something from it.
     for k in range(num_epochs):
-        advantage_module(data)
-        data_view = data.reshape(-1)
+        # We'll need an "advantage" signal to make PPO work.
+        # We re-compute it at each epoch as its value depends on the value
+        # network which is updated in the inner loop.
+        advantage_module(tensordict_data)
+        data_view = tensordict_data.reshape(-1)
         replay_buffer.extend(data_view)
-        for j in range(frames_per_batch // batch_size):
-            subdata, *_ = replay_buffer.sample(batch_size)
+        for j in range(frames_per_batch // sub_batch_size):
+            subdata, *_ = replay_buffer.sample(sub_batch_size)
             loss_vals = loss_module(subdata)
             loss_value = loss_vals["loss_objective"] + loss_vals[
                 "loss_critic"] + loss_vals["loss_entropy"]
 
-            # Optim
+            # Optimization: backward, grad clipping and optim step
             loss_value.backward()
+            # this is not strictly mandatory but it's good practice to keep
+            # your gradient norm bounded
             torch.nn.utils.clip_grad_norm_(
                 loss_module.parameters(),
                 max_grad_norm
-                )
+            )
             optim.step()
             optim.zero_grad()
 
-    logs["reward"].append(data["reward"].mean().item())
-    pbar.update(data.numel() * frame_skip)
+    logs["reward"].append(tensordict_data["reward"].mean().item())
+    pbar.update(tensordict_data.numel() * frame_skip)
     cum_reward_str = f"average reward={logs['reward'][-1]: 4.4f} (init={logs['reward'][0]: 4.4f})"
-    logs["step_count"].append(data['step_count'].max().item())
+    logs["step_count"].append(tensordict_data['step_count'].max().item())
     stepcount_str = f"step count (max): {logs['step_count'][-1]}"
     logs["lr"].append(optim.param_groups[0]['lr'])
     lr_str = f"lr policy: {logs['lr'][-1]: 4.4f}"
     if i % 10 == 0:
+        # We evaluate the policy once every 10 batches of data.
+        # Evaluation is rather simple: execute the policy without exploration
+        # (take the expected value of the action distribution) for a given
+        # number of steps (1000, which is our env horizon).
+        # The ``rollout`` method of the env can take a policy as argument:
+        # it will then execute this policy at each step.
         with set_exploration_mode("mean"), torch.no_grad():
             # execute a rollout with the trained policy
             eval_rollout = env.rollout(1000, policy_module)
@@ -521,11 +633,9 @@ for i, data in enumerate(collector):
         )
     )
 
-    collector.update_policy_weights_()
+    # We're also using a learning rate scheduler. Like the gradient clipping,
+    # this is a nice-to-have but nothing necessary for PPO to work.
     scheduler.step()
-
-collector.shutdown()
-del collector
 
 ######################################################################
 # Results
@@ -563,4 +673,3 @@ plt.show()
 # inverted pendulum in action. Check :obj:`torchrl.record.VideoRecorder` to
 # know more.
 #
-
