6ba9341cfc632534084cb2f8d5081fb448451a83
Svetlana Karslioglu <svekars@fb.com>
2023-03-10 09:20:00 -0800

    Update validate_tutorials_built.py

--- .jenkins/validate_tutorials_built.py
+++ .jenkins/validate_tutorials_built.py
@@ -49,9 +49,7 @@ NOT_RUN = [
     "recipes/Captum_Recipe",
     "flask_rest_api_tutorial",
     "text_to_speech_with_torchaudio",
-    "forward_ad_usage", # remove after #2218 is resolved.
-    "text_sentiment_ngrams_tutorial", # reenable after 2.0 release.
-    "tensorboard_profiler_tutorial", # reenable after 2.0 release.
+    "forward_ad_usage" # remove after #2218 is resolved.
 ]
 
 
