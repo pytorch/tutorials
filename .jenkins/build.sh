d637bdaf6abd9e40a0162989c4f0a58fd18ffc39
Nikita Shulga <nshulga@fb.com>
2023-03-15 14:11:38 -0700

    Update .jenkins/build.sh

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -29,13 +29,6 @@ pip install -r $DIR/../requirements.txt
 # pip uninstall -y torch torchvision torchaudio torchtext
 # pip install --pre --upgrade -f https://download.pytorch.org/whl/test/cu116/torch_test.html torch torchdata torchvision torchaudio torchtext
 
-# Install 2.0 nightly
-
-pip uninstall -y torch torchvision torchaudio torchtext torchdata
-#pip install --pre --upgrade -f https://download.pytorch.org/whl/test/cu118/torch_test.html torch torchdata torchvision torchaudio torchtext
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/test/cu117
-pip3 install torchdata torchtext --index-url https://download.pytorch.org/whl/test/cpu
-
 # Install two language tokenizers for Translation with TorchText tutorial
 python -m spacy download en_core_web_sm
 python -m spacy download de_core_news_sm
