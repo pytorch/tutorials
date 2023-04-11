93442503c258463dfa60748fef078336cd54a124
pytorchbot <soumith+bot@pytorch.org>
2023-04-11 17:46:37 +0000

    Automated tutorials push

--- _downloads/d2274535fcc404633095941d2fbbe536/captumyt.py
+++ _downloads/d2274535fcc404633095941d2fbbe536/captumyt.py
@@ -155,7 +155,7 @@ from matplotlib.colors import LinearSegmentedColormap
 # now.
 # 
 
-model = models.resnet101(pretrained=True)
+model = models.resnet101(weights='IMAGENET1K_V1')
 model = model.eval()
 
 
