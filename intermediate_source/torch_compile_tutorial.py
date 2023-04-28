039a26f3a72d8dd840ee28d4da0b3bc2600ac541
William Wen <williamwen@fb.com>
2023-04-28 22:19:45 +0000

    add note and check for GPU capability

--- intermediate_source/torch_compile_tutorial.py
+++ intermediate_source/torch_compile_tutorial.py
@@ -33,9 +33,25 @@ torch.compile Tutorial
 # - ``numpy``
 # - ``scipy``
 # - ``tabulate``
-#
-# Note: a modern NVIDIA GPU (Volta or Ampere) is recommended for this tutorial.
-#
+
+######################################################################
+# NOTE: a modern NVIDIA GPU (A100 or V100) is recommended for this tutorial in
+# order to reproduce the speedup numbers shown below and documented elsewhere.
+
+import torch
+import warnings
+
+gpu_ok = False
+if torch.cuda.is_available():
+    device = torch.cuda.get_device_name()
+    if "V100" in device or "A100" in device:
+        gpu_ok = True
+
+if not gpu_ok:
+    warnings.warn(
+        "GPU is not NVIDIA V100 or A100. Speedup numbers may be lower than "
+        "expected."
+    )
 
 ######################################################################
 # Basic Usage
@@ -50,8 +66,6 @@ torch.compile Tutorial
 # Arbitrary Python functions can be optimized by passing the callable to
 # ``torch.compile``. We can then call the returned optimized
 # function in place of the original function.
-
-import torch
 
 def foo(x, y):
     a = torch.sin(x)
