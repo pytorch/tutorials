f988562f0910ca7569b2e35757ddf02f2865583b
BeniaminC <benjamin.condrea@gmail.com>
2023-05-31 11:38:53 -0700

    Added matplotlib dependency to blitz tutorial.

--- beginner_source/blitz/cifar10_tutorial.py
+++ beginner_source/blitz/cifar10_tutorial.py
@@ -94,14 +94,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # functions to show an image
-
-
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
-
 
 # get some random training images
 dataiter = iter(trainloader)
