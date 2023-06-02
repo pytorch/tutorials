f460032606c38edd1e3521fd8a225791b69bc9a8
Catherine Lee <csl@fb.com>
2023-06-02 14:28:41 -0700

    update

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -1,10 +1,8 @@
+#!/bin/bash
+
 set -ex
 
-if [[ "$COMMIT_SOURCE" == master || "$COMMIT_SOURCE" == main ]]; then
-  export BUCKET_NAME=pytorch-tutorial-build-master
-else
-  export BUCKET_NAME=pytorch-tutorial-build-pull-request
-fi
+export BUCKET_NAME=pytorch-tutorial-build-pull-request
 
 # set locale for click dependency in spacy
 export LC_ALL=C.UTF-8
@@ -25,7 +23,7 @@ pip install -r $DIR/../requirements.txt
 # Nightly - pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
 # RC Link
 # pip uninstall -y torch torchvision torchaudio torchtext
-# pip install --pre --upgrade -f https://download.pytorch.org/whl/test/cu102/torch_test.html torch  torchvision torchaudio torchtext 
+# pip install --pre --upgrade -f https://download.pytorch.org/whl/test/cu102/torch_test.html torch  torchvision torchaudio torchtext
 # pip uninstall -y torch torchvision torchaudio torchtext
 # pip install --pre --upgrade -f https://download.pytorch.org/whl/test/cu116/torch_test.html torch torchdata torchvision torchaudio torchtext
 
@@ -37,8 +35,7 @@ awsv2 -i
 awsv2 configure set default.s3.multipart_threshold 5120MB
 
 # Decide whether to parallelize tutorial builds, based on $JOB_BASE_NAME
-export NUM_WORKERS=20
-if [[ "${JOB_BASE_NAME}" == *worker_* ]]; then
+if [[ "${JOB_TYPE}" == "worker" ]]; then
   # Step 1: Remove runnable code from tutorials that are not supposed to be run
   python $DIR/remove_runnable_code.py beginner_source/aws_distributed_training_tutorial.py beginner_source/aws_distributed_training_tutorial.py || true
   # python $DIR/remove_runnable_code.py advanced_source/ddp_pipeline_tutorial.py advanced_source/ddp_pipeline_tutorial.py || true
@@ -47,7 +44,7 @@ if [[ "${JOB_BASE_NAME}" == *worker_* ]]; then
   # python $DIR/remove_runnable_code.py intermediate_source/spatial_transformer_tutorial.py intermediate_source/spatial_transformer_tutorial.py || true
   # Temp remove for 1.10 release.
   # python $DIR/remove_runnable_code.py advanced_source/neural_style_tutorial.py advanced_source/neural_style_tutorial.py || true
-  
+
   # TODO: Fix bugs in these tutorials to make them runnable again
   # python $DIR/remove_runnable_code.py beginner_source/audio_classifier_tutorial.py beginner_source/audio_classifier_tutorial.py || true
 
@@ -56,7 +53,6 @@ if [[ "${JOB_BASE_NAME}" == *worker_* ]]; then
 
   # Step 2: Keep certain tutorials based on file count, and remove runnable code in all other tutorials
   # IMPORTANT NOTE: We assume that each tutorial has a UNIQUE filename.
-  export WORKER_ID=$(echo "${JOB_BASE_NAME}" | tr -dc '0-9')
   FILES_TO_RUN=$(python .jenkins/get_files_to_run.py)
   echo "FILES_TO_RUN: " ${FILES_TO_RUN}
 
@@ -117,25 +113,17 @@ if [[ "${JOB_BASE_NAME}" == *worker_* ]]; then
   # Step 6: Copy generated files to S3, tag with commit ID
   7z a worker_${WORKER_ID}.7z docs
   awsv2 s3 cp worker_${WORKER_ID}.7z s3://${BUCKET_NAME}/${COMMIT_ID}/worker_${WORKER_ID}.7z --acl public-read
-elif [[ "${JOB_BASE_NAME}" == *manager ]]; then
+elif [[ "${JOB_TYPE}" == "manager" ]]; then
   # Step 1: Generate no-plot HTML pages for all tutorials
   make html-noplot
   cp -r _build/html docs
 
   # Step 2: Wait for all workers to finish
-  set +e
-  for ((worker_id=0;worker_id<NUM_WORKERS;worker_id++)); do
-    until awsv2 s3api head-object --bucket ${BUCKET_NAME} --key ${COMMIT_ID}/worker_$worker_id.7z
-    do
-      echo "Waiting for worker $worker_id to finish..."
-      sleep 5
-    done
-  done
-  set -e
+  # Don't actually need to do this because gha will wait
 
   # Step 3: Download generated with-plot HTML files and static files from S3, merge into one folder
   mkdir -p docs_with_plot/docs
-  for ((worker_id=0;worker_id<NUM_WORKERS;worker_id++)); do
+  for ((worker_id=1;worker_id<NUM_WORKERS+1;worker_id++)); do
     awsv2 s3 cp s3://${BUCKET_NAME}/${COMMIT_ID}/worker_$worker_id.7z worker_$worker_id.7z
     7z x worker_$worker_id.7z -oworker_$worker_id
     yes | cp -R worker_$worker_id/docs/* docs_with_plot/docs
@@ -153,13 +141,13 @@ elif [[ "${JOB_BASE_NAME}" == *manager ]]; then
   awsv2 s3 cp manager.7z s3://${BUCKET_NAME}/${COMMIT_ID}/manager.7z --acl public-read
 
   # Step 7: push new HTML files and static files to gh-pages
-  if [[ "$COMMIT_SOURCE" == master || "$COMMIT_SOURCE" == main ]]; then
+  if [[ "$COMMIT_SOURCE" == "refs/heads/master" || "$COMMIT_SOURCE" == "refs/heads/main" ]]; then
     git clone https://github.com/pytorch/tutorials.git -b gh-pages gh-pages
     cp -r docs/* gh-pages/
     pushd gh-pages
     # DANGER! DO NOT REMOVE THE `set +x` SETTING HERE!
     set +x
-    git remote set-url origin https://${GITHUB_PYTORCHBOT_USERNAME}:${GITHUB_PYTORCHBOT_TOKEN}@github.com/pytorch/tutorials.git
+    git remote set-url origin https://pytorchbot:${GITHUB_PYTORCHBOT_TOKEN}@github.com/pytorch/tutorials.git
     set -x
     git add -f -A || true
     git config user.email "soumith+bot@pytorch.org"
