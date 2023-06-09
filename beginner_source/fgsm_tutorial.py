c9f1d2ff8b3b7bbdcab966a4078c4ce9cc94c3c4
Qasim Khan <straightouttadetroit.qk@gmail.com>
2023-06-09 14:52:17 +0500

    Update model weights link

--- beginner_source/fgsm_tutorial.py
+++ beginner_source/fgsm_tutorial.py
@@ -123,7 +123,7 @@ import matplotlib.pyplot as plt
 # -  ``pretrained_model`` - path to the pretrained MNIST model which was
 #    trained with
 #    `pytorch/examples/mnist <https://github.com/pytorch/examples/tree/master/mnist>`__.
-#    For simplicity, download the pretrained model `here <https://drive.google.com/drive/folders/1fn83DF14tWmit0RTKWRhPq5uVXt73e0h?usp=sharing>`__.
+#    For simplicity, download the pretrained model `here <https://drive.google.com/file/d/1HJV2nUHJqclXQ8flKvcWmjZ-OU5DGatl/view?usp=drive_link>`__.
 # 
 # -  ``use_cuda`` - boolean flag to use CUDA if desired and available.
 #    Note, a GPU with CUDA is not critical for this tutorial as a CPU will
