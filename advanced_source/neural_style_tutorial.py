671960cd883d798b3267e6c1dda774a2d35261f1
Onur Berk Töre <onurberk_t@hotmail.com>
2023-06-10 18:38:41 +0300

    Fix cpp_extension.rst
    Signed-off-by: Onur Berk Töre <onurberk_t@hotmail.com>

--- advanced_source/neural_style_tutorial.py
+++ advanced_source/neural_style_tutorial.py
@@ -14,7 +14,7 @@ This tutorial explains how to implement the `Neural-Style algorithm <https://arx
 developed by Leon A. Gatys, Alexander S. Ecker and Matthias Bethge.
 Neural-Style, or Neural-Transfer, allows you to take an image and
 reproduce it with a new artistic style. The algorithm takes three images,
-an input image, a content-image, and a style-image, and changes the input 
+an input image, a content-image, and a style-image, and changes the input
 to resemble the content of the content-image and the artistic style of the style-image.
 
  
@@ -107,8 +107,7 @@ loader = transforms.Compose([
 def image_loader(image_name):
     image = Image.open(image_name)
     # fake batch dimension required to fit network's input dimensions
-    image = loader(image).unsqueeze(0)
-    return image
+    return image.to(device, torch.float)
 
 
 style_img = image_loader("./data/images/neural-style/picasso.jpg")
