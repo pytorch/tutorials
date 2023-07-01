173538ae9ba3ed43cc4ad8523b4ef9fbe582d51c
Subham Sarkar <subhamsarkar@Subhams-MacBook-Air.local>
2023-07-01 14:38:48 +0530

    gramatically updated sentence

--- beginner_source/basics/tensorqs_tutorial.py
+++ beginner_source/basics/tensorqs_tutorial.py
@@ -15,7 +15,7 @@ Tensors
 Tensors are a specialized data structure that are very similar to arrays and matrices.
 In PyTorch, we use tensors to encode the inputs and outputs of a model, as well as the model’s parameters.
 
-Tensors are similar to `NumPy’s <https://numpy.org/>`_ ndarrays, except that tensors can run on GPUs or other hardware accelerators. In fact, tensors and
+Tensors are similar to `NumPy’s <https://numpy.org/>`_ ndarrays, except those tensors can run on GPUs or other hardware accelerators. In fact, tensors and
 NumPy arrays can often share the same underlying memory, eliminating the need to copy data (see :ref:`bridge-to-np-label`). Tensors
 are also optimized for automatic differentiation (we'll see more about that later in the `Autograd <autogradqs_tutorial.html>`__
 section). If you’re familiar with ndarrays, you’ll be right at home with the Tensor API. If not, follow along!
