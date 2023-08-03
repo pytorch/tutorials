1e0c60ed93e0a6a5a5cd10cc85d6833e12d3cb15
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2023-08-03 12:28:12 -0700

    Update on "[wip] Loading nn.Module from checkpoint tutorial"
    [ghstack-poisoned]

--- recipes_source/recipes/module_load_state_dict_tips.py
+++ recipes_source/recipes/module_load_state_dict_tips.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 """
 
-Tips for Loading an nn.Module from a Checkpoint
-===============================================
+Tips for Loading an `nn.Module` from a Checkpoint
+=================================================
 
 In this tutorial, we will share some tips for loading a model from a checkpoint.
 In particular, we will discuss
 
 
-1.   The `mmap` kwarg on `torch.load`
-2.   The `torch.device()` context manager
-3.  The `assign` kwarg on `nn.Module.load_state_dict()`
+1.  The `mmap` keyword argument on `torch.load`
+2.  The `torch.device()` context manager
+3.  The `assign` keyword argument on `nn.Module.load_state_dict()`
 
 The following snippet of code illustrates the use of the above three utilities.
 """
@@ -33,7 +32,7 @@ m.load_state_dict(state_dict)
 ###############################################################################
 # At `torch.save` time, tensor storages are tagged with the device they are
 # saved on. At `torch.load` time, tensor storages will be loaded to the device
-# they were tagged with (unless this behavior is overriden using the
+# they were tagged with (unless this behavior is overridden using the
 # `map_location` flag). For ease of explanation, let us assume that the tensors
 # were saved on CPU. This means that on the first line
 
@@ -45,12 +44,12 @@ state_dict = torch.load('checkpoint.pth')
 #     2. The user does not want to wait for the entire checkpoint to be loaded
 #        into RAM before doing for example some per-tensor processing
 #
-# The `mmap` kwarg to `torch.load` attempts to solve the above two problems by
-# using an [mmap call](https://man7.org/linux/man-pages/man2/mmap.2.html) on
-# the checkpoint, so that tensor storages are memory-mapped and when they are
+# The `mmap` keyword argument to `torch.load` attempts to solve the above two
+# problems by using an [`mmap` call](https://man7.org/linux/man-pages/man2/mmap.2.html)
+# on the checkpoint, so that tensor storages are memory-mapped and when they are
 # fetched from disk to memory is managed by the OS.
 #
-# Next, we consider the instantiation of the module.
+# Next, we consider the creation of the module.
 
 m = SomeModule()
 
@@ -75,7 +74,6 @@ with torch.device('meta'):
 # context manager makes sure that factory calls will be performed as if they
 # were passed device as an argument. However, it does not affect factory
 # function calls which are called with an explicit device argument.
-# TODO: maybe include recent PR to override part 2 with TorchFunctionMode
 #
 # Tensors on the `meta` device do not carry data. However, they possess all
 # other metadata a tensor carries such as `.size()` and `.stride()`,
@@ -92,7 +90,7 @@ m.load_state_dict(state_dict)
 # the parameters/buffers in the `nn.Module`).
 #
 # However, an in-place copy into a tensor on the `meta` device is a no-op.
-# In order to avoid this, we can pass the `assign=True` kwarg to
+# In order to avoid this, we can pass the `assign=True` keyword argument to
 # `load_state_dict()`.
 
 meta_m.load_state_dict(state_dict, assign=True)
@@ -103,7 +101,7 @@ meta_m.load_state_dict(state_dict, assign=True)
 # is loaded from state dict if `assign=True` is passed.
 
 ###############################################################################
-# To recap, in this tutorial, we learnt about `torch.load(mmap=True)`, the
+# To recap, in this tutorial, we learned about `torch.load(mmap=True)`, the
 # `torch.device()` context manager with `device=meta` and the
 # `nn.Module.load_state_dict(assign=True)` and how these tools could be used
 # to aid when loading a model from a checkpoint.
