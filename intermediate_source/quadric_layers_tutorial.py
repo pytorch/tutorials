6608422cdd738852fb900cb9cc48a8763b225082
Dirk Roeckmann <57329722+diro5t@users.noreply.github.com>
2023-08-04 11:45:13 -0400

    Update quadric_layers_tutorial.py

--- intermediate_source/quadric_layers_tutorial.py
+++ intermediate_source/quadric_layers_tutorial.py
@@ -54,6 +54,95 @@ for index in range(1, img_num + 1):
     plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
 
 ######################################################################
+# 2. Introduce quadric layer
+# NOTE: If quadric layers are part of torch.nn in the future, this defintion is not necessary anymore
+# -------------------
+
+class Quadric(nn.Module):
+    r"""Applies a quadric transformation to the incoming data: :math:`y = x^2A^T + xB^T + b`
+
+    This module supports :ref:`TensorFloat32<tf32_on_ampere>`.
+
+    On certain ROCm devices, when using float16 inputs this module will use :ref:`different precision<fp16_on_mi200>` for backward.
+
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        bias: If set to ``False``, the layer will not learn an additive bias.
+            Default: ``True``
+
+    Shape:
+        - Input: :math:`(*, H_{in})` where :math:`*` means any number of
+          dimensions including none and :math:`H_{in} = \text{in\_features}`.
+        - Output: :math:`(*, H_{out})` where all but the last dimension
+          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
+
+    Attributes:
+        qweight: the learnable quadratic weights of the module of shape
+            :math:`(\text{out\_features}, \text{in\_features})`. The values are
+            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
+            :math:`k = \frac{1}{\text{in\_features}}`
+        weight: the learnable linear weights of the module of shape
+            :math:`(\text{out\_features}, \text{in\_features})`. The values are
+            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
+            :math:`k = \frac{1}{\text{in\_features}}`
+        bias:   the learnable bias of the module of shape :math:`(\text{out\_features})`.
+                If :attr:`bias` is ``True``, the values are initialized from
+                :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
+                :math:`k = \frac{1}{\text{in\_features}}`
+
+    Examples::
+
+        >>> m = nn.Quadric(20, 30)
+        >>> input = torch.randn(128, 20)
+        >>> output = m(input)
+        >>> print(output.size())
+        torch.Size([128, 30])
+    """
+    __constants__ = ['in_features', 'out_features']
+    in_features: int
+    out_features: int
+    qweight: Tensor
+    lweight: Tensor
+
+    def __init__(self, in_features: int, out_features: int, bias: bool = True,
+                 device=None, dtype=None) -> None:
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.qweight = Parameter(torch.empty((out_features, in_features), **factory_kwargs))
+        self.lweight = Parameter(torch.empty((out_features, in_features), **factory_kwargs))
+        if bias:
+            self.bias = Parameter(torch.empty(out_features, **factory_kwargs))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
+        # https://github.com/pytorch/pytorch/issues/57109
+        nn.init.kaiming_uniform_(self.qweight, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.lweight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in_q, _ = nn.init._calculate_fan_in_and_fan_out(self.qweight)
+            fan_in_l, _ = nn.init._calculate_fan_in_and_fan_out(self.lweight)
+            bound = 1 / math.sqrt(fan_in_l) if fan_in_l > 0 else 0
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input: Tensor) -> Tensor:
+        input_sqr = torch.mul(input, input)
+        q = nn.functional.linear(input_sqr, self.qweight, None)
+        lb = nn.functional.linear(input, self.lweight, self.bias)
+        return torch.add(q, lb)
+
+    def extra_repr(self) -> str:
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
+
+######################################################################
 # 2. Define the model
 # -------------------
 #
