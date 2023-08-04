4e1b184194fde41b1a9f898b560d57130ec3b804
Dirk Roeckmann <57329722+diro5t@users.noreply.github.com>
2023-08-04 10:26:00 -0400

    Update quadric_layers_tutorial.py

--- intermediate_source/quadric_layers_tutorial.py
+++ intermediate_source/quadric_layers_tutorial.py
@@ -7,10 +7,17 @@
 Introduction
 ------------
 
-Quadric layers are drop-in layers for linear layers which introduce quadratic functions (quadric). 
-Hereby second-order decision boundaries are achieved, which can in many applications
+Quadric layers introduce quadratic functions with second-order decision boundaries (quadric hypersurfaces)
+and can be used as 100% drop-in layers for linear layers (torch.nn.Linear) and present a high-level means
+to reduce overall model size.
 
-In this tutorial, we will apply the easiest form of quantization -
+In comparison to linear layers with n weights and 1 bias (if needed) per neuron, a quadric neuron consists of
+2n weights (n quadratic weights and n linear weights) and 1 bias (if needed).
+Although this means a doubling in weights per neuron, the more powerful decision boundaries per neuron lead 
+in many applications to siginificantly less neurons per layer or even less layers and in total to less model parameters.
+
+In this tutorial, I will present a trivial example - binary classification of circular data using a quadric layer 
+with just one neuron.
 `dynamic quantization <https://pytorch.org/docs/stable/quantization.html#torch.quantization.quantize_dynamic>`_ -
 to an LSTM-based next word-prediction model, closely following the
 `word language model <https://github.com/pytorch/examples/tree/master/word_language_model>`_
