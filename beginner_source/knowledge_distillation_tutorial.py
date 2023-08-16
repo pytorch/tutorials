de2d4ef9adc776d129044c87ff17363b0e27a3b5
AlexandrosChrtn <56091961+AlexandrosChrtn@users.noreply.github.com>
2023-08-16 23:03:36 +0300

    Update beginner_source/knowledge_distillation_tutorial.py
    Co-authored-by: Svetlana Karslioglu <svekars@meta.com>

--- beginner_source/knowledge_distillation_tutorial.py
+++ beginner_source/knowledge_distillation_tutorial.py
@@ -65,7 +65,7 @@ transforms_cifar = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-# Load CIFAR-10 dataset
+# Loading the CIFAR-10 dataset:
 train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms_cifar)
 test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms_cifar)
 
