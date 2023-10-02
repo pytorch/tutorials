d1c8e323a38a235e68d9e05d654551c79f7ed905
angelayi <yiangela7@gmail.com>
2023-10-02 14:42:35 -0700

    v2

--- prototype_source/aot_inductor_tutorial.py
+++ prototype_source/aot_inductor_tutorial.py
@@ -16,19 +16,19 @@ AOTInductor Tutorial
 #
 # AOTInductor is a specialized version of
 # `TorchInductor <https://dev-discuss.pytorch.org/t/torchinductor-a-pytorch-native-compiler-with-define-by-run-ir-and-symbolic-shapes/747>`__
-# which take an exported PyTorch model, optimizes it, and generates a shared
+# which takes an exported PyTorch model, optimizes it, and generates a shared
 # library. These compiled artifacts can be deployed to non-Python environments,
 # which are commonly used for inference deployments.
 #
 # AOTInductor is a vital component along the
-# `export <https://pytorch.org/docs/2.1/export.html>`__ path as it provides a way
+# `export <https://pytorch.org/docs/main/export.html>`__ path as it provides a way
 # to run an exported model without a Python runtime.
 #
 # In this tutorial, you will learn how take a PyTorch model, export it to a
-# compilation graph, pass it to AOTInductor to be compiled shared binary, and
-# run this binary.
+# compilation graph, pass it to AOTInductor to be compiled into a shared binary,
+# and run this binary.
 #
-# Note: a modern NVIDIA GPU (H100, A100, or V100) is recommended for this
+# ..note:: a modern NVIDIA GPU (H100, A100, or V100) is recommended for this
 # tutorial in order to reproduce the speedup numbers shown below and documented
 # elsewhere.
 
@@ -53,17 +53,19 @@ AOTInductor Tutorial
 #   ) -> Tuple[str, bytes]
 #
 # This function does the following:
+#
 #    1. Exports the given model into a compilation graph through the
-#      `torch.export <https://pytorch.org/docs/2.1/export.html>`__ workflow. If
+#      `torch.export <https://pytorch.org/docs/main/export.html>`__ workflow. If
 #      there are any errors when exporting the model, please refer to the
-#      `torch.export <https://pytorch.org/docs/2.1/export.html>`__ documentation.
+#      `torch.export <https://pytorch.org/docs/main/export.html>`__ documentation.
 #    2. Compiles the exported program using TorchInductor into a shared library.
 #    3. Returns the path to the shared library.
 #
 
 import torch
-from torch.export import export
+import torch._export
 import torch.utils._pytree as pytree
+from typing import Any, Tuple, Mapping
 
 class M(torch.nn.Module):
     def __init__(self) -> None:
@@ -75,13 +77,19 @@ class M(torch.nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.branch1(x)
 
+def _normalize_inputs(example_inputs) -> Tuple[Tuple[Any], Mapping[str, Any]]:
+    if isinstance(example_inputs, dict):
+        return (), example_inputs
+    else:
+        return example_inputs, {}
+
 with torch.no_grad():
     m = M().to("cuda")
-    example_args = (torch.randn(32, 64, device="cuda"),)
-    example_outputs = m(*example_args)
+    example_inputs = (torch.randn(32, 64, device="cuda"),)
+    example_args, example_kwargs = _normalize_inputs(example_inputs)
 
     so_path, exported_program = torch._export.aot_compile(
-        m, args=example_args
+        m, args=example_args, kwargs=example_kwargs
     )
 
 print(".so path:")
@@ -104,7 +112,7 @@ print()
 # Running the .so
 # ---------------
 #
-# To run the .so, we can import ``aot_inductor_launcher`` from
+# To run the ``.so``, we can import ``aot_inductor_launcher`` from
 # ``torch._inductor.utils`` which includes some starter C++ code to launch the
 # program.
 #
@@ -114,10 +122,6 @@ print()
 
 from torch._inductor.utils import aot_inductor_launcher
 
-is_cpu = any(x.device.type == "cpu" for x in example_args)
-if is_cpu:
-    aot_inductor_launcher = aot_inductor_launcher.replace("false /*is_cpu*/", "true /*is_cpu*/")
-
 aot_inductor_model_container = torch.utils.cpp_extension.load_inline(
     name="aot_inductor",
     cpp_sources=[aot_inductor_launcher],
@@ -126,12 +130,12 @@ aot_inductor_model_container = torch.utils.cpp_extension.load_inline(
     with_cuda=True,
 )
 
-def run_container(example_args):
-    flat_args, _ = pytree.tree_flatten(example_args)
-    flat_outputs = aot_inductor_model_container.run(flat_args)
+def run_container(example_inputs):
+    flat_example_inputs, _ = pytree.tree_flatten(example_inputs)
+    flat_outputs = aot_inductor_model_container.run(flat_example_inputs)
     outputs = pytree.tree_unflatten(flat_outputs, exported_program.call_spec.out_spec)
     return outputs
 
-example_args = (torch.randn(32, 64, device="cuda"),)
-run_container(*example_args)
+example_inputs = (torch.randn(32, 64, device="cuda"),)
+run_container(*example_inputs)
 
