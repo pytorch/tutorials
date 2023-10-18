586cd51137b6e5cc6dabbe2fb3f8c413e4303fee
aniketh reddy <anikethreddy3@gmail.com>
2023-10-18 19:42:16 -0400

    made the suggested changes to cifar10_tutorial.py given for image

--- beginner_source/blitz/cifar10_tutorial.py
+++ beginner_source/blitz/cifar10_tutorial.py
@@ -32,7 +32,7 @@ It has the classes: ‘airplane’, ‘automobile’, ‘bird’, ‘cat’, ‘
 ‘dog’, ‘frog’, ‘horse’, ‘ship’, ‘truck’. The images in CIFAR-10 are of
 size 3x32x32, i.e. 3-channel color images of 32x32 pixels in size.
 
-![](../_static/img/cifar10.png)
+<img src="../_static/img/cifar10.png" alt="cifar10.png">
 
    cifar10
 
