a92ddf46c7422cef989d3bba95ec4eb5b4172041
Svetlana Karslioglu <svekars@meta.com>
2023-12-15 13:04:57 -0800

    Temporarily add tutorials survey on all pages(#2715)
    * Add 2023 tutorials survey

--- conf.py
+++ conf.py
@@ -285,7 +285,8 @@ texinfo_documents = [
 
 html_css_files = [
         'https://cdn.jsdelivr.net/npm/katex@0.10.0-beta/dist/katex.min.css',
-        'css/custom.css'
+        'css/custom.css',
+        'css/custom2.css'
     ]
 
 def setup(app):
