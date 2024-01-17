0c9f2b82ce29884a2c182298674ac026010df1f7
pytorchbot <soumith+bot@pytorch.org>
2024-01-17 23:25:36 +0000

    Automated tutorials push

--- _downloads/176a0eb17083f2a53b7e388098a15704/dcgan_faces_tutorial.py
+++ _downloads/176a0eb17083f2a53b7e388098a15704/dcgan_faces_tutorial.py
@@ -265,7 +265,7 @@ plt.figure(figsize=(8,8))
 plt.axis("off")
 plt.title("Training Images")
 plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-
+plt.show()
 
 
 ######################################################################
