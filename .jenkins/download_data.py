2fb562fb116bea916a96f08ceab6fc3d546fce3d
Svetlana Karslioglu <svekars@meta.com>
2024-01-25 09:05:46 -0800

    Update

--- .jenkins/download_data.py
+++ .jenkins/download_data.py
@@ -107,7 +107,7 @@ def download_lenet_mnist() -> None:
 
 def download_gpu_quantization_torchao() -> None:
     # Download SAM model checkpoint for prototype_source/gpu_quantization_torchao_tutorial.py
-    z = downloaad_url_to_file("https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
+    z = download_url_to_file("https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
                          prefix=DATA_DIR,
                          sha256="a7bf3b02f3ebf1267aba913ff637d9a2d5c33d3173bb679e46d9f338c26f262e",
                          )
