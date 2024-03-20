e3e6e0c33329d012af0cc3621ed00cf341d14c5a
Svetlana Karslioglu <svekars@meta.com>
2024-03-20 11:00:47 -0700

    Update build.sh

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -30,7 +30,8 @@ pip install --progress-bar off -r $DIR/../requirements.txt
 # Nightly - pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
 # Install 2.2 for testing - uncomment to install nightly binaries (update the version as needed).
 pip uninstall -y torch torchvision torchaudio torchtext torchdata
-pip3 install torch==2.3.0 torchvision torchaudio torchtext --no-cache-dir --index-url https://download.pytorch.org/whl/test/cu121 
+pip3 install torch==2.3.0 torchvision torchaudio --no-cache-dir --index-url https://download.pytorch.org/whl/test/cu121 
+pip3 install torchtext --index-url https://download.pytorch.org/whl/test/cpu 
 
 # Install two language tokenizers for Translation with TorchText tutorial
 python -m spacy download en_core_web_sm
