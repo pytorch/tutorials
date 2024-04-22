090cb697e56f51f353055b70ed9f2e85616e2274
Jesse Cai <jcjessecai@gmail.com>
2024-04-22 10:59:23 -0700

    update max_steps

--- advanced_source/semi_structured_sparse.py
+++ advanced_source/semi_structured_sparse.py
@@ -433,6 +433,8 @@ training_args = transformers.TrainingArguments(
     per_device_train_batch_size=32,
     per_device_eval_batch_size=256,
     logging_steps=50, 
+    # Limit max steps for tutorial runnners. Delete the below line to see the final accuracy numbers. 
+    max_steps=500,
     report_to=None,
 )
 
