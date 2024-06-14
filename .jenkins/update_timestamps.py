05e3818b837a745e3b1e3caf320f39d4c0400883
Benjamin Collins <sstben@gmail.com>
2024-06-14 09:40:26 -0600

    Updating timestamp line style and text
    Changed date formatting to work with datetime

--- .jenkins/update_timestamps.py
+++ .jenkins/update_timestamps.py
@@ -1,7 +1,9 @@
 import re
 import sys
 import os
+import datetime
 import subprocess
+
 
 def get_last_commit_timestamp_for_file(file_path: str) -> str:
     """Get the last commit timestamp for a file.
@@ -12,15 +14,25 @@ def get_last_commit_timestamp_for_file(file_path: str) -> str:
     Returns:
         str: Last committed timestamp string
     """
-    git_command = ["git", "log", "-1", '--date=format:%B %d, %Y', "--format=%at", "--", file_path]
+
+    git_command = [
+        "git",
+        "log",
+        "-1",
+        "--date=format:%B %d, %Y",
+        "--format=%at",
+        "--",
+        file_path,
+    ]
     timestamp = subprocess.check_output(git_command).decode().strip()
 
     if not timestamp:
         # If there is no git commit history, use last modified date
         timestamp = str(int(os.path.getmtime(file_path)))
 
-    date_command = ["date", "-d", "@" + timestamp, "+%I:%M %p, %B %d, %Y"]
-    return subprocess.check_output(date_command).decode().strip()
+    dt = datetime.datetime.fromtimestamp(int(timestamp), tz=datetime.timezone.utc)
+    return dt.strftime("%I:%M %p, %B %d, %Y")
+
 
 def update_timestamp(file_path: str):
     """Adds a timestamp of the most recent time the file was edited.
@@ -28,13 +40,18 @@ def update_timestamp(file_path: str):
     Args:
         file_path (str): Path to file
     """
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         lines = file.readlines()
 
     # Get current timestamp
     timestamp = get_last_commit_timestamp_for_file(file_path)
-    timestamp_line = f'{"# " if file_path.endswith("py") else ""}**Updated:** *{timestamp}*\n'
-    timestamp_pattern = r'\*\*Updated:\*\*\s\**\d{1,2}:\d{2} [AP]M, \w+ \d{1,2}, \d{4}\*'
+    if not timestamp:
+        return
+
+    timestamp_line = (
+        f'{"# " if file_path.endswith("py") else ""}_Last Updated: {timestamp}_\n'
+    )
+    timestamp_pattern = r"_Last Updated:\s.+_"
 
     if not lines:
         lines = [timestamp_line]
@@ -46,10 +63,10 @@ def update_timestamp(file_path: str):
         if re.search(timestamp_pattern, lines[i]):
             lines[i] = timestamp_line
         else:
-            lines.append('\n\n' + timestamp_line)
-    
+            lines.append("\n\n" + timestamp_line)
+
     # Write updated lines back to file
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         file.writelines(lines)
 
 
