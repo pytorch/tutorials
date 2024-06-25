2b0e4649f2d88f29160d2e4050195c33f613882a
Svetlana Karslioglu <svekars@meta.com>
2024-06-25 13:46:16 -0700

    Disable failing tutorials 2.4 (#2955)

--- .jenkins/validate_tutorials_built.py
+++ .jenkins/validate_tutorials_built.py
@@ -54,7 +54,10 @@ NOT_RUN = [
     "recipes_source/recipes/Captum_Recipe",
     "intermediate_source/flask_rest_api_tutorial",
     "intermediate_source/text_to_speech_with_torchaudio",
-    "intermediate_source/tensorboard_profiler_tutorial" # reenable after 2.0 release.
+    "intermediate_source/tensorboard_profiler_tutorial", # reenable after 2.0 release.
+    "intermediate_source/inductor_debug_cpu", # reenable after 2942 
+    "beginner_source/onnx/onnx_registry_tutorial", # reenable after 2941 is fixed.
+    "intermediate_source/torch_export_tutorial" # reenable after 2940 is fixed.
 ]
 
 def tutorial_source_dirs() -> List[Path]:
