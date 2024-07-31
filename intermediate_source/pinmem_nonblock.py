a04d09e8e6b3654e18427e27534da8cdd91db909
Vincent Moens <vmoens@meta.com>
2024-07-31 18:45:59 -0400

    fix after -> before

--- intermediate_source/pinmem_nonblock.py
+++ intermediate_source/pinmem_nonblock.py
@@ -556,7 +556,7 @@ except AssertionError:
 # CUDA-enabled device.
 #
 # In summary, copying data from CPU to GPU is safe when using ``non_blocking=True``, but for any other direction,
-# ``non_blocking=True`` can still be used but the user must make sure that a device synchronization is executed after
+# ``non_blocking=True`` can still be used but the user must make sure that a device synchronization is executed before
 # the data is accessed.
 #
 # Practical recommendations
