cf3de687a83b8a1601e8cb3c5db530f746363e55
Matthew Schultz <schultz.home@gmail.com>
2024-09-13 14:39:57 -0400

    based on Joel's review of Sept 9th: removing NameData object, combining all RNN definition into one, moving RNN.learn() to separate train()

--- intermediate_source/char_rnn_classification_tutorial.py
+++ intermediate_source/char_rnn_classification_tutorial.py
@@ -74,44 +74,28 @@ print(f"Using device = {torch.get_default_device()}")
 # line, mostly romanized (but we still need to convert from Unicode to
 # ASCII).
 #
-# The first thing we need to define is our data items. In this case, we will create a class called NameData 
-# which will have an __init__ function to specify the input fields and some helper functions. Our first 
-# helper function will be __str__ to convert objects to strings for easy printing 
-#
-# There are two key pieces of this that we will flesh out over the course of this tutorial. First is the basic data 
-# object which a label and some text. In this instance, label = the country of origin and text = the name. 
-#
-# However, our data has some issues that we will need to clean up. First off, we need to convert Unicode to plain ASCII to 
+# The first thing we need to define and clean our data. First off, we need to convert Unicode to plain ASCII to 
 # limit the RNN input layers. This is accomplished by converting Unicode strings to ASCII and allowing a small set of allowed characters (allowed_characters)
 
 import string 
 import unicodedata
 
-class NameData:
-    allowed_characters = string.ascii_letters + " .,;'"
-    n_letters = len(allowed_characters) 
+allowed_characters = string.ascii_letters + " .,;'"
+n_letters = len(allowed_characters) 
 
-
-    def __init__(self, label, text):
-        self.label = label
-        self.text = NameData.unicodeToAscii(text) 
-    
-    def __str__(self):
-        return f"label={self.label}, text={self.text}"
-
-    # Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427    
-    def unicodeToAscii(s):
-        return ''.join(
-            c for c in unicodedata.normalize('NFD', s)
-            if unicodedata.category(c) != 'Mn'
-            and c in NameData.allowed_characters
-        )
+# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427    
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in allowed_characters
+    )
 
 #########################
-#Now we can use that class to create a singe piece of data.
+# Here's an example of converting a unicode alphabet name to plain ASCII. This simplifies the input layer
 #
 
-print (f"{NameData(label='Polish', text='Ślusàrski')}")
+print (f"converting 'Ślusàrski' to {unicodeToAscii('Ślusàrski')}")
 
 ######################################################################
 # Turning Names into Tensors
@@ -129,50 +113,24 @@ print (f"{NameData(label='Polish', text='Ślusàrski')}")
 #
 # That extra 1 dimension is because PyTorch assumes everything is in
 # batches - we're just using a batch size of 1 here.
-#
-# For this, you'll need to add a couple of capabilities to our NameData object.
 
-import string 
-import unicodedata
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return allowed_characters.find(letter)
 
-class NameData:
-    allowed_characters = string.ascii_letters + " .,;'"
-    n_letters = len(allowed_characters) 
-
-
-    def __init__(self, label, text):
-        self.label = label
-        self.text = NameData.unicodeToAscii(text) 
-        self.tensor =  NameData.lineToTensor(self.text) 
-    
-    def __str__(self):
-        return f"label={self.label}, text={self.text}\ntensor = {self.tensor}"
-
-    # Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427    
-    def unicodeToAscii(s):
-        return ''.join(
-            c for c in unicodedata.normalize('NFD', s)
-            if unicodedata.category(c) != 'Mn'
-            and c in NameData.allowed_characters
-        )
-
-    # Find letter index from all_letters, e.g. "a" = 0
-    def letterToIndex(letter):
-        return NameData.allowed_characters.find(letter)
-
-    # Turn a line into a <line_length x 1 x n_letters>,
-    # or an array of one-hot letter vectors
-    def lineToTensor(line):
-        tensor = torch.zeros(len(line), 1, NameData.n_letters)
-        for li, letter in enumerate(line):
-            tensor[li][0][NameData.letterToIndex(letter)] = 1
-        return tensor
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
 
 #########################
-# Here are some examples of how to use the NameData object
+# Here are some examples of how to use lineToTensor() for a single and multiple character string.
 
-print (f"{NameData(label='none', text='a')}")
-print (f"{NameData(label='Korean', text='Ahn')}")
+print (f"The letter 'a' becomes {lineToTensor('a')}") #notice that the first position in the tensor = 1
+print (f"The name 'Ahn' becomes {lineToTensor('Ahn')}") #notice 'A' sets the 27th index to 1
 
 #########################
 # Congratulations, you have built the foundational tensor objects for this learning task! You can use a similar approach 
@@ -181,12 +139,9 @@ print (f"{NameData(label='Korean', text='Ahn')}")
 # Next, we need to combine all our examples into a dataset so we can train, text and validate our models. For this, 
 # we will use the `Dataset and DataLoader <https://pytorch.org/tutorials/beginner/basics/data_tutorial.html>` classes 
 # to hold our dataset. Each Dataset needs to implement three functions: __init__, __len__, and __getitem__. 
-
 from io import open
 import glob
 import os
-import unicodedata
-import string
 import time 
 
 import torch
@@ -200,6 +155,9 @@ class NamesDataset(Dataset):
         labels_set = set() #set of all classes
 
         self.data = []
+        self.data_tensors = []
+        self.labels = [] 
+        self.labels_tensors = [] 
 
         #read all the txt files in the specified directory
         text_files = glob.glob(os.path.join(data_dir, '*.txt'))                           
@@ -208,22 +166,30 @@ class NamesDataset(Dataset):
             labels_set.add(label)
             lines = open(filename, encoding='utf-8').read().strip().split('\n')
             for name in lines: 
-                self.data.append(NameData(label=label, text=name))
+                self.data.append(name)
+                self.data_tensors.append(lineToTensor(name))
+                self.labels.append(label)
 
-        self.labels = list(labels_set)
+        #Cache the tensor representation of the labels 
+        self.labels_uniq = list(labels_set)
+        for idx in range(len(self.labels)):
+            temp_tensor = torch.tensor([self.labels_uniq.index(self.labels[idx])], dtype=torch.long)
+            self.labels_tensors.append(temp_tensor)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         data_item = self.data[idx]
-        label_tensor = torch.tensor([self.labels.index(data_item.label)], dtype=torch.long)
-        return label_tensor, data_item.tensor, data_item.label, data_item.text
-    
+        data_label = self.labels[idx]
+        data_tensor = self.data_tensors[idx]
+        label_tensor = self.labels_tensors[idx] 
+
+        return label_tensor, data_tensor, data_label, data_item
+
 
 #########################
-#Here are some examples of how to use the NamesDataset object
-
+#Here we can load our example data into the NamesDataset
 
 alldata = NamesDataset("data/names")
 print(f"loaded {len(alldata)} items of data")
@@ -257,51 +223,8 @@ print(f"train examples = {len(train_set)}, validation examples = {len(test_set)}
 # which operate on an input and hidden state, with a ``LogSoftmax`` layer 
 # after the output.s
 #
-
-import torch.nn as nn
-import torch.nn.functional as F
-
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_labels):
-        super(RNN, self).__init__()
-
-        self.hidden_size = hidden_size
-        self.output_labels = output_labels
-
-        self.i2h = nn.Linear(input_size, hidden_size)
-        self.h2h = nn.Linear(hidden_size, hidden_size)
-        self.h2o = nn.Linear(hidden_size, len(output_labels))
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        hidden = F.tanh(self.i2h(input) + self.h2h(hidden))
-        output = self.h2o(hidden)
-        output = self.softmax(output)
-        return output, hidden
-
-###########################
-#We can then create a RNN with 128 hidden nodes and given our datasets
-
-
-n_hidden = 128
-rnn = RNN(NameData.n_letters, n_hidden, alldata.labels)
-print(rnn) 
-
-######################################################################
-# To run a step of this network we need to pass a single character input 
-# and a hidden state (which we initialize as zeros at first). We'll get to 
-# multi-character names next
-
-input = NameData(label='none', text='A').tensor
-output, next_hidden = rnn(input[0], torch.zeros(1, n_hidden))
-print(output) 
-
-######################################################################
-# Scoring Multi-character names 
-# --------------------
-# Multi-character names require just a little bit more effort which is 
-# keeping track of the hidden output and passing it back into the RNN. 
-# You can see this updated work defined in the function forward()
+# forward() loops through each of the characters in the given tensor, computes each
+# layer and then passes the hidden layer onto to the next iteration.
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -330,23 +253,27 @@ class RNN(nn.Module):
 
         return output
 
-    def label_from_output(self, output):
-        top_n, top_i = output.topk(1)
-        label_i = top_i[0].item()
-        return self.output_labels[label_i], label_i
-
 
 ###########################
-#Now we can score the output for names!
-
+#We can then create a RNN with 128 hidden nodes and given our datasets
 
 n_hidden = 128
-rnn = RNN(NameData.n_letters, n_hidden, alldata.labels)
+rnn = RNN(n_letters, n_hidden, alldata.labels_uniq)
+print(rnn) 
 
-input = NameData(label='none', text='Albert').tensor
+######################################################################
+# We can then pass our Tensor to the runn to get a predicted output and 
+# use a helper function, label_from_output, to get a text label for the class.
+
+def label_from_output(output, output_labels):
+    top_n, top_i = output.topk(1)
+    label_i = top_i[0].item()
+    return output_labels[label_i], label_i
+
+input = lineToTensor('Albert')
 output = rnn(input) #this is equivalent to output = rnn.forward(input)
 print(output) 
-print(rnn.label_from_output(output))
+print(label_from_output(output, alldata.labels_uniq))
 
 ######################################################################
 #
@@ -374,95 +301,61 @@ print(rnn.label_from_output(output))
 # -  Back-propagate
 # -  Return the output and loss
 #
-# We also define a learn() function which trains on a given dataset with minibatches
+# We do this by defining a learn() function which trains on a given dataset with minibatches
 
-import torch.nn as nn
-import torch.nn.functional as F
 import random 
 import numpy as np 
 
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_labels):
-        super(RNN, self).__init__()
+def train(rnn, training_data, n_epoch = 250, n_batch_size = 64, report_every = 50, learning_rate = 0.005, criterion = nn.NLLLoss()):
+    """
+    Learn on a batch of training_data for a specified number of iterations and reporting thresholds
+    """
+    # Keep track of losses for plotting
+    current_loss = 0
+    all_losses = []
+    rnn.train() 
+    optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
 
-        self.hidden_size = hidden_size
-        self.output_labels = output_labels
+    start = time.time()
+    print(f"training on data set with n = {len(training_data)}")
 
-        self.i2h = nn.Linear(input_size, hidden_size)
-        self.h2h = nn.Linear(hidden_size, hidden_size)
-        self.h2o = nn.Linear(hidden_size, len(output_labels))
-        self.softmax = nn.LogSoftmax(dim=1)
-    
-    def forward(self, line_tensor):
-        hidden = torch.zeros(1, rnn.hidden_size)
-        output = torch.zeros(1, len(self.output_labels))
+    for iter in range(1, n_epoch + 1): 
+        rnn.zero_grad() # clear the gradients 
 
-        for i in range(line_tensor.size()[0]):
-            input = line_tensor[i]
-            hidden = F.tanh(self.i2h(input) + self.h2h(hidden))
-            output = self.h2o(hidden)
-            output = self.softmax(output)
+        # create some minibatches
+        # we cannot use dataloaders because each of our names is a different length
+        batches = list(range(len(training_data)))
+        random.shuffle(batches)
+        batches = np.array_split(batches, len(batches) //n_batch_size )
 
-        return output
+        for idx, batch in enumerate(batches): 
+            batch_loss = 0
+            for i in batch: #for each example in this batch
+                (label_tensor, text_tensor, label, text) = training_data[i]
+                output = rnn.forward(text_tensor)
+                loss = criterion(output, label_tensor)
+                batch_loss += loss
 
-    def label_from_output(self, output):
-        top_n, top_i = output.topk(1)
-        label_i = top_i[0].item()
-        return self.output_labels[label_i], label_i
-    
-    def learn(self, training_data, n_epoch = 250, n_batch_size = 64, report_every = 50, learning_rate = 0.005, criterion = nn.NLLLoss()):
-        """
-        Learn on a batch of training_data for a specified number of iterations and reporting thresholds
-        """
-        # Keep track of losses for plotting
-        current_loss = 0
-        all_losses = []
-        self.train() 
-        optimizer = torch.optim.SGD(self.parameters(), lr=learning_rate)
+            # optimize parameters
+            batch_loss.backward()
+            nn.utils.clip_grad_norm_(rnn.parameters(), 3)
+            optimizer.step()
+            optimizer.zero_grad()
 
-        start = time.time()
-        print(f"training on data set with n = {len(training_data)}")
-
-        for iter in range(1, n_epoch + 1): 
-            self.zero_grad() # clear the gradients 
-
-            # create some minibatches
-            # we cannot use dataloaders because each of our names is a different length
-            batches = list(range(len(training_data)))
-            random.shuffle(batches)
-            batches = np.array_split(batches, len(batches) //n_batch_size )
-
-            for idx, batch in enumerate(batches): 
-                batch_loss = 0
-                for i in batch: #for each example in this batch
-                    (label_tensor, text_tensor, label, text) = training_data[i]
-                    output = self.forward(text_tensor)
-                    loss = criterion(output, label_tensor)
-                    batch_loss += loss
-
-                # optimize parameters
-                batch_loss.backward()
-                nn.utils.clip_grad_norm_(self.parameters(), 3)
-                optimizer.step()
-                optimizer.zero_grad()
-
-                current_loss += batch_loss.item() / len(batch)
-            
-            all_losses.append(current_loss / len(batches) )
-            if iter % report_every == 0:
-                print(f"{iter} ({iter / n_epoch:.0%}): \t average batch loss = {all_losses[-1]}")
-            current_loss = 0
+            current_loss += batch_loss.item() / len(batch)
         
-        return all_losses
+        all_losses.append(current_loss / len(batches) )
+        if iter % report_every == 0:
+            print(f"{iter} ({iter / n_epoch:.0%}): \t average batch loss = {all_losses[-1]}")
+        current_loss = 0
+    
+    return all_losses
 
 ##########################################################################
 # We can now train a dataset with mini batches for a specified number of epochs
 
-n_hidden = 128
-hidden = torch.zeros(1, n_hidden)
-rnn = RNN(NameData.n_letters, n_hidden, alldata.labels)
 start = time.time()
-all_losses = rnn.learn(train_set, n_epoch=10, learning_rate=0.2, report_every=1)
+all_losses = train(rnn, train_set, n_epoch=10, learning_rate=0.2, report_every=1)
 end = time.time()
 print(f"training took {end-start}s")
 
@@ -492,21 +385,23 @@ plt.show()
 # ``evaluate()``, which is the same as ``train()`` minus the backprop.
 #
 
-def evaluate(rnn, testing_data):
-    confusion = torch.zeros(len(rnn.output_labels), len(rnn.output_labels))
+def evaluate(rnn, testing_data, classes):
+    confusion = torch.zeros(len(classes), len(classes))
     
     rnn.eval() #set to eval mode
     with torch.no_grad(): # do not record the gradients during eval phase
         for i in range(len(testing_data)):
             (label_tensor, text_tensor, label, text) = testing_data[i]
-            output = rnn.forward(text_tensor)
-            guess, guess_i = rnn.label_from_output(output)
-            label_i = rnn.output_labels.index(label)
+            output = rnn(text_tensor)   
+            guess, guess_i = label_from_output(output, classes)
+            label_i = classes.index(label)
             confusion[label_i][guess_i] += 1
 
     # Normalize by dividing every row by its sum
-    for i in range(len(rnn.output_labels)):
-        confusion[i] = confusion[i] / confusion[i].sum()
+    for i in range(len(classes)):
+        denom = confusion[i].sum()
+        if denom > 0:
+            confusion[i] = confusion[i] / denom
 
     # Set up plot
     fig = plt.figure()
@@ -515,8 +410,8 @@ def evaluate(rnn, testing_data):
     fig.colorbar(cax)
 
     # Set up axes
-    ax.set_xticklabels([''] + rnn.output_labels, rotation=90)
-    ax.set_yticklabels([''] + rnn.output_labels)
+    ax.set_xticklabels([''] + classes, rotation=90)
+    ax.set_yticklabels([''] + classes)
 
     # Force label at every tick
     ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
@@ -526,8 +421,9 @@ def evaluate(rnn, testing_data):
     plt.show()
 
 
-evaluate(rnn, test_set)
 
+evaluate(rnn, test_set, classes=alldata.labels_uniq)
+ 
 
 ######################################################################
 # You can pick out bright spots off the main axis that show which
