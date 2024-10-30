49ba46e431c83d10a2b27b6177bb8290fefd6767
Svetlana Karslioglu <svekars@meta.com>
2024-10-30 13:52:01 -0700

    Reduce num_workers to 0

--- beginner_source/introyt/introyt1_tutorial.py
+++ beginner_source/introyt/introyt1_tutorial.py
@@ -358,7 +358,7 @@ trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
 # 
 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+                                          shuffle=True, num_workers=0)
 
 
 ##########################################################################
@@ -436,12 +436,12 @@ transform = transforms.Compose(
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+                                          shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+                                         shuffle=False, num_workers=0)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
