216f7830a352c2e0c0f1a3e4325d0b245a4c9ea4
Svetlana Karslioglu <svekars@meta.com>
2024-11-04 08:52:41 -0800

    Updated

--- beginner_source/introyt/introyt1_tutorial.py
+++ beginner_source/introyt/introyt1_tutorial.py
@@ -358,7 +358,7 @@ trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
 # 
 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=0)
+                                          shuffle=True, num_workers=2)
 
 
 ##########################################################################
@@ -436,12 +436,12 @@ transform = transforms.Compose(
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=0)
+                                          shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=0)
+                                         shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
