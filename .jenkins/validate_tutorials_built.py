ec3f4a4f80c358e8b91ddb272e7f1b8b8a81253b
titaiwangms <titaiwang@microsoft.com>
2024-12-19 21:26:42 +0000

    enable onnx

--- .jenkins/validate_tutorials_built.py
+++ .jenkins/validate_tutorials_built.py
@@ -52,8 +52,6 @@ NOT_RUN = [
     "intermediate_source/text_to_speech_with_torchaudio",
     "intermediate_source/tensorboard_profiler_tutorial", # reenable after 2.0 release.
     "intermediate_source/torch_export_tutorial", # reenable after 2940 is fixed.
-    "beginner_source/onnx/export_simple_model_to_onnx_tutorial", # enable when 3191 is fixed
-    "beginner_source/onnx/onnx_registry_tutorial",  # enable when 3191 is fixed
     "prototype_source/gpu_quantization_torchao_tutorial", # enable when 3194
     "advanced_source/pendulum", # enable when 3195 is fixed
     "intermediate_source/reinforcement_ppo" # enable when 3195 is fixed 
