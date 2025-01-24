6a70faf346ea7ae8e705758193a5657560f53871
Justin Chu <justinchuby@users.noreply.github.com>
2025-01-24 13:55:56 -0800

    you

--- beginner_source/onnx/onnx_registry_tutorial.py
+++ beginner_source/onnx/onnx_registry_tutorial.py
@@ -142,7 +142,7 @@ torch.testing.assert_close(result, torch.tensor([3.0]))
 # Using custom ONNX operators
 # ---------------------------
 #
-# In this case, the user creates a model with standard PyTorch operators, but the runtime
+# In this case, you create a model with standard PyTorch operators, but the runtime
 # (e.g. Microsoft's ONNX Runtime) can provide a custom implementation for that kernel, effectively replacing the
 # existing implementation.
 #
