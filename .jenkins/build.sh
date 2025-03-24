81efd5fc778030111f5bcf61760fb1c83bd8789e
Svetlana Karslioglu <svekars@meta.com>
2025-03-24 14:36:37 -0700

    Update .jenkins/build.sh

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -26,6 +26,8 @@ sudo pip uninstall -y torch torchvision torchaudio torchtext torchdata torchrl t
 pip3 install torch==2.7.0 torchvision torchaudio --no-cache-dir --index-url https://download.pytorch.org/whl/test/cu126
 sudo pip uninstall -y fbgemm-gpu
 sudo pip3 install --pre fbgemm-gpu --index-url https://download.pytorch.org/whl/nightly/cu126/
+pip install tensordict-nightly
+pip install torchrl-nightly
 #sudo pip3 install fbgemm-gpu==1.1.0 torchrec==1.0.0 --no-cache-dir --index-url https://download.pytorch.org/whl/test/cu126
 
 
