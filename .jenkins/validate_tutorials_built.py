f2fcf6f4b4815e828ffe0504c78ca99a60ca9720
Svetlana Karslioglu <svekars@meta.com>
2025-03-25 19:45:54 -0700

    Disable rl tutorials again

--- .jenkins/validate_tutorials_built.py
+++ .jenkins/validate_tutorials_built.py
@@ -50,12 +50,8 @@ NOT_RUN = [
     "intermediate_source/flask_rest_api_tutorial",
     "intermediate_source/text_to_speech_with_torchaudio",
     "intermediate_source/tensorboard_profiler_tutorial", # reenable after 2.0 release.
-<<<<<<< HEAD
     "advanced_source/semi_structured_sparse", # reenable after 3303 is fixed.
     "recipes_source/recipes/reasoning_about_shapes"
-=======
-    "advanced_source/semi_structured_sparse" # reenable after 3303 is fixed.
->>>>>>> 4b04c9b (Update .jenkins/validate_tutorials_built.py)
 ]
 
 def tutorial_source_dirs() -> List[Path]:
