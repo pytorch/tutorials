2a6b6580db44607c2a8be654fbd4a02d05bf18f2
Svetlana Karslioglu <svekars@meta.com>
2025-03-31 15:37:51 -0700

    Update

--- conf.py
+++ conf.py
@@ -22,7 +22,6 @@
 #   GALLERY_PATTERN="neural_style_transfer_tutorial.py" sphinx-build . _build
 #
 # GALLERY_PATTERN variable respects regular expressions.
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
