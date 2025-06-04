588184071660a9711322cc73311803bc1e3463e0
Runtian (Rachel) Li <115634891+Rachel0619@users.noreply.github.com>
2025-06-04 21:16:25 +0000

    Fix code block indentation by removing extra spaces before ::

--- beginner_source/introyt/introyt1_tutorial.py
+++ beginner_source/introyt/introyt1_tutorial.py
@@ -304,20 +304,20 @@ transform = transforms.Compose(
 #    standard deviations (second tuple) of the rgb values of the images in
 #    the dataset. You can calculate these values yourself by running these
 #    few lines of code:
-#          ::
 #
-#           from torch.utils.data import ConcatDataset
-#           transform = transforms.Compose([transforms.ToTensor()])
-#           trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+#    ::
+#        from torch.utils.data import ConcatDataset
+#        transform = transforms.Compose([transforms.ToTensor()])
+#        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
 #                                        download=True, transform=transform)
 #
-#           #stack all train images together into a tensor of shape 
-#           #(50000, 3, 32, 32)
-#           x = torch.stack([sample[0] for sample in ConcatDataset([trainset])])
+#        # stack all train images together into a tensor of shape 
+#        # (50000, 3, 32, 32)
+#        x = torch.stack([sample[0] for sample in ConcatDataset([trainset])])
 #           
-#           #get the mean of each channel            
-#           mean = torch.mean(x, dim=(0,2,3)) #tensor([0.4914, 0.4822, 0.4465])
-#           std = torch.std(x, dim=(0,2,3)) #tensor([0.2470, 0.2435, 0.2616])  
+#        # get the mean of each channel            
+#        mean = torch.mean(x, dim=(0,2,3)) # tensor([0.4914, 0.4822, 0.4465])
+#        std = torch.std(x, dim=(0,2,3)) # tensor([0.2470, 0.2435, 0.2616])  
 #    
 # 
 # There are many more transforms available, including cropping, centering,
