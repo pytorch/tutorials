d37a7fbdd551440937c4846ef2f44f715f046ff9
Parag Ekbote <thecoolekbote189@gmail.com>
2025-06-12 19:16:18 +0000

    update.

--- beginner_source/basics/autogradqs_tutorial.py
+++ beginner_source/basics/autogradqs_tutorial.py
@@ -116,7 +116,7 @@ print(z.requires_grad)
 
 with torch.no_grad():
     z = torch.matmul(x, w)+b
-print(z.requires_grad)
+prin(z.requires_grad)
 
 
 ######################################################################
