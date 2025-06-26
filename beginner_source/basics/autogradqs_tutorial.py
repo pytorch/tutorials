86cf7022004d4f218bbfa9cfdba48769c384d871
Parag Ekbote <thecoolekbote189@gmail.com>
2025-06-26 06:14:29 +0000

    update.

--- beginner_source/basics/autogradqs_tutorial.py
+++ beginner_source/basics/autogradqs_tutorial.py
@@ -32,7 +32,7 @@ x = torch.ones(5)  # input tensor
 y = torch.zeros(3)  # expected output
 w = torch.randn(5, 3, requires_grad=True)
 b = torch.randn(3, requires_grad=True)
-z = torch.matmul(x, w) + b
+z = torch.matmul(x, w)+b
 loss = torch.nn.functional.binary_cross_entropy_with_logits(z, y)
 
 
