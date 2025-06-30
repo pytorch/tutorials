94acfda4dbb7c86e2210c1cc4149746f024d053a
Ti-Tai Wang <titaiwang@microsoft.com>
2025-06-30 23:55:36 +0000

    add redirects.py

--- redirects.py
+++ redirects.py
@@ -3,4 +3,5 @@ redirects = {
     "beginner/ptcheat.html": "../index.html",
     "beginner/deploy_seq2seq_hybrid_frontend_tutorial.html": "../index.html",
     "recipes/bundled_inputs.html": "../index.html",
+    "advanced/super_resolution_with_onnxruntime.html": "../index.html",
 }
