a2e226c75621d88942996d937a821b21febb3f70
Alanna Burke <burkealanna@meta.com>
2025-07-01 12:48:46 -0400

    Removing Torchserve tutorial per Tutorial audit t228126880 (#3412)
    * Removing Torchserve tutorial per tutorial audit.
    * Adding removed page to redirects file.
    
    ---------
    
    Co-authored-by: Svetlana Karslioglu <svekars@meta.com>

--- redirects.py
+++ redirects.py
@@ -3,5 +3,6 @@ redirects = {
     "beginner/ptcheat.html": "../index.html",
     "beginner/deploy_seq2seq_hybrid_frontend_tutorial.html": "../index.html",
     "recipes/bundled_inputs.html": "../index.html",
+    "intermediate/torchserve_with_ipex_2": "../index.html",
     "recipes/torchserve_vertexai_tutorial": "../index.html",
 }
