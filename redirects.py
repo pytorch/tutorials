b696cb1d6a15411b4c4be876409de6cc0ab0163d
ebsmothers <ebs@meta.com>
2025-07-08 13:42:46 -0700

    remove FLAVA tutorial (#3436)
    * remove FLAVA tutorial
    
    * add redirect
    
    * dummy commit
    
    * whitespace
    
    ---------
    
    Co-authored-by: Svetlana Karslioglu <svekars@meta.com>

--- redirects.py
+++ redirects.py
@@ -3,7 +3,8 @@ redirects = {
     "beginner/ptcheat.html": "../index.html",
     "beginner/deploy_seq2seq_hybrid_frontend_tutorial.html": "../index.html",
     "recipes/bundled_inputs.html": "../index.html",
-    "intermediate/torchserve_with_ipex.html": "../index.html",  
+    "intermediate/torchserve_with_ipex.html": "../index.html",
     "intermediate/torchserve_with_ipex_2.html": "../index.html",
     "recipes/torchserve_vertexai_tutorial.html": "../index.html",
+    "beginner/flava_finetuning_tutorial.html": "../index.html",
 }
