bfb17d50947e3ee96be430ee6d4e56136a45a343
Svetlana Karslioglu <svekars@meta.com>
2025-07-14 14:59:27 -0700

    Update build.sh

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -24,9 +24,10 @@ sudo apt-get install -y pandoc
 # Install 2.5 to merge all 2.4 PRs - uncomment to install nightly binaries (update the version as needed).
 sudo pip uninstall -y fbgemm-gpu torchrec
 sudo pip uninstall -y torch torchvision torchaudio torchtext torchdata torchrl tensordict
-sudo pip3 install fbgemm-gpu==1.2.0 torchrec==1.2.0 --no-cache-dir --index-url https://download.pytorch.org/whl/test/cu126
 pip3 install torch==2.8.0 torchvision torchaudio --no-cache-dir --index-url https://download.pytorch.org/whl/test/cu126
 pip3 install torchrl==0.9.1 tensordict==0.9.1
+sudo pip3 install fbgemm-gpu==1.2.0 torchrec==1.2.0 --no-cache-dir --index-url https://download.pytorch.org/whl/test/cu126
+
 
 # Install two language tokenizers for Translation with TorchText tutorial
 python -m spacy download en_core_web_sm
