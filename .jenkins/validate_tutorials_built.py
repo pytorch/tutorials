40332ad8136534e8e7c2174f42fa50cc27a99f7a
sekyondaMeta <127536312+sekyondaMeta@users.noreply.github.com>
2025-07-30 10:00:11 -0400

    Disabling torchrec_intro_tutorial.py
    Disabling torchrec_intro_tutorial.py as it is failing with 2.8

--- .jenkins/validate_tutorials_built.py
+++ .jenkins/validate_tutorials_built.py
@@ -50,6 +50,7 @@ NOT_RUN = [
     "intermediate_source/text_to_speech_with_torchaudio",
     "intermediate_source/tensorboard_profiler_tutorial", # reenable after 2.0 release.
     "advanced_source/semi_structured_sparse", # reenable after 3303 is fixed.
+    "intermediate_source/torchrec_intro_tutorial.py", #failing with 2.8 reenable after 3498
 ]
 
 def tutorial_source_dirs() -> List[Path]:
