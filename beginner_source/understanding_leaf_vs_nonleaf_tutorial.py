468fc1b21f57e059ed70fa6085e843c67a88cf47
saurabhkthakur <thakurksaurabh96@gmail.com>
2026-05-06 22:55:38 +0530

    fixed mermaid indentation

--- beginner_source/understanding_leaf_vs_nonleaf_tutorial.py
+++ beginner_source/understanding_leaf_vs_nonleaf_tutorial.py
@@ -90,7 +90,7 @@ loss = F.mse_loss(y_pred, y)              # scalar loss
 # .. mermaid::
 #
 #    graph TD
-
+#
 #        x["x<br/>is_leaf=True<br/>requires_grad=False<br/>retains_grad=False<br/>grad=None"]
 #        W["W<br/>is_leaf=True<br/>requires_grad=True<br/>retains_grad=False<br/>grad=None"]
 #        b["b<br/>is_leaf=True<br/>requires_grad=True<br/>retains_grad=False<br/>grad=None"]
@@ -99,7 +99,7 @@ loss = F.mse_loss(y_pred, y)              # scalar loss
 #        relu["y_pred = relu(z)<br/>is_leaf=False<br/>requires_grad=True<br/>retains_grad=False<br/>grad=None"]
 #        y["y<br/>is_leaf=True<br/>requires_grad=False<br/>retains_grad=False<br/>grad=None"]
 #        loss["loss = mse(y_pred, y)<br/>is_leaf=False<br/>requires_grad=True<br/>retains_grad=False<br/>grad=None"]
-
+#
 #        x --> matmul
 #        W --> matmul
 #        matmul --> z
@@ -278,22 +278,24 @@ print(f"{loss.grad=}")
 # .. mermaid::
 #
 #    graph TD
-
-#         x["x<br/>is_leaf=True<br/>requires_grad=False<br/>retains_grad=False<br/>grad=None"]
-#         W["W<br/>is_leaf=True<br/>requires_grad=True<br/>retains_grad=False<br/>grad=torch.Tensor"]
-#         b["b<br/>is_leaf=True<br/>requires_grad=True<br/>retains_grad=False<br/>grad=torch.Tensor"]
-#         matmul["x @ W"]
-#         z["z = x @ W + b<br/>is_leaf=False<br/>requires_grad=True<br/>retains_grad=True<br/>grad=torch.Tensor"]
-#         relu["y_pred = relu(z)<br/>is_leaf=False<br/>requires_grad=True<br/>retains_grad=True<br/>grad=torch.Tensor"]
-#         y["y<br/>is_leaf=True<br/>requires_grad=True<br/>retains_grad=False<br/>grad=None"]
-#         loss["loss = mse(y_pred, y)<br/>is_leaf=False<br/>requires_grad=True<br/>retains_grad=True<br/>grad=torch.Tensor"]
-#         x --> matmul
-#         W --> matmul
-#         matmul --> z
-#         b --> z
-#         z --> relu
-#         relu --> loss
-#         y --> loss
+#
+#        x["x<br/>is_leaf=True<br/>requires_grad=False<br/>retains_grad=False<br/>grad=None"]
+#        W["W<br/>is_leaf=True<br/>requires_grad=True<br/>retains_grad=False<br/>grad=torch.Tensor"]
+#        b["b<br/>is_leaf=True<br/>requires_grad=True<br/>retains_grad=False<br/>grad=torch.Tensor"]
+#        matmul["x @ W"]
+#        z["z = x @ W + b<br/>is_leaf=False<br/>requires_grad=True<br/>retains_grad=True<br/>grad=torch.Tensor"]
+#        relu["y_pred = relu(z)<br/>is_leaf=False<br/>requires_grad=True<br/>retains_grad=True<br/>grad=torch.Tensor"]
+#        y["y<br/>is_leaf=True<br/>requires_grad=True<br/>retains_grad=False<br/>grad=None"]
+#        loss["loss = mse(y_pred, y)<br/>is_leaf=False<br/>requires_grad=True<br/>retains_grad=True<br/>grad=torch.Tensor"]
+#
+#        x --> matmul
+#        W --> matmul
+#        matmul --> z
+#        b --> z
+#        z --> relu
+#        relu --> loss
+#        y --> loss
+#
 # If you call ``retain_grad()`` on a leaf tensor, it results in a no-op
 # since leaf tensors already retain their gradients by default (when
 # ``requires_grad=True``).
