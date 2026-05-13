673a1dd09f772077d3a80fe586559da79075257e
Max Klein <telamonian@users.noreply.github.com>
2026-05-13 14:06:42 -0400

    fix description of equation in polynomial_autograd.py example
    The equation that a + b*x + c*x^2 + d*x^3 converges to changed in a recent commit, from y=sin(x) to y=e^x. However, they didn't change the text description of the equation. This commit fixes that

--- beginner_source/examples_autograd/polynomial_autograd.py
+++ beginner_source/examples_autograd/polynomial_autograd.py
@@ -2,8 +2,8 @@ r"""
 PyTorch: Tensors and autograd
 -------------------------------
 
-A third order polynomial, trained to predict :math:`y=\sin(x)` from :math:`-\pi`
-to :math:`\pi` by minimizing squared Euclidean distance.
+A third order polynomial, trained to predict :math:`y=e^x` from :math:`-1`
+to :math:`1` by minimizing squared Euclidean distance.
 
 This implementation computes the forward pass using operations on PyTorch
 Tensors, and uses PyTorch autograd to compute gradients.
